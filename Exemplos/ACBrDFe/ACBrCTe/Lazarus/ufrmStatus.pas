93d832b0e9ed3f095446d219e805061a987ef9f2
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-03-30 02:51:02 +0000

    Programa exemplo do ACBrCTe.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@8655 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Exemplos/ACBrDFe/ACBrCTe/Lazarus/ufrmStatus.pas
@@ -0,0 +1,29 @@
+unit ufrmStatus;
+
+{$MODE Delphi}
+
+interface
+
+uses
+  LCLIntf, LCLType, LMessages, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TfrmStatus = class(TForm)
+    Panel1: TPanel;
+    lbl1: TLabel;
+    lblStatus: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmStatus: TfrmStatus;
+
+implementation
+
+{$R *.lfm}
+
+end.

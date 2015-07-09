dd8e203e5eea1106a9245d9bec292951066b6e81
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-07-09 18:36:42 +0000

    Correção ESCPOS estava faltando na lista de Comandos http://www.projetoacbr.com.br/forum/topic/22787-comando-escpos-n%C3%A3o-reconhecido/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@9511 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrMonitorPLUS/Lazarus/CmdUnit.pas
+++ Projetos/ACBrMonitorPLUS/Lazarus/CmdUnit.pas
@@ -37,7 +37,7 @@ interface
 Uses SysUtils, Classes, Math ;
 
 Const
-   Objetos = '"ECF","CHQ","GAV","DIS","LCB","ACBR","BAL","ETQ","BOLETO","CEP","IBGE","EMAIL","SEDEX","NCM","NFE","SAT"' ;
+   Objetos = '"ECF","CHQ","GAV","DIS","LCB","ACBR","BAL","ETQ","BOLETO","CEP","IBGE","EMAIL","SEDEX","NCM","NFE","SAT","ESCPOS"' ;
 
 type
 TACBrCmd = class

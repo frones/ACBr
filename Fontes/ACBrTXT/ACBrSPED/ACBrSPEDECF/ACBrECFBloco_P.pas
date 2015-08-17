9684993cbc90a198260ae4f0f266ec67dc8f331f
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-08-17 21:01:14 +0000

    Bloco P e suas funções http://www.projetoacbr.com.br/forum/topic/23565-sped-ecf-disponibilizado-do-trunk2/?do=findComment&comment=152954
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@9741 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSPED/ACBrSPEDECF/ACBrECFBloco_P.pas
+++ Fontes/ACBrTXT/ACBrSPED/ACBrSPEDECF/ACBrECFBloco_P.pas
@@ -48,34 +48,77 @@ uses
   SysUtils, Classes, Contnrs, DateUtils, ACBrECFBlocos;
 
 type
+  TRegistroP030List = class;
+  TRegistroP100List = class;
+  TRegistroP130List = class;
+  TRegistroP150List = class;
+  TRegistroP200List = class;
+  TRegistroP230List = class;
+  TRegistroP300     = class;
+  TRegistroP400     = class;
+  TRegistroP500     = class;
+
   /// Registro P001 - Abertura do Bloco P – Lucro Presumido
   TRegistroP001 = class(TOpenBlocos)
   private
+    FRegistroP030         : TRegistroP030List; // NIVEL 2
   public
+    constructor Create;  virtual;              /// Create
+    destructor  Destroy; override;             /// Destroy
+
+    property RegistroP030 : TRegistroP030List read FRegistroP030 write FRegistroP030;
   end;
 
-  /// Registro P030 - Identificação dos Período e Forma de Apuração do
-  /// IRPJ e da CSLL das Empresas Tributadas pelo
-  /// Lucro Presumido
+  // Registro P030 - Identificação dos Período e Forma de Apuração do
+  // IRPJ e da CSLL das Empresas Tributadas pelo Lucro Presumido
 
   { TRegistroP030 }
-
-  TRegistroP030 = class(TBlocos)
+  TRegistroP030 = class
   private
+    FRegistroP100           : TRegistroP100List; // NIVEL 3
+    FRegistroP130           : TRegistroP130List; // NIVEL 3
+    FRegistroP150           : TRegistroP150List; // NIVEL 3
+    FRegistroP200           : TRegistroP200List; // NIVEL 3
+    FRegistroP230           : TRegistroP230List; // NIVEL 3
+    FRegistroP300           : TRegistroP300; // NIVEL 3
+    FRegistroP400           : TRegistroP400; // NIVEL 3
+    FRegistroP500           : TRegistroP500; // NIVEL 3
+
     fDT_FIN:   TDateTime;
     fDT_INI:   TDateTime;
     fPER_APUR: string;
   public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    property RegistroP100 : TRegistroP100List read FRegistroP100 write FRegistroP100;
+    property RegistroP130 : TRegistroP130List read FRegistroP130 write FRegistroP130;
+    property RegistroP150 : TRegistroP150List read FRegistroP150 write FRegistroP150;
+    property RegistroP200 : TRegistroP200List read FRegistroP200 write FRegistroP200;
+    property RegistroP230 : TRegistroP230List read FRegistroP230 write FRegistroP230;
+    property RegistroP300 : TRegistroP300 read FRegistroP300 write FRegistroP300;
+    property RegistroP400 : TRegistroP400 read FRegistroP400 write FRegistroP400;
+    property RegistroP500 : TRegistroP500 read FRegistroP500 write FRegistroP500;
+
     property DT_INI: TDateTime read fDT_INI write fDT_INI;
     property DT_FIN: TDateTime read fDT_FIN write fDT_FIN;
     property PER_APUR: string read fPER_APUR write fPER_APUR;
   end;
 
-  /// Registro P100 - Balanço Patrimonial
+  { TRegistroP030 - Lista }
+  TRegistroP030List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP030;
+    procedure SetItem(Index: Integer; const Value: TRegistroP030);
+  public
+    function New: TRegistroP030;
+    property Items[Index: Integer]: TRegistroP030 read GetItem write SetItem;
+  end;
+
+  // Registro P100 - Balanço Patrimonial
 
   { TRegistroP100 }
-
-  TRegistroP100 = class(TBlocos)
+  TRegistroP100 = class
   private
     fCODIGO: string;
     fCOD_CTA_SUP: string;
@@ -100,11 +143,24 @@ type
     property IND_VAL_CTA_REF_FIN: string read fIND_VAL_CTA_REF_FIN write fIND_VAL_CTA_REF_FIN;
   end;
 
-  /// Registro P130 - Demonstração das Receitas Incentivadas do Lucro Presumido
+  { TRegistroP100 - Lista }
+  TRegistroP100List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP100;
+    procedure SetItem(Index: Integer; const Value: TRegistroP100);
+  public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    function New: TRegistroP100;
+    property Items[Index: Integer]: TRegistroP100 read GetItem write SetItem;
+  end;
+
+
+  // Registro P130 - Demonstração das Receitas Incentivadas do Lucro Presumido
 
   { TRegistroP130 }
-
-  TRegistroP130 = class(TBlocos)
+  TRegistroP130 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -115,11 +171,24 @@ type
     property VALOR: variant read fVALOR write fVALOR;
   end;
 
-  /// Registro P150 - Demonstração do Resultado
+  { TRegistroP150 - Lista }
+  TRegistroP130List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP130;
+    procedure SetItem(Index: Integer; const Value: TRegistroP130);
+  public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    function New: TRegistroP130;
+    property Items[Index: Integer]: TRegistroP130 read GetItem write SetItem;
+  end;
+
+
+  // Registro P150 - Demonstração do Resultado
 
   { TRegistroP150 }
-
-  TRegistroP150 = class(TBlocos)
+  TRegistroP150 = class
   private
     fCODIGO: string;
     fCOD_CTA_SUP: string;
@@ -138,14 +207,26 @@ type
     property COD_CTA_SUP: string read fCOD_CTA_SUP write fCOD_CTA_SUP;
     property VALOR: variant read fVALOR write fVALOR;
     property IND_VALOR: string read fIND_VALOR write fIND_VALOR;
-
   end;
+
+  { TRegistroP150 - Lista }
+  TRegistroP150List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP150;
+    procedure SetItem(Index: Integer; const Value: TRegistroP150);
+  public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    function New: TRegistroP150;
+    property Items[Index: Integer]: TRegistroP150 read GetItem write SetItem;
+  end;
+
 
   /// Registro P200 - Apuração da Base de Cálculo do Lucro Presumido
 
   { TRegistroP200 }
-
-  TRegistroP200 = class(TBlocos)
+  TRegistroP200 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -155,12 +236,25 @@ type
     property DESCRICAO: string read fDESCRICAO write fDESCRICAO;
     property VALOR: variant read fVALOR write fVALOR;
   end;
+
+  { TRegistroP200 - Lista }
+  TRegistroP200List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP200;
+    procedure SetItem(Index: Integer; const Value: TRegistroP200);
+  public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    function New: TRegistroP200;
+    property Items[Index: Integer]: TRegistroP200 read GetItem write SetItem;
+  end;
+
 
   /// Registro P230 - Cálculo da Isenção e Redução do Lucro Presumido
 
   { TRegistroP230 }
-
-  TRegistroP230 = class(TBlocos)
+  TRegistroP230 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -171,11 +265,23 @@ type
     property VALOR: variant read fVALOR write fVALOR;
   end;
 
-  /// Registro P300 - Cálculo do IRPJ com Base no Lucro Presumido
+  { TRegistroP230 - Lista }
+  TRegistroP230List = class(TObjectList)
+  private
+    function  GetItem(Index: Integer): TRegistroP230;
+    procedure SetItem(Index: Integer; const Value: TRegistroP230);
+  public
+    constructor Create;  virtual;
+    destructor  Destroy; override;
+
+    function New: TRegistroP230;
+    property Items[Index: Integer]: TRegistroP230 read GetItem write SetItem;
+  end;
+
+  // Registro P300 - Cálculo do IRPJ com Base no Lucro Presumido
 
   { TRegistroP300 }
-
-  TRegistroP300 = class(TBlocos)
+  TRegistroP300 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -190,7 +296,7 @@ type
 
   { TRegistroP400 }
 
-  TRegistroP400 = class(TBlocos)
+  TRegistroP400 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -205,7 +311,7 @@ type
 
   { TRegistroP500 }
 
-  TRegistroP500 = class(TBlocos)
+  TRegistroP500 = class
   private
     fCODIGO:    string;
     fDESCRICAO: string;
@@ -218,8 +324,214 @@ type
 
   /// Registro P990 - ENCERRAMENTO DO BLOCO P
   TRegistroP990 = class(TCloseBlocos)
+    FQTD_LIN_P: integer;
+  public
+    property QTD_LIN_P: integer read FQTD_LIN_P write FQTD_LIN_P;
   end;
 
 implementation
+
+{ TRegistroP001 }
+
+constructor TRegistroP001.Create;
+begin
+  FRegistroP030 := TRegistroP030List.Create;
+end;
+
+destructor TRegistroP001.Destroy;
+begin
+  FRegistroP030.Free;
+  inherited;
+end;
+
+{ TRegistroP030 }
+
+constructor TRegistroP030.Create;
+begin
+  FRegistroP100 := TRegistroP100List.Create;
+  FRegistroP130 := TRegistroP130List.Create;
+  FRegistroP150 := TRegistroP150List.Create;
+  FRegistroP200 := TRegistroP200List.Create;
+  FRegistroP230 := TRegistroP230List.Create;
+  FRegistroP300 := TRegistroP300.Create;
+  FRegistroP400 := TRegistroP400.Create;
+  FRegistroP500 := TRegistroP500.Create;
+end;
+
+destructor TRegistroP030.Destroy;
+begin
+  FRegistroP100.Free;
+  FRegistroP130.Free;
+  FRegistroP150.Free;
+  FRegistroP200.Free;
+  FRegistroP230.Free;
+  FRegistroP300.Free;
+  FRegistroP400.Free;
+  FRegistroP500.Free;
+  inherited;
+end;
+
+{ TRegistroP030List }
+
+function TRegistroP030List.GetItem(Index: Integer): TRegistroP030;
+begin
+  Result := TRegistroP030(Inherited Items[Index]);
+end;
+
+function TRegistroP030List.New: TRegistroP030;
+begin
+
+end;
+
+procedure TRegistroP030List.SetItem(Index: Integer; const Value: TRegistroP030);
+begin
+  Put(Index, Value);
+end;
+
+
+{ TRegistroP100List }
+
+constructor TRegistroP100List.Create;
+begin
+
+end;
+
+destructor TRegistroP100List.Destroy;
+begin
+
+  inherited;
+end;
+
+function TRegistroP100List.GetItem(Index: Integer): TRegistroP100;
+begin
+  Result := TRegistroP100(Inherited Items[Index]);
+end;
+
+function TRegistroP100List.New: TRegistroP100;
+begin
+  Result := TRegistroP100.Create;
+  Add(Result);
+end;
+
+procedure TRegistroP100List.SetItem(Index: Integer; const Value: TRegistroP100);
+begin
+  Put(Index, Value);
+end;
+
+{ TRegistroP130List }
+
+constructor TRegistroP130List.Create;
+begin
+
+end;
+
+destructor TRegistroP130List.Destroy;
+begin
+
+  inherited;
+end;
+
+function TRegistroP130List.GetItem(Index: Integer): TRegistroP130;
+begin
+  Result := TRegistroP130(Inherited Items[Index]);
+end;
+
+function TRegistroP130List.New: TRegistroP130;
+begin
+  Result := TRegistroP130.Create;
+  Add(Result);
+end;
+
+procedure TRegistroP130List.SetItem(Index: Integer; const Value: TRegistroP130);
+begin
+  Put(Index, Value);
+end;
+
+{ TRegistroP150List }
+
+constructor TRegistroP150List.Create;
+begin
+
+end;
+
+destructor TRegistroP150List.Destroy;
+begin
+
+  inherited;
+end;
+
+function TRegistroP150List.GetItem(Index: Integer): TRegistroP150;
+begin
+  Result := TRegistroP150(Inherited Items[Index]);
+end;
+
+function TRegistroP150List.New: TRegistroP150;
+begin
+  Result := TRegistroP150.Create;
+  Add(Result);
+end;
+
+procedure TRegistroP150List.SetItem(Index: Integer; const Value: TRegistroP150);
+begin
+  Put(Index, Value);
+end;
+
+{ TRegistroP200List }
+
+constructor TRegistroP200List.Create;
+begin
+
+end;
+
+destructor TRegistroP200List.Destroy;
+begin
+
+  inherited;
+end;
+
+function TRegistroP200List.GetItem(Index: Integer): TRegistroP200;
+begin
+  Result := TRegistroP200(Inherited Items[Index]);
+end;
+
+function TRegistroP200List.New: TRegistroP200;
+begin
+  Result := TRegistroP200.Create;
+  Add(Result);
+end;
+
+procedure TRegistroP200List.SetItem(Index: Integer; const Value: TRegistroP200);
+begin
+  Put(Index, Value);
+end;
+
+{ TRegistroP230List }
+
+constructor TRegistroP230List.Create;
+begin
+
+end;
+
+destructor TRegistroP230List.Destroy;
+begin
+
+  inherited;
+end;
+
+function TRegistroP230List.GetItem(Index: Integer): TRegistroP230;
+begin
+  Result := TRegistroP230(Inherited Items[Index]);
+end;
+
+function TRegistroP230List.New: TRegistroP230;
+begin
+  Result := TRegistroP230.Create;
+  Add(Result);
+end;
+
+procedure TRegistroP230List.SetItem(Index: Integer; const Value: TRegistroP230);
+begin
+  Put(Index, Value);
+end;
 
 end.
--- Fontes/ACBrTXT/ACBrSPED/ACBrSPEDECF/ACBrECFBloco_P_Class.pas
+++ Fontes/ACBrTXT/ACBrSPED/ACBrSPEDECF/ACBrECFBloco_P_Class.pas
@@ -49,44 +49,390 @@ uses
   ACBrTXTClass, ACBrECFBloco_0_Class;
 
 type
-  /// TBloco_P-
-
   { TBloco_P }
 
   TBloco_P = class(TACBrSPED)
   private
-    FBloco_0: TBloco_0;
     FRegistroP001: TRegistroP001;
     FRegistroP990: TRegistroP990;
+    fRegistroP030Count : Integer;
+    fRegistroP100Count : Integer;
+    fRegistroP130Count : Integer;
+    fRegistroP150Count : Integer;
+    fRegistroP200Count : Integer;
+    fRegistroP230Count : Integer;
+    fRegistroP300Count : Integer;
+    fRegistroP400Count : Integer;
+    fRegistroP500Count : Integer;
+
+    FBloco_0           : TBloco_0;
+
+    procedure WriteRegistroP030( RegP001 : TRegistroP001 );
+    procedure WriteRegistroP100( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP130( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP150( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP200( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP230( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP300( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP400( RegP030 : TRegistroP030 );
+    procedure WriteRegistroP500( RegP030 : TRegistroP030 );
+
+    procedure CriaRegistros;
+    procedure LiberaRegistros;
   public
-    property Bloco_0: TBloco_0 read FBloco_0 write FBloco_0;
+    constructor Create ;                                      /// Create
+    destructor  Destroy; override;                            /// Destroy
 
-    constructor Create;
-    destructor Destroy;
+    procedure LimpaRegistros;
 
-    property RegistroP001: TRegistroP001 read FRegistroP001 write FRegistroP001;
-    property RegistroP990: TRegistroP990 read FRegistroP990 write FRegistroP990;
-  published
+    function RegistroP001New            : TRegistroP001;
+    function RegistroP030New            : TRegistroP030;
+    function RegistroP100New            : TRegistroP100;
+    function RegistroP130New            : TRegistroP130;
+    function RegistroP150New            : TRegistroP150;
+    function RegistroP200New            : TRegistroP200;
+    function RegistroP230New            : TRegistroP230;
+    function RegistroP300New            : TRegistroP300;
+    function RegistroP400New            : TRegistroP400;
+    function RegistroP500New            : TRegistroP500;
+
+    procedure WriteRegistroP001;
+    procedure WriteRegistroP990;
+
+    property Bloco_0           : TBloco_0      read FBloco_0           write FBloco_0;
+    property RegistroP001      : TRegistroP001 read fRegistroP001      write fRegistroP001;
+    property RegistroP990      : TRegistroP990 read fRegistroP990      write fRegistroP990;
+
+    property RegistroP030Count : Integer       read FRegistroP030Count write fRegistroP030Count;
+    property RegistroP100Count : Integer       read FRegistroP100Count write fRegistroP100Count;
+    property RegistroP130Count : Integer       read FRegistroP130Count write fRegistroP130Count;
+    property RegistroP150Count : Integer       read FRegistroP150Count write fRegistroP150Count;
+    property RegistroP200Count : Integer       read FRegistroP200Count write fRegistroP200Count;
+    property RegistroP230Count : Integer       read FRegistroP230Count write fRegistroP230Count;
+    property RegistroP300Count : Integer       read FRegistroP300Count write fRegistroP300Count;
+    property RegistroP400Count : Integer       read FRegistroP400Count write fRegistroP400Count;
+    property RegistroP500Count : Integer       read FRegistroP500Count write fRegistroP500Count;
+
   end;
-
 
 implementation
 
 uses
-  ACBrTXTUtils, StrUtils;
+  ACBrTXTUtils;
 
 { TBloco_P }
 
 constructor TBloco_P.Create;
 begin
-  FRegistroP001 := TRegistroP001.Create;
-  FRegistroP990 := TRegistroP990.Create;
+  inherited;
+  CriaRegistros;
 end;
 
 destructor TBloco_P.Destroy;
 begin
-  FRegistroP001.Free;
-  FRegistroP990.Free;
+  LiberaRegistros;
+  inherited;
+end;
+
+procedure TBloco_P.CriaRegistros;
+begin
+  fRegistroP001 := TRegistroP001.Create;
+  fRegistroP990 := TRegistroP990.Create;
+
+  fRegistroP030Count := 0;
+  fRegistroP100Count := 0;
+  fRegistroP130Count := 0;
+  fRegistroP150Count := 0;
+  fRegistroP200Count := 0;
+  fRegistroP230Count := 0;
+  fRegistroP300Count := 0;
+  fRegistroP400Count := 0;
+  fRegistroP500Count := 0;
+
+  fRegistroP990.QTD_LIN_P := 0;
+end;
+
+procedure TBloco_P.LiberaRegistros;
+begin
+  fRegistroP001.Free;
+  fRegistroP990.Free;
+end;
+
+procedure TBloco_P.LimpaRegistros;
+begin
+  LiberaRegistros;
+  CriaRegistros;
+end;
+
+function TBloco_P.RegistroP001New: TRegistroP001;
+begin
+  Result := FRegistroP001;
+end;
+
+function TBloco_P.RegistroP030New: TRegistroP030;
+begin
+   Result := FRegistroP001.RegistroP030.New;
+end;
+
+function TBloco_P.RegistroP100New: TRegistroP100;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP100.New;
+end;
+
+function TBloco_P.RegistroP130New: TRegistroP130;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP130.New;
+end;
+
+function TBloco_P.RegistroP150New: TRegistroP150;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP150.New;
+end;
+
+function TBloco_P.RegistroP200New: TRegistroP200;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP200.New;
+end;
+
+function TBloco_P.RegistroP230New: TRegistroP230;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP230.New;
+end;
+
+function TBloco_P.RegistroP300New: TRegistroP300;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP300;
+end;
+
+function TBloco_P.RegistroP400New: TRegistroP400;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP400;
+end;
+
+function TBloco_P.RegistroP500New: TRegistroP500;
+var
+  P030Count: integer;
+begin
+  P030Count := FRegistroP001.RegistroP030.Count -1;
+  Result := FRegistroP001.RegistroP030.Items[P030Count].RegistroP500;
+end;
+
+procedure TBloco_P.WriteRegistroP001;
+begin
+  if Assigned( fRegistroP001 ) then
+  begin
+    with fRegistroP001 do
+    begin
+       Add(LFill('P001') +
+           LFill( Integer(IND_DAD), 0 ) );
+
+       if IND_DAD = idComDados then
+       begin
+         WriteRegistroP030(fRegistroP001);
+       end;
+    end;
+    RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP030(RegP001: TRegistroP001);
+var
+  iCount: Integer;
+begin
+  if Assigned( RegP001.RegistroP030 ) then
+  begin
+    for iCount := 0 to RegP001.RegistroP030.Count -1 do
+    begin
+      with RegP001.RegistroP030.Items[iCount] do
+      begin
+        Add(
+           LFill( 'P030' )
+         );
+      end;
+      // Registros FILHOS
+      WriteRegistroP100( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP130( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP150( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP200( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP230( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP300( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP400( RegP001.RegistroP030.Items[iCount] );
+      WriteRegistroP500( RegP001.RegistroP030.Items[iCount] );
+
+      RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+    end;
+    fRegistroP030Count := fRegistroP030Count + RegP001.RegistroP030.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP100(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP100 ) then
+  begin
+     for iCount := 0 to RegP030.RegistroP100.Count - 1 do
+     begin
+        with RegP030.RegistroP100.Items[iCount] do
+        begin
+
+        end;
+        RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+     end;
+     FRegistroP100Count := FRegistroP100Count + RegP030.RegistroP100.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP130(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP130 ) then
+  begin
+     for iCount := 0 to RegP030.RegistroP130.Count - 1 do
+     begin
+        with RegP030.RegistroP130.Items[iCount] do
+        begin
+
+        end;
+        RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+     end;
+     FRegistroP130Count := FRegistroP130Count + RegP030.RegistroP130.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP150(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP150 ) then
+  begin
+     for iCount := 0 to RegP030.RegistroP150.Count - 1 do
+     begin
+        with RegP030.RegistroP150.Items[iCount] do
+        begin
+
+        end;
+        RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+     end;
+     FRegistroP150Count := FRegistroP150Count + RegP030.RegistroP150.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP200(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP200 ) then
+  begin
+     for iCount := 0 to RegP030.RegistroP200.Count - 1 do
+     begin
+        with RegP030.RegistroP200.Items[iCount] do
+        begin
+
+        end;
+        RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+     end;
+     FRegistroP200Count := FRegistroP200Count + RegP030.RegistroP200.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP230(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP230 ) then
+  begin
+     for iCount := 0 to RegP030.RegistroP230.Count - 1 do
+     begin
+        with RegP030.RegistroP230.Items[iCount] do
+        begin
+
+        end;
+        RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+     end;
+     FRegistroP230Count := FRegistroP230Count + RegP030.RegistroP230.Count;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP300(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP300 ) then
+  begin
+    with RegP030.RegistroP300 do
+    begin
+
+    end;
+    RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+    FRegistroP300Count := FRegistroP300Count + 1;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP400(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP400 ) then
+  begin
+    with RegP030.RegistroP400 do
+    begin
+
+    end;
+    RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+    FRegistroP400Count := FRegistroP400Count + 1;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP500(RegP030: TRegistroP030);
+var
+  iCount:Integer;
+begin
+  if Assigned( RegP030.RegistroP500 ) then
+  begin
+    with RegP030.RegistroP500 do
+    begin
+
+    end;
+    RegistroP990.QTD_LIN_P := RegistroP990.QTD_LIN_P + 1;
+    FRegistroP500Count := FRegistroP500Count + 1;
+  end;
+end;
+
+procedure TBloco_P.WriteRegistroP990;
+begin
+  if Assigned(RegistroP990) then
+  begin
+     with RegistroP990 do
+     begin
+        QTD_LIN_P := QTD_LIN_P + 1;
+        //
+        Add(LFill( 'P990' ) +
+            LFill( QTD_LIN_P, 0 ) );
+     end;
+  end;
 end;
 
 end.

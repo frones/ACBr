7b93707ad159d7b950d74945e779bc80a3ce94b2
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-08-27 20:33:13 +0000

    Exemplo de comunicação em Lazarus
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@9840 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrMonitorPLUS/Lazarus/Exemplos/Lazarus/unit1.pas
@@ -0,0 +1,78 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, blcksock;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    bEnviar: TButton;
+    bConectar: TButton;
+    edIP: TEdit;
+    Label1: TLabel;
+    mEnviar: TMemo;
+    mResposta: TMemo;
+    Panel1: TPanel;
+    procedure bConectarClick(Sender: TObject);
+    procedure bEnviarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { private declarations }
+    fSocket : TBlockSocket;
+
+    procedure AguardaReposta;
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  fSocket := TBlockSocket.Create;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  fSocket.Free;
+end;
+
+procedure TForm1.bConectarClick(Sender: TObject);
+begin
+  fSocket.Connect(edIP.Text, '3434');
+  AguardaReposta;
+end;
+
+procedure TForm1.bEnviarClick(Sender: TObject);
+begin
+  fSocket.SendString(mEnviar.Lines.Text + #13+#10+'.'+#13+#10);
+  AguardaReposta;
+end;
+
+procedure TForm1.AguardaReposta;
+var
+  Resposta: String;
+begin
+  Resposta := fSocket.RecvTerminated(5000, #3);
+  mResposta.Lines.Add('===================');
+  mResposta.Lines.Add(Resposta);
+end;
+
+
+end.
+

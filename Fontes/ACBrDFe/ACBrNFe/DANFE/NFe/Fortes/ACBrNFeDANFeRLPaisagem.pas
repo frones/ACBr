dfe72eb2951dbc52c940fb464df27ac18f45ec12
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2015-09-25 12:17:43 +0000

    - acerto na uses data.db que foi adicionada e passou desapercebida
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@10081 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fortes/ACBrNFeDANFeRLPaisagem.pas
+++ Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fortes/ACBrNFeDANFeRLPaisagem.pas
@@ -55,7 +55,7 @@ uses
       jpeg,
     {$IFEND}
   {$ENDIF}
-  ACBrNFeDANFeRL, pcnConversao, RLBarcode, Data.DB;
+  ACBrNFeDANFeRL, pcnConversao, RLBarcode, DB;
 
 type
   TfrlDANFeRLPaisagem = class(TfrlDANFeRL)

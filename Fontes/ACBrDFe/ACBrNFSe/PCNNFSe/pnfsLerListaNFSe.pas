ddb1885cd48b4d13c326618eeed74e78884d6397
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-01-22 19:39:54 +0000

    Inversão da condição.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@10948 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsLerListaNFSe.pas
+++ Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsLerListaNFSe.pas
@@ -342,9 +342,9 @@ begin
         Nivel := 2;
 
       i := 0;
-      while (Leitor.rExtrai(Nivel, 'CompNfse', '', i + 1) <> '') or
+      while (Leitor.rExtrai(Nivel, 'tcCompNfse', '', i + 1) <> '') or
+            (Leitor.rExtrai(Nivel, 'CompNfse', '', i + 1) <> '') or
             (Leitor.rExtrai(Nivel, 'ComplNfse', '', i + 1) <> '') or
-            (Leitor.rExtrai(Nivel, 'tcCompNfse', '', i + 1) <> '') or
             ((Provedor in [proActcon]) and (Leitor.rExtrai(Nivel + 1, 'Nfse', '', i + 1) <> '')) do
       begin
         NFSe := TNFSe.Create;

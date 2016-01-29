028d381c97690ea70f5c98bbceeffd57fae219e5
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-01-29 19:38:37 +0000

    http://www.projetoacbr.com.br/forum/topic/26563-erro-na-leitura-do-retorno-da-caixa/#comment-179660
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11023 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBoleto.pas
+++ Fontes/ACBrBoleto/ACBrBoleto.pas
@@ -56,7 +56,7 @@ uses Classes, Graphics, Contnrs,
      ACBrBase, ACBrMail, ACBrValidador;
 
 const
-  CACBrBoleto_Versao = '0.0.174a';
+  CACBrBoleto_Versao = '0.0.175a';
 
   cACBrTipoOcorrenciaDecricao: array[0..180] of String = (
   'Remessa Registrar',

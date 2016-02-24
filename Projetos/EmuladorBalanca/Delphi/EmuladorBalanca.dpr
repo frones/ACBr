342c44fc804269c5f159b94e06d900cc75392269
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-02-24 13:41:00 +0000

    Adicionando Emulador de Balanças no Trunk2
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11223 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/EmuladorBalanca/Delphi/EmuladorBalanca.dpr
@@ -0,0 +1,13 @@
+program EmuladorBalanca;
+
+uses
+  Forms,
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.

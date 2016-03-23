931eb4cfd1cddc55c0d7cad276f710cb0933d0a0
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-03-23 13:27:17 +0000

    Correção para mostrar correto a informação do CEP no evento http://www.projetoacbr.com.br/forum/topic/28876-erro-na-formata%C3%A7%C3%A3o-do-cep-danfe-evento-fortes/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11486 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fortes/ACBrNFeDANFeEventoRLRetrato.pas
+++ Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fortes/ACBrNFeDANFeEventoRLRetrato.pas
@@ -266,7 +266,7 @@ begin
               else
                 rllEmitEndereco.Caption := Emit.EnderEmit.xLgr + ', ' + Emit.EnderEmit.nro;
               rllEmitBairro.Caption := Emit.EnderEmit.xBairro;
-              rllEmitCEP.Caption := FormatarCEP(IntToStr(Emit.EnderEmit.CEP));
+              rllEmitCEP.Caption := FormatarCEP(Poem_Zeros(Emit.EnderEmit.CEP, 8));
               rllEmitCidade.Caption := Emit.EnderEmit.xMun;
               rllEmitFone.Caption := FormatarFone(Emit.EnderEmit.fone);
               rllEmitUF.Caption := Emit.EnderEmit.UF;
@@ -280,7 +280,7 @@ begin
               else
                 rllDestEndereco.Caption := Dest.EnderDest.xLgr + ', ' + Dest.EnderDest.nro;
               rllDestBairro.Caption := Dest.EnderDest.xBairro;
-              rllDestCEP.Caption := FormatarCEP(IntToStr(Dest.EnderDest.CEP));
+              rllDestCEP.Caption := FormatarCEP(Poem_Zeros(Dest.EnderDest.CEP, 8));
               rllDestCidade.Caption := Dest.EnderDest.xMun;
               rllDestFone.Caption := FormatarFone(Dest.EnderDest.fone);
               rllDestUF.Caption := Dest.EnderDest.UF;

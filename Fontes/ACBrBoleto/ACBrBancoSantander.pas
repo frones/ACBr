b82d8405455ce225579d829a640694995cc29026
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-04-05 00:32:45 +0000

    http://www.projetoacbr.com.br/forum/topic/29127-ajuste-remessa-240-santander-data-da-multa-segmento-r/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11593 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBancoSantander.pas
+++ Fontes/ACBrBoleto/ACBrBancoSantander.pas
@@ -580,7 +580,7 @@ begin
               IntToStrZero(0, 15)                                        + // 027 - 041 / Valor/Percentual a ser concedido
               Space(24)                                                  + // 042 – 065 / Reservado (uso Banco)
               '1'                                                        + // 066 - 066 / Código da multa
-              FormatDateTime('ddmmyyyy',DataMoraJuros)                   + // 067 - 074 / Data da multa
+              sDataMoraJuros                                             + // 067 - 074 / Data da multa
               IntToStrZero(round(ValorDocumento * PercentualMulta), 15)  + // 075 - 089 / Valor/Percentual a ser aplicado
               Space(10)                                                  + // 090 - 099 / Reservado (uso Banco)
               MontarInstrucoes1                                          + // 100 - 139 / Mensagem 3

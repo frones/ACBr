f928fdac3a4809f4e208f4223b2c1dd4943eea07
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-04-08 17:19:43 +0000

    Correção.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11612 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeW_ABRASFv1.pas
+++ Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeW_ABRASFv1.pas
@@ -141,7 +141,8 @@ begin
   begin
     Gerador.wGrupoNFSe('Tomador');
 
-    if (NFSe.Tomador.Endereco.UF <> 'EX') then
+    if (NFSe.Tomador.Endereco.UF <> 'EX') or
+       (FProvedor = proSimplISS) or (FProvedor = proISSNet) then
     begin
       Gerador.wGrupoNFSe('IdentificacaoTomador');
       Gerador.wGrupoNFSe('CpfCnpj');

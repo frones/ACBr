595b4d62d0262204f017317504c0fa3b1bbb5040
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-04-14 17:41:00 +0000

    Adição do exemplo do ACBrIBPTax em Lazarus
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11639 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Exemplos/ACBrTCP/ACBrIBPTax/Lazarus/ProxyConfig.pas
@@ -0,0 +1,48 @@
+unit ProxyConfig;
+
+{$MODE Delphi}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Buttons,
+  StdCtrls, ACBrEnterTab, ACBrBase;
+
+type
+  TfrProxyConfig = class(TForm)
+    bCancelar: TBitBtn;
+    bOk: TBitBtn;
+    cbAutenticacao: TCheckBox;
+    edPorta: TEdit;
+    edUser: TEdit;
+    edSenha: TEdit;
+    edServidor: TEdit;
+    lSenha1: TLabel;
+    lUser: TLabel;
+    lSenha: TLabel;
+    lUser1: TLabel;
+    Panel2: TPanel;
+    procedure cbAutenticacaoChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+implementation
+
+{$R *.lfm}
+
+procedure TfrProxyConfig.FormShow(Sender: TObject);
+begin
+  cbAutenticacao.Checked := (edUser.Text <> '');
+end;
+
+procedure TfrProxyConfig.cbAutenticacaoChange(Sender: TObject);
+begin
+  edUser.Enabled := cbAutenticacao.Checked;
+  edSenha.Enabled := cbAutenticacao.Checked;
+end;
+
+end.

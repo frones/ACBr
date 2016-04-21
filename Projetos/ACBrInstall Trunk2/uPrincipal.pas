6f18335922b9e220eb9a5c0f1fb9a3853fce42d6
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-04-21 01:32:23 +0000

    - Acerto para listar o Berlin na instalação
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11678 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrInstall Trunk2/uPrincipal.pas
+++ Projetos/ACBrInstall Trunk2/uPrincipal.pas
@@ -788,7 +788,7 @@ begin
      if VersionNumberStr = 'd16' then
         Sender.Options.Add('-NSData.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell;System;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win');
 
-     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23']) then
+     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23','d24']) then
         Sender.Options.Add('-NSWinapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;System;Xml;Data;Datasnap;Web;Soap;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell');
 
   end;

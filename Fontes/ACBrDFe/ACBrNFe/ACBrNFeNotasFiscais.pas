5fb8a7621c7462810edc04385ee8d2e48eb3a2c5
cmjunior <cmjunior@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-05-05 18:42:44 +0000

    Correção sugerida pelo usuário do fórum, russocorp, link do tópico abaixo. http://www.projetoacbr.com.br/forum/topic/29815-loadfromfile-sempre-retorna-true/#comment-197152
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@11746 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFe/ACBrNFeNotasFiscais.pas
+++ Fontes/ACBrDFe/ACBrNFe/ACBrNFeNotasFiscais.pas
@@ -1533,13 +1533,14 @@ begin
 
   // Converte de UTF8 para a String nativa da IDE //
   XMLStr := DecodeToString(XMLUTF8, True);
-  LoadFromString(XMLStr, AGerarNFe);
+  Result := LoadFromString(XMLStr, AGerarNFe);
 
-  // Atribui Nome do arquivo a novas notas inseridas //
-  for i := l to Self.Count - 1 do
-    Self.Items[i].NomeArq := CaminhoArquivo;
-
-  Result := True;
+  if Result then
+  begin
+    // Atribui Nome do arquivo a novas notas inseridas //
+    for i := l to Self.Count - 1 do
+      Self.Items[i].NomeArq := CaminhoArquivo;
+  end;
 end;
 
 function TNotasFiscais.LoadFromStream(AStream: TStringStream;

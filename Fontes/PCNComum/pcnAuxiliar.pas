1c0c141ca37613e745d3fb9c3467fa41a90fd104
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-08-03 13:44:36 +0000

    03/08/2016 -- pcnAuxiliar -- [*] Incluido o caracter "." no final das constantes UFS e CODIGOS da function CodigoParaUF.     por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12082 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/PCNComum/pcnAuxiliar.pas
+++ Fontes/PCNComum/pcnAuxiliar.pas
@@ -135,8 +135,8 @@ uses
 
 function CodigoParaUF(const codigo: integer): string;
 const
-  (**)UFS = '.AC.AL.AP.AM.BA.CE.DF.ES.GO.MA.MT.MS.MG.PA.PB.PR.PE.PI.RJ.RN.RS.RO.RR.SC.SP.SE.TO.DF.DF';
-  CODIGOS = '.12.27.16.13.29.23.53.32.52.21.51.50.31.15.25.41.26.22.33.24.43.11.14.42.35.28.17.90.91';
+  (**)UFS = '.AC.AL.AP.AM.BA.CE.DF.ES.GO.MA.MT.MS.MG.PA.PB.PR.PE.PI.RJ.RN.RS.RO.RR.SC.SP.SE.TO.DF.DF.';
+  CODIGOS = '.12.27.16.13.29.23.53.32.52.21.51.50.31.15.25.41.26.22.33.24.43.11.14.42.35.28.17.90.91.';
 begin
   try
     result := copy(UFS, pos('.' + IntToStr(Codigo) + '.', CODIGOS) + 1, 2);

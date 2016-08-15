886a77d48f7813be087271b067872b905bed6d68
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-08-15 15:35:25 +0000

    Ao gerar a Aliquota para o provedor RJ a mesma tem que ser dividida por 100. Alteração realizada. Por: Rodrigo de Souza Moreira
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12127 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeW_ABRASFv1.pas
+++ Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeW_ABRASFv1.pas
@@ -286,6 +286,7 @@ begin
     proSimplISS: Gerador.wCampoNFSe(tcDe2, '#25', 'Aliquota', 01, 05, 0, NFSe.Servico.Valores.Aliquota, '');
 
     proGINFES,
+    proRJ,
     proPublica:   Gerador.wCampoNFSe(tcDe4, '#25', 'Aliquota', 01, 05, 0, (NFSe.Servico.Valores.Aliquota / 100), '');
 
     proGovBR,

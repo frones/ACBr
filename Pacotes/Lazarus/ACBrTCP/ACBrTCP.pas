17c19107952d15366c85eef4f2b22749ceb1e7e3
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-11-02 21:08:40 +0000

    Ajustes para separação de ACBrMTER de ACBrTCP
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12527 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrTCP/ACBrTCP.pas
+++ Pacotes/Lazarus/ACBrTCP/ACBrTCP.pas
@@ -10,7 +10,6 @@ uses
   ACBrSocket, ACBrCEP, ACBrTCPReg, ACBrIBGE, ACBrCNIEE, ACBrSuframa, 
   ACBrDownload, ACBrDownloadClass, ACBrNFPws, ACBrConsultaCNPJ, ACBrIBPTax, 
   ACBrCotacao, ACBrMail, ACBrConsultaCPF, ACBrSpedTabelas, ACBrSedex, 
-  ACBrMTer, ACBrMTerClass, ACBrMTerVT100, ACBrMTerStxEtx, ACBrMTerPMTG, 
   LazarusPackageIntf;
 
 implementation

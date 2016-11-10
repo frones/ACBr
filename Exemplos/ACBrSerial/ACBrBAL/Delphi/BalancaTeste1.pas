625db338b581716c05a5f04f1e43e122c86e101b
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2016-11-10 11:31:23 +0000

    Aplicado modificações no demo do ACBrBAL http://www.projetoacbr.com.br/forum/topic/33301-corre%C3%A7%C3%A3o-no-exemplo-do-acbrbal/ usei o demo em lazarus como base
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12567 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrSerial/ACBrBAL/Delphi/BalancaTeste1.pas
+++ Exemplos/ACBrSerial/ACBrBAL/Delphi/BalancaTeste1.pas
@@ -1,22 +1,30 @@
-{$I ..\..\Fontes\ACBrComum\ACBr.inc}
-
 unit BalancaTeste1;
 
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ExtCtrls, StdCtrls, ACBrBase, ACBrBAL, ACBrDevice;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Buttons, ExtCtrls, ACBrDevice, ACBrBAL, ACBrBase;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
     ACBrBAL1: TACBrBAL;
+    btnConectar: TButton;
+    btnDesconectar: TButton;
+    btnLerPeso: TButton;
+    edLog: TEdit;
+    Label12: TLabel;
+    SbArqLog: TSpeedButton;
     sttPeso: TStaticText;
     sttResposta: TStaticText;
     Label2: TLabel;
     Label3: TLabel;
     edtTimeOut: TEdit;
     Label9: TLabel;
+    chbMonitorar: TCheckBox;
     Label10: TLabel;
     Memo1: TMemo;
     Panel1: TPanel;
@@ -34,40 +42,31 @@ type
     cmbParity: TComboBox;
     Label11: TLabel;
     cmbStopBits: TComboBox;
-    Panel2: TPanel;
-    btnConectar: TButton;
-    btnDesconectar: TButton;
-    btnLerPeso: TButton;
-    chbMonitorar: TCheckBox;
-    btnLimpar: TButton;
-    btnSair: TButton;
     procedure btnConectarClick(Sender: TObject);
     procedure btnLerPesoClick(Sender: TObject);
-    procedure btnSairClick(Sender: TObject);
     procedure btnDesconectarClick(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure edtTimeOutKeyPress(Sender: TObject; var Key: Char);
     procedure chbMonitorarClick(Sender: TObject);
-    procedure ACBrBAL1LePeso(Peso: Double; Resposta: String);
-    procedure btnLimparClick(Sender: TObject);
+    procedure FormCreate(Sender : TObject) ;
+    procedure SbArqLogClick(Sender: TObject);
+    procedure ACBrBAL1LePeso(Peso: Double; Resposta: AnsiString);
   private
-    { Private declarations }
-
-    FRespostaList: TStringList;
-
+    { private declarations }
     Function Converte( cmd : String) : String;
-
   public
-    { Public declarations }
-  end;
+    { public declarations }
+  end; 
 
 var
-  Form1: TForm1;
+  Form1: TForm1; 
 
 implementation
 
 {$R *.dfm}
 
+Uses
+  typinfo, ACBrUtil ;
 
 function TForm1.Converte(cmd: String): String;
 var A : Integer ;
@@ -78,11 +77,33 @@ begin
      if not (cmd[A] in ['A'..'Z','a'..'z','0'..'9',
                         ' ','.',',','/','?','<','>',';',':',']','[','{','}',
                         '\','|','=','+','-','_',')','(','*','&','^','%','$',
-                        '#','@','!','~',']' ]) then
+                        '#','@','!','~' ]) then
         Result := Result + '#' + IntToStr(ord( cmd[A] )) + ' '
      else
         Result := Result + cmd[A] + ' ';
   end ;
+end;
+
+procedure TForm1.ACBrBAL1LePeso(Peso: Double; Resposta: AnsiString);
+var valid : integer;
+begin
+   sttPeso.Caption     := formatFloat('##0.000', Peso );
+   sttResposta.Caption := Converte( Resposta ) ;
+
+   if Peso > 0 then
+      Memo1.Lines.Text := 'Leitura OK !'
+   else
+    begin
+      valid := Trunc(ACBrBAL1.UltimoPesoLido);
+      case valid of
+         0 : Memo1.Lines.Text := 'TimeOut !'+sLineBreak+
+                                 'Coloque o produto sobre a Balança!' ;
+        -1 : Memo1.Lines.Text := 'Peso Instavel ! ' +sLineBreak+
+                                 'Tente Nova Leitura' ;
+        -2 : Memo1.Lines.Text := 'Peso Negativo !' ;
+       -10 : Memo1.Lines.Text := 'Sobrepeso !' ;
+      end;
+    end ;
 end;
 
 procedure TForm1.btnConectarClick(Sender: TObject);
@@ -99,6 +120,7 @@ begin
    ACBrBAL1.Device.Data      := StrToInt( cmbDataBits.text );
    ACBrBAL1.Device.Baud      := StrToInt( cmbBaudRate.Text );
    ACBrBAL1.Device.Porta     := cmbPortaSerial.Text;
+   ACBrBAL1.ArqLOG           := edLog.Text;
 
    // Conecta com a balança
    ACBrBAL1.Ativar;
@@ -121,11 +143,6 @@ begin
    ACBrBAL1.LePeso( TimeOut );
 end;
 
-procedure TForm1.btnSairClick(Sender: TObject);
-begin
-   close;
-end;
-
 procedure TForm1.btnDesconectarClick(Sender: TObject);
 begin
   ACBrBAL1.Desativar;
@@ -136,18 +153,6 @@ begin
   btnLerPeso.Enabled     := False;
 end;
 
-procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
-  ACBrBAL1.Desativar;
-
-  // Salvar
-  if Assigned(FRespostaList) then
-  begin
-    FRespostaList.SaveToFile(ChangeFileExt(ParamStr(0), '.log'));
-    FRespostaList.Free;
-  end;
-
-end;
 
 procedure TForm1.edtTimeOutKeyPress(Sender: TObject; var Key: Char);
 begin
@@ -160,42 +165,25 @@ begin
    ACBrBAL1.MonitorarBalanca := chbMonitorar.Checked ;
 end;
 
-procedure TForm1.ACBrBAL1LePeso(Peso: Double; Resposta: String);
-var valid : integer;
+procedure TForm1.FormCreate(Sender : TObject) ;
+var
+  I : TACBrBALModelo ;
 begin
-  // Objeto para armazenar as leituras
-  if (not Assigned(FRespostaList)) then
-    FRespostaList := TStringList.Create;
-
-  if FRespostaList.Count > 0 then
-    FRespostaList.Add(StringOfChar('-', 80));
-
-   sttPeso.Caption     := formatFloat('######0.000', Peso );
-   sttResposta.Caption := Converte( Resposta ) ;
-
-   // Acrescentar resposta
-   FRespostaList.Add(sttResposta.Caption);
-
-   if Peso > 0 then
-      Memo1.Lines.Text := 'Leitura OK !'
-   else
-    begin
-      valid := Trunc(ACBrBAL1.UltimoPesoLido);
-      case valid of
-         0 : Memo1.Lines.Text := 'TimeOut !'+sLineBreak+
-                                 'Coloque o produto sobre a Balança!' ;
-        -1 : Memo1.Lines.Text := 'Peso Instavel ! ' +sLineBreak+
-                                 'Tente Nova Leitura' ;
-        -2 : Memo1.Lines.Text := 'Peso Negativo !' ;
-       -10 : Memo1.Lines.Text := 'Sobrepeso !' ;
-      end;
-    end ;
+  cmbBalanca.Items.Clear ;
+  For I := Low(TACBrBALModelo) to High(TACBrBALModelo) do
+     cmbBalanca.Items.Add( GetEnumName(TypeInfo(TACBrBALModelo), integer(I) ) ) ;
+  cmbBalanca.ItemIndex := 0;
 end;
 
-procedure TForm1.btnLimparClick(Sender: TObject);
+procedure TForm1.SbArqLogClick(Sender: TObject);
 begin
-  sttPeso.Caption := '';
-  sttResposta.Caption := '';
+  OpenURL( ExtractFilePath( Application.ExeName ) + edLog.Text);
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  ACBrBAL1.Desativar ;
 end;
 
 end.
+

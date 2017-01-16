a5fff428266160c4a49c9f76a913e597ccab9e6a
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2017-01-16 22:27:34 +0000

    Correção de bug na geração do registro e120 e código do município http://www.projetoacbr.com.br/forum/topic/34625-ajuda-erro-no-bloco-e-erro-registroe120new/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12824 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSEF2/ACBrSEF2_BlocoE_1.pas
+++ Fontes/ACBrTXT/ACBrSEF2/ACBrSEF2_BlocoE_1.pas
@@ -619,7 +619,7 @@ begin
                LFill(IndOperToStr(IND_OPER))                     +
                LFill(IndEmissaoToStr(IND_EMIT))                  +
                LFill(COD_PART)                                   +
-               LFill(COD_MUN_SERV)                               +
+               LFill(COD_MUN_SERV,0)                               +
                LFill(ModDocumentoToStr(COD_MOD))                 +
                LFill(CodSituacaoToStr(COD_SIT),2)                +
                LFill(SER)                                        +

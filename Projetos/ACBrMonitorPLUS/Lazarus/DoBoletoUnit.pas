b8e65e6bb9b3dd02cc8e636ef7d53260fe41757c
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2017-02-22 17:09:11 +0000

    Inclusão da leitura da tag DataLImitePagto na DoBoletoUnit.pas
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@12950 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrMonitorPLUS/Lazarus/DoBoletoUnit.pas
+++ Projetos/ACBrMonitorPLUS/Lazarus/DoBoletoUnit.pas
@@ -360,6 +360,7 @@ begin
          DataMoraJuros       := StrToDateDef(Trim(aIni.ReadString(Sessao,'DataMoraJuros','')),0);
          DataProtesto        := StrToDateDef(Trim(aIni.ReadString(Sessao,'DataProtesto','')),0);
          DataBaixa           := StrToDateDef(Trim(aIni.ReadString(Sessao,'DataBaixa','')),0);
+         DataLimitePagto     := StrToDateDef(Trim(aIni.ReadString(Sessao,'DataLimitePagto','')),0);
          LocalPagamento      := IfThen(Trim(LocalPagto) <> '',LocalPagto,LocalPagamento);
          NumeroDocumento     := aIni.ReadString(Sessao,'NumeroDocumento',NumeroDocumento);
          EspecieDoc          := aIni.ReadString(Sessao,'Especie',EspecieDoc);

d2de1bf088cc0873667575d3ae84e579984c4356
cmjunior <cmjunior@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2017-03-21 20:33:28 +0000

    Adicionado formulário e unit de seleção do certificado. Faltou, conforme lembrado em: http://www.projetoacbr.com.br/forum/topic/35792-selecionarcertificadopas/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@13055 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrMonitorPLUS/Lazarus/selecionarcertificado.pas
@@ -0,0 +1,34 @@
+unit SelecionarCertificado;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Grids, ExtCtrls, Buttons, ACBrGIF;
+
+type
+
+  { TfrSelecionarCertificado }
+
+  TfrSelecionarCertificado = class(TForm)
+    BitBtn1: TBitBtn;
+    BitBtn2: TBitBtn;
+    Panel1: TPanel;
+    StringGrid1: TStringGrid;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frSelecionarCertificado: TfrSelecionarCertificado;
+
+implementation
+
+{$R *.lfm}
+
+end.
+

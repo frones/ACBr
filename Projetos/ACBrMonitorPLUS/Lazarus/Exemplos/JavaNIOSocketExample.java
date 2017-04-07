bc5a8fd6b3d2aae33ea36f8799e8145644ac08e5
cmjunior <cmjunior@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2017-04-07 15:01:09 +0000

    - Ajustado o exemplo em Java para ler a resposta até que encontre o caracter de fim de resposta - Modificado também o exemplo para permitir que usuário digite o comando
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@13158 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrMonitorPLUS/Lazarus/Exemplos/JavaNIOSocketExample.java
+++ Projetos/ACBrMonitorPLUS/Lazarus/Exemplos/JavaNIOSocketExample.java
@@ -6,68 +6,89 @@ import java.nio.ByteBuffer;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 
 public class JavaNIOSocketExample {
 
     private static final int BUFFER_SIZE = 1024;
     private static final String END_COMMAND = System.getProperty("line.separator") + "." + System.getProperty("line.separator");
-
-    private static String[] messages = {
-        "BOLETO.ListaBancos",
-        "ACBr.Ativo",
-    };
+    private static final char ETX = 3;
 
     public static void main(String[] args) throws InterruptedException {
+        char c;
+        String comando;
+        Scanner sc = new Scanner(System.in);
+
         logger("Iniciando a conexão com o ACBrMonitorPLUS...");
-        try {
+
+        try {    
             int port = 3434;
             InetAddress inetAddress = InetAddress.getLocalHost();
-            InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, port);
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(inetAddress, port); 
             SocketChannel socketChannelClient = SocketChannel.open(inetSocketAddress);
             socketChannelClient.configureBlocking(false);
-            
-            logger(String.format("Tentando conectar em %s:%d...", inetSocketAddress.getHostName(), inetSocketAddress.getPort()));
 
-            Thread.sleep(100);
-            
-            // Lendo mensagem de boas vindas do ACBrMonitor
-            ByteBuffer buffer_out = ByteBuffer.allocate(1024);
-            int idx = socketChannelClient.read(buffer_out);
-            while (idx > 0 && socketChannelClient.isConnected()) {
-                buffer_out.flip();
-                while (buffer_out.hasRemaining()) {
-                        System.out.print((char) buffer_out.get());
-                }
-                buffer_out.clear();
-                idx = socketChannelClient.read(buffer_out);
-            }                       
-			
-            // Envia comandos e lê as respostas
-            for (String message : messages) {
-                ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-                buffer.put(message.getBytes());
-                buffer.put(END_COMMAND.getBytes());
-                buffer.flip();
-                socketChannelClient.write(buffer);
-                logger(String.format("\nEnviando comando: \"%s\"", message));
-				
-                Thread.sleep(100);                
-                logger("Resposta do ACBrMonitorPLUS:");
-                buffer_out = ByteBuffer.allocate(1024);
-                idx = socketChannelClient.read(buffer_out);
+            try {                      
+                logger(String.format("Tentando conectar em %s:%d...", inetSocketAddress.getHostName(), inetSocketAddress.getPort()));
+
+                Thread.sleep(100);
+                
+                // Lendo mensagem de boas vindas do ACBrMonitor
+                ByteBuffer buffer_out = ByteBuffer.allocate(1024);
+                int idx = socketChannelClient.read(buffer_out);
                 while (idx > 0 && socketChannelClient.isConnected()) {
-                        buffer_out.flip();
-                        while (buffer_out.hasRemaining()) {
-                                System.out.print((char) buffer_out.get());
-                        }
-                        buffer_out.clear();
-                        idx = socketChannelClient.read(buffer_out);
-                }
+                    buffer_out.flip();
+                    while (buffer_out.hasRemaining()) {
+                            System.out.print((char) buffer_out.get());
+                    }
+                    buffer_out.clear();
+                    idx = socketChannelClient.read(buffer_out);
+                }                       
+                
+                // Envia comandos e lê as respostas                            
+                System.out.println("\nDigite o comando e tecle <ENTER> para enviar. Para sair tecle <CTRL+C>\n");
+                
+                do {
+                    System.out.print("=> ");
+                    comando = sc.nextLine();
+                    if (! comando.trim().isEmpty()) {
+                        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+                        buffer.put(comando.getBytes());
+                        buffer.put(END_COMMAND.getBytes());
+                        buffer.flip();
+                        socketChannelClient.write(buffer);
+                        logger(String.format("\n=> Enviando comando: \"%s\"", comando));
+                        
+                        Thread.sleep(100);                
+                        logger("\nResposta do ACBrMonitorPLUS:");
+                        buffer_out = ByteBuffer.allocate(1024);
+                        c = 0;
+
+                        while ( c != ETX ){
+                            idx = socketChannelClient.read(buffer_out);
+                            while (idx > 0 && socketChannelClient.isConnected() ) {
+                                buffer_out.flip();
+                                while (buffer_out.hasRemaining()) {
+                                    c = (char) buffer_out.get();
+                                    System.out.print(c);
+                                }
+                                buffer_out.clear();
+                                idx = socketChannelClient.read(buffer_out);                                
+                            }
+                        }                        
+                    }
+                    System.out.println();
+
+                } while (true);                        
+            } catch (Exception e) {
+                logger(e.getMessage());
+                e.printStackTrace();
+            } finally {
+                logger("\nFechando a conexão...");
+                socketChannelClient.close();
+                sc.close();
             }
-            
-            logger("\nFechando a conexão...");
-            socketChannelClient.close();
-        } catch (Exception e) {
+        } catch (Exception e){
             logger(e.getMessage());
             e.printStackTrace();
         }

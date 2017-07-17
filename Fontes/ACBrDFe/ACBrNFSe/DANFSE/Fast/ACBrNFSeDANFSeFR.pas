ea66141316be917f9b115f0aeed5069b81377208
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2017-07-17 23:10:37 +0000

    Mudança para atender o provedor BHISS http://www.projetoacbr.com.br/forum/topic/37780-correção/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@13597 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSe/DANFSE/Fast/ACBrNFSeDANFSeFR.pas
+++ Fontes/ACBrDFe/ACBrNFSe/DANFSE/Fast/ACBrNFSeDANFSeFR.pas
@@ -948,7 +948,12 @@ begin
         FieldByName('ValorIss').AsFloat               := ValorIss;
         FieldByName('OutrasRetencoes').AsFloat        := OutrasRetencoes;
         FieldByName('BaseCalculo').AsFloat            := BaseCalculo;
-        FieldByName('Aliquota').AsFloat               := Aliquota;
+
+        if provedor = proBhiss then
+          FieldByName('Aliquota').AsFloat               := RoundTo(Aliquota * 100, -2)
+        else
+          FieldByName('Aliquota').AsFloat               := Aliquota;
+
         FieldByName('ValorLiquidoNfse').AsFloat       := ValorLiquidoNfse;
         FieldByName('ValorIssRetido').AsFloat         := ValorIssRetido;
         FieldByName('DescontoCondicionado').AsFloat   := DescontoCondicionado;

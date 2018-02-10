261428d8efbba1ccb79400c3c0209cd09a22dd69
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-02-10 02:31:43 +0000

    [*] Pacotes atualizados.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@14681 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrDFe/ACBreSocial/ACBre_Social.pas
+++ Pacotes/Lazarus/ACBrDFe/ACBreSocial/ACBre_Social.pas
@@ -12,12 +12,11 @@ uses
   ACBreSocialReg, ACBreSocialWebServices, pcesConversaoeSocial, pcesGerador, 
   pcesIniciais, pcesNaoPeriodicos, pcesPeriodicos, pcesS1000, pcesS1005, 
   pcesS1010, pcesS1020, pcesS1030, pcesS1035, pcesS1040, pcesS1050, pcesS1060, 
-  pcesS1070, pcesS1080, pcesS1200, pcesS1202, pcesS1207, pcesS1210, pcesS1220, 
-  pcesS1250, pcesS1260, pcesS1270, pcesS1280, pcesS1295, pcesS1298, pcesS1299, 
-  pcesS1300, pcesS2100, pcesS2190, pcesS2200, pcesS2205, pcesS2206, pcesS2210, 
-  pcesS2220, pcesS2230, pcesS2240, pcesS2241, pcesS2250, pcesS2298, pcesS2299, 
-  pcesS2300, pcesS2305, pcesS2306, pcesS2399, pcesS2400, pcesS3000, pcesS4000, 
-  pcesS4999, pcesS5001, pcesTabelas, LazarusPackageIntf;
+  pcesS1070, pcesS1080, pcesS1200, pcesS1202, pcesS1207, pcesS1210, pcesS1250, 
+  pcesS1260, pcesS1270, pcesS1280, pcesS1295, pcesS1298, pcesS1299, pcesS1300, 
+  pcesS2190, pcesS2200, pcesS2205, pcesS2206, pcesS2210, pcesS2220, pcesS2230, 
+  pcesS2240, pcesS2241, pcesS2250, pcesS2298, pcesS2299, pcesS2300, pcesS2306, 
+  pcesS2399, pcesS2400, pcesS3000, pcesS5001, pcesTabelas, LazarusPackageIntf;
 
 implementation
 

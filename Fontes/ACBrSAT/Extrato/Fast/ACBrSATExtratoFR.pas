ea7620b7cae85b5d6bacc1f35edc0662527b0399
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-03-01 11:41:01 +0000

    Inicio do código para Extrato SAT em Fast Report. Aguardar a continuação para depois poder instalar.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@14778 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Fontes/ACBrSAT/Extrato/Fast/ACBrSATExtratoFR.pas
@@ -0,0 +1,85 @@
+{******************************************************************************}
+{ Projeto: Componente ACBrSAT                                                  }
+{ Biblioteca multiplataforma de componentes Delphi para emissão Cupom Fiscal   }
+{ Eletrônico SAT                                                               }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2018 Juliomar Marchetti                     }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do Projeto ACBr     }
+{ Componentes localizado em http://www.sourceforge.net/projects/acbr           }
+{                                                                              }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{              Praça Anita Costa, 34 - Tatuí - SP - 18270-410                  }
+{                                                                              }
+{******************************************************************************}
+
+{******************************************************************************
+|* Historico
+|*
+|*
+******************************************************************************}
+{$I ACBr.inc}
+
+unit ACBrSATExtratoFR;
+
+interface
+
+uses 
+  Classes, SysUtils, ACBrSATExtratoClass, pcnConversao;
+
+const
+  CACBrSATExtratoFast_Versao = '1.0' ;
+
+type
+
+  { TACBrSATExtratoFortesClass }
+
+  TACBrSATExtratoFortesClass = class( TACBrSATExtratoClass )
+  private
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+  published
+
+  end ;
+
+implementation
+
+{ TACBrSATExtratoFortes }
+
+constructor TACBrSATExtratoFortesClass.Create(AOwner: TComponent);
+begin
+  inherited create( AOwner );
+
+  
+  fpAbout := 'ACBrSATExtratoFast ver: ' + CACBrSATExtratoFast_Versao  ;
+end;
+
+destructor TACBrSATExtratoFortesClass.Destroy;
+begin
+  fMargens.Free;
+
+  inherited Destroy ;
+end;
+
+end.
---
+++ Fontes/ACBrSAT/Extrato/Fast/ACBrSATExtratoFRReg.pas
@@ -0,0 +1,103 @@
+{******************************************************************************}
+{ Projeto: Componente ACBrSAT                                                  }
+{ Biblioteca multiplataforma de componentes Delphi para emissão Cupom Fiscal   }
+{ Eletrônico SAT                                                               }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2018 Juliomar Marchetti                     }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do Projeto ACBr     }
+{ Componentes localizado em http://www.sourceforge.net/projects/acbr           }
+{                                                                              }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{              Praça Anita Costa, 34 - Tatuí - SP - 18270-410                  }
+{                                                                              }
+{******************************************************************************}
+
+{******************************************************************************
+|* Historico
+|*
+|*
+******************************************************************************}
+{$I ACBr.inc}
+
+unit ACBrSATExtratoFRReg;
+
+interface
+
+uses
+  Classes, SysUtils, ACBrSATExtratoClass, ACBrSATExtratoReportClass,
+  pcnCFe, pcnCFeCanc, pcnConversao;
+
+type
+
+  { TACBrSATExtratoFast }
+  {$IFDEF RTL230_UP}
+  [ComponentPlatformsAttribute(pidWin32 or pidWin64)]
+  {$ENDIF RTL230_UP}
+  TACBrSATExtratoFast = class( TACBrSATExtratoReportClass )
+  private
+  protected
+    procedure Imprimir;
+  public
+    procedure ImprimirExtrato(ACFe: TCFe = nil); override;
+    procedure ImprimirExtratoResumido(ACFe : TCFe = nil); override;
+    procedure ImprimirExtratoCancelamento(ACFe : TCFe = nil; ACFeCanc: TCFeCanc = nil); override;
+  end ;
+
+implementation
+
+{$IFNDEF FPC}
+//  {$R ACBrSATExtratoFast.dcr}
+{$ENDIF}
+
+{ TACBrSATExtratoFast }
+
+procedure Register;
+begin
+  RegisterComponents('ACBrSAT',[TACBrSATExtratoFast]);
+end;
+
+procedure TACBrSATExtratoFast.Imprimir;
+begin
+
+end;
+
+procedure TACBrSATExtratoFast.ImprimirExtrato(ACFe: TCFe);
+begin
+  inherited;
+  Imprimir;
+end;
+
+procedure TACBrSATExtratoFast.ImprimirExtratoCancelamento(ACFe: TCFe;
+  ACFeCanc: TCFeCanc);
+begin
+  inherited;
+  Imprimir;
+end;
+
+procedure TACBrSATExtratoFast.ImprimirExtratoResumido(ACFe: TCFe);
+begin
+  inherited;
+  Imprimir;
+end;
+
+end.

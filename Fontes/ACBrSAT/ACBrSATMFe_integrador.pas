33d72a8c651d0ffe57b2166aa5a13080bdc0684a
anfm <anfm@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-03-06 18:16:23 +0000

    Correção para evitar AV no ACBrSAT com Integrador - https://www.projetoacbr.com.br/forum/topic/41873-erro-no-respostafiscal/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@14795 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSAT/ACBrSATMFe_integrador.pas
+++ Fontes/ACBrSAT/ACBrSATMFe_integrador.pas
@@ -337,7 +337,10 @@ begin
   try
     Result := FIntegrador.EnviarPagamento(Pagamento);
   finally
-    TACBrSAT(Owner).FinalizaComando( Result.XML );
+    if Assigned(Result) then
+      TACBrSAT(Owner).FinalizaComando(Result.XML)
+    else
+      TACBrSAT(Owner).FinalizaComando('');
   end;
 end;
 
@@ -351,7 +354,10 @@ begin
   try
     Result := FIntegrador.EnviarStatusPagamento(StatusPagamento);
   finally
-    TACBrSAT(Owner).FinalizaComando( Result.XML );
+    if Assigned(Result) then
+      TACBrSAT(Owner).FinalizaComando(Result.XML)
+    else
+      TACBrSAT(Owner).FinalizaComando('');
   end;
 end;
 
@@ -366,7 +372,10 @@ begin
   try
     Result := FIntegrador.VerificarStatusValidador(AVerificarStatusValidador);
   finally
-    TACBrSAT(Owner).FinalizaComando( Result.XML );
+    if Assigned(Result) then
+      TACBrSAT(Owner).FinalizaComando(Result.XML)
+    else
+      TACBrSAT(Owner).FinalizaComando('');
   end;
 end;
 
@@ -380,7 +389,10 @@ begin
   try
     Result := FIntegrador.RespostaFiscal(ARespostaFiscal);
   finally
-    TACBrSAT(Owner).FinalizaComando( Result.XML );
+    if Assigned(Result) then
+      TACBrSAT(Owner).FinalizaComando(Result.XML)
+    else
+      TACBrSAT(Owner).FinalizaComando('');
   end;
 end;
 

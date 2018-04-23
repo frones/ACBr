9c947febeead7b74695c79bafe7e210c69f93003
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-04-23 17:52:40 +0000

    -- ACBrUtil -- [-] Aplicado Workround para BUG em FPC em:     function FormatFloatBr(const AFormat: TFormatMask; const AValue: Extended): String;     https://www.projetoacbr.com.br/forum/topic/42922-cteimprimirdactepdf-falta-ponto/     (por: DSA)
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15029 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrComum/ACBrUtil.pas
+++ Fontes/ACBrComum/ACBrUtil.pas
@@ -4127,6 +4127,11 @@ begin
   end;
 
   Result := FormatFloatBr(AValue, Mask);
+  {$IfDef FPC}
+  // Workround para BUG em FPC
+  if (AValue > 999) and (pos(',', Mask) > 0) and (pos('.', Result) = 0) then
+    Result := FormatFloatBr(AValue);
+  {$EndIf}
 end;
 
 //*****************************************************************************************

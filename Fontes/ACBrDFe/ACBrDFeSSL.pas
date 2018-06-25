b73ee978a87e4f58a06b67ca763855fe0aee53e5
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-06-25 15:27:12 +0000

    https://www.projetoacbr.com.br/forum/topic/44310-erro-12175-usando-wininet-timeoutporthread-false-sugest%C3%A3o-de-corre%C3%A7%C3%A3o/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15375 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrDFeSSL.pas
+++ Fontes/ACBrDFe/ACBrDFeSSL.pas
@@ -1546,6 +1546,8 @@ begin
   if Assigned(FSSLHttpClass) then
     FreeAndNil(FSSLHttpClass);
 
+  FSSLHttpLib := ASSLHttpLib;
+
   case ASSLHttpLib of
     httpWinINet, httpWinHttp:
     begin
@@ -1577,8 +1579,6 @@ begin
   else
     FSSLHttpClass := TDFeSSLHttpClass.Create(Self);
   end;
-
-  FSSLHttpLib := ASSLHttpLib;
 end;
 
 procedure TDFeSSL.SetSSLXmlSignLib(ASSLXmlSignLib: TSSLXmlSignLib);

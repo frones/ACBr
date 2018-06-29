27678b77cda311c9275e5185a10e13362211c38f
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-06-29 17:44:02 +0000

    Adição do número do layout do novo manual dia 26/06/2018 https://www.projetoacbr.com.br/forum/topic/44431-versão-004-do-sped-contribuições/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15393 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSPED/ACBrSPEDPisCofins/ACBrEPCBlocos.pas
+++ Fontes/ACBrTXT/ACBrSPED/ACBrSPEDPisCofins/ACBrEPCBlocos.pas
@@ -57,7 +57,8 @@ type
                  vlVersao100,  // Código 001 - Versão 100 ADE Cofis nº 31/2010 de 01/01/2011
                  vlVersao101,  // Código 002 - Versão 101 ADE Cofis nº 34/2010 de 01/01/2011
                  vlVersao200,  // Código 002 - Versão 200 ADE Cofis nº 20/2012
-                 vlVersao201   // Código 003 - Versão 201 ADE Cofis nº 20/2012 de 14/03/2012
+                 vlVersao201,  // Código 003 - Versão 201 ADE Cofis nº 20/2012 de 14/03/2012
+				 				 vlVersao202   // Código 004
                 );
   TACBrVersaoLeiaute = TACBrCodVer;
 
@@ -1695,6 +1696,9 @@ begin
    else
    if AValue = vlVersao201 then
       Result := '003'
+   else
+   if AValue = vlVersao202 then
+      Result := '004';
 end;
 
 function StrToCodVer(AValue: string): TACBrCodVer;
@@ -1707,8 +1711,11 @@ begin
    else
    if AValue = '003' then
       Result := vlVersao201
+	else
+	if AValue = '004' then
+		Result := vlVersao202
     else
-     raise Exception.Create(format('Valor informado [%s] deve estar entre (001,002 e 003)',[AValue]));
+     raise Exception.Create(format('Valor informado [%s] deve estar entre (001, 002, 003 e 004)',[AValue]));
 end;
 
 function TipoEscritToStr(AValue: TACBrTipoEscrit): string;

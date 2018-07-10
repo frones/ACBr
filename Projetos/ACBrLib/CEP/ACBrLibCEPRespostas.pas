25a8d8e0927db8223de53777270ff18740956f9b
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-07-10 12:32:35 +0000

    10/07/2018 [+] Units que serão utilizadas pela DLL do CEP e pelo ACBrMonitor Plus. Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15419 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/CEP/ACBrLibCEPRespostas.pas
@@ -0,0 +1,86 @@
+﻿{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+
+{ Direitos Autorais Reservados (c) 2018 Daniel Simoes de Almeida               }
+
+{ Colaboradores nesse arquivo: Rafael Teno Dias                                }
+
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/gpl-license.php                           }
+
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{        Rua Cel.Aureliano de Camargo, 973 - Tatuí - SP - 18270-170            }
+
+{******************************************************************************}
+
+{$I ACBr.inc}
+
+unit ACBrLibCEPRespostas;
+
+interface
+
+uses
+  SysUtils, Classes, ACBrLibResposta;
+
+type
+
+  { TLibCEPResposta }
+  TLibCEPResposta = class(TACBrLibResposta)
+  private
+    FBairro: string;
+    FCEP: string;
+    FComplemento: string;
+    FIBGE_Municipio: string;
+    FIBGE_UF: string;
+    FLogradouro: string;
+    FMunicipio: string;
+    FTipo_Logradouro: string;
+    FUF: string;
+  public
+    constructor Create(const ASessao: String; const ATipo: TACBrLibRespostaTipo); reintroduce;
+
+  published
+    property CEP: string read FCEP write FCEP;
+    property Tipo_Logradouro: string read FTipo_Logradouro write FTipo_Logradouro;
+    property Logradouro: string read FLogradouro write FLogradouro;
+    property Complemento: string read FComplemento write FComplemento;
+    property Bairro: string read FBairro write FBairro;
+    property Municipio: string read FMunicipio write FMunicipio;
+    property UF: string read FUF write FUF;
+    property IBGE_Municipio: string read FIBGE_Municipio write FIBGE_Municipio;
+    property IBGE_UF: string read FIBGE_UF write FIBGE_UF;
+  end;
+
+implementation
+
+uses
+  ACBrLibCEPConsts;
+
+{ TLibCEPResposta }
+
+constructor TLibCEPResposta.Create(const ASessao: String;
+  const ATipo: TACBrLibRespostaTipo);
+begin
+  inherited Create(ASessao, ATipo);
+end;
+
+end.
+

dcb7d048c0a6bb31a6846afd7d047f7070cfb03e
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-07-31 13:43:47 +0000

    31/07/2018 [*] Incluido no arquivo ApagarAcbr a mascara para apagar os arquivos do e-Social. Por: Ricardo Voigt
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15494 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- apagarAcbr.bat
+++ apagarAcbr.bat
@@ -1,3 +1,3 @@
 cd\
-del ACBr*.bpl  ACBr*.dcp  ACBr*.dcu PCN*.bpl PCN*.dcp PCN*.dcu SYNA*.bpl SYNA*.dcp SYNA*.dcu pnfs*.dcu pcte*.bpl pcte*.dcp pcte*.dcu pmdfe*.bpl pmdfe*.dcp pmdfe*.dcu /s
+del ACBr*.bpl  ACBr*.dcp  ACBr*.dcu PCN*.bpl PCN*.dcp PCN*.dcu SYNA*.bpl SYNA*.dcp SYNA*.dcu pnfs*.dcu pcte*.bpl pcte*.dcp pcte*.dcu pmdfe*.bpl pmdfe*.dcp pmdfe*.dcu pces*.bpl pces*.dcp pces*.dcu /s
 pause

042b8c3f19fc50f963db86c41c2f31f56e44ea78
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-07-31 00:13:29 +0000

    30/07/2018 [+] ACBrLib para o GNRe. Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15491 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/GNRe/ACBrLibGNReRespostas.pas
@@ -0,0 +1,106 @@
+﻿{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+
+{ Direitos Autorais Reservados (c) 2018 Daniel Simoes de Almeida               }
+
+{ Colaboradores nesse arquivo: Rafael Teno Dias                                }
+
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/gpl-license.php                           }
+
+{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
+{        Rua Cel.Aureliano de Camargo, 973 - Tatuí - SP - 18270-170            }
+
+{******************************************************************************}
+
+{$I ACBr.inc}
+
+unit ACBrLibGNReRespostas;
+
+interface
+
+uses
+  SysUtils, Classes, ACBrLibResposta;
+
+type
+
+  { TLibGNReEnvio }
+  TLibGNReEnvio = class(TACBrLibResposta)
+  private
+    FAmbiente: string;
+    FCodigo: string;
+    FDescricao: string;
+    FProtocolo: string;
+    FRecibo: string;
+  public
+    constructor Create(const ATipo: TACBrLibRespostaTipo); reintroduce;
+
+  published
+    property Ambiente: string read FAmbiente write FAmbiente;
+    property Codigo: string read FCodigo write FCodigo;
+    property Descricao: string read FDescricao write FDescricao;
+    property Recibo: string read FRecibo write FRecibo;
+    property Protocolo: string read FProtocolo write FProtocolo;
+  end;
+
+  { TLibGNReConsulta }
+
+  TLibGNReConsulta = class(TACBrLibResposta)
+  private
+    FAmbiente: string;
+    FCodigo: string;
+    FDescricao: string;
+    FExigeReceita: string;
+    FExigeUfFavorecida: string;
+    FUF: string;
+  public
+    constructor Create(const ATipo: TACBrLibRespostaTipo); reintroduce;
+
+  published
+    property Ambiente: string read FAmbiente write FAmbiente;
+    property Codigo: string read FCodigo write FCodigo;
+    property Descricao: string read FDescricao write FDescricao;
+    property UF: string read FUF write FUF;
+    property ExigeUfFavorecida: string read FExigeUfFavorecida write FExigeUfFavorecida;
+    property ExigeReceita: string read FExigeReceita write FExigeReceita;
+  end;
+
+implementation
+
+uses
+  ACBrLibGNReConsts;
+
+{ TLibGNReConsulta }
+
+constructor TLibGNReConsulta.Create(const ATipo: TACBrLibRespostaTipo);
+begin
+  inherited Create(CSessaoRespConsulta, ATipo);
+end;
+
+{ TLibGNReEnvio }
+
+constructor TLibGNReEnvio.Create(const ATipo: TACBrLibRespostaTipo);
+begin
+  inherited Create(CSessaoRespEnvio, ATipo);
+end;
+
+end.
+

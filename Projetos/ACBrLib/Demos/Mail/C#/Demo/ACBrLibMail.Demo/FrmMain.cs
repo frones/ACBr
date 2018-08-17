61f749467021b98e036f43255fac91351eeac234
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-08-17 03:59:27 +0000

    [+] Adicionado o demo em C# do ACBrLibMail.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15574 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/Mail/C#/Demo/ACBrLibMail.Demo/FrmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ACBrLibMail.Demo
+{
+    public partial class FrmMain : Form
+    {
+        #region Constructors
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            var ret = ACBrMail.MAIL_Inicializar("".ToUTF8(), "".ToUTF8());
+            ACBrMail.CheckResult(ret);
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var ret = ACBrMail.MAIL_Finalizar();
+            ACBrMail.CheckResult(ret);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            var ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Nome".ToUTF8(), txtNome.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Conta".ToUTF8(), txtEmail.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Usuario".ToUTF8(), txtUsuario.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Senha".ToUTF8(), txtSenha.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Servidor".ToUTF8(), txtHost.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "Porta".ToUTF8(), nudPorta.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "SSL".ToUTF8(), Convert.ToInt32(ckbSSL.Checked).ToString().ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravarValor("Email".ToUTF8(), "TLS".ToUTF8(), Convert.ToInt32(ckbTLS.Checked).ToString().ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_ConfigGravar("".ToUTF8());
+            ACBrMail.CheckResult(ret);
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            var ret = ACBrMail.MAIL_AddAddress(txtDestinatario.Text.ToUTF8(), txtDestinatario.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_SetSubject(txtAssunto.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_AddBody(txtBody.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_AddAltBody(txtAltBody.Text.ToUTF8());
+            ACBrMail.CheckResult(ret);
+
+            ret = ACBrMail.MAIL_Send(false);
+            ACBrMail.CheckResult(ret);
+        }
+
+        #endregion Methods
+    }
+}

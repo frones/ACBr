198fff75dbede7cac71f14ef5eef2b9ab32aed89
juniorsantos <juniorsantos@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-10-30 17:55:14 +0000

    30-10-2018 -- ACBrBancoBanestes [*] Correção na geração do arquivo de Remessa CNAB400
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15880 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBancoBanestes.pas
+++ Fontes/ACBrBoleto/ACBrBancoBanestes.pas
@@ -220,7 +220,7 @@ begin
                'REMESSA'                                          + // Literal de Remessa
                '01'                                               + // Código do Tipo de Serviço
                PadRight('COBRANCA', 15 )                          +
-               PadLeft(OnlyNumber(Copy(Trim(Conta),1,11)), 11,'0')+ // Codigo da Empresa no Banco
+               PadLeft(OnlyNumber(Copy(Trim(Conta),2,11)+trim(ContaDigito)), 11,'0')+ // Codigo da Empresa no Banco
                space(9)                                           + // COMPLEMENTO DO REGISTRO
                PadRight(Nome, 30)                                 + // Nome da Empresa
                IntToStrzero(Numero,3)                             +
@@ -334,7 +334,7 @@ begin
         wLinha := '1'                                                         +  // ID Registro
                   ATipoInscricao                                              +  // TIPO INSCRICAO EMPRESA(CNPJ, CPF);
                   PadRight(OnlyNumber(Cedente.CNPJCPF), 14, '0')              +
-                  PadLeft(OnlyNumber(Copy(Trim(Cedente.Conta),1,11)), 11, '0')+ // Codigo da Empresa no Banco
+                  PadLeft(OnlyNumber(Copy(Trim(Cedente.Conta),2,11)+trim(cedente.ContaDigito)), 11, '0')+ // Codigo da Empresa no Banco
                   Space(9)                                                    +
                   PadRight(SeuNumero,25)                                       +  // identificacao da operacao na empresa
                   PadRight(Copy(NossoNumero, 1, 8) +

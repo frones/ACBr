700c30c8d8acc69ae7bc0f567596a7ef7bc7c2e9
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-11-14 11:47:11 +0000

    Aumentar tamanho do campo sistema no danfe https://www.projetoacbr.com.br/forum/topic/47377-sugestão-de-alteração-na-unit-acbrnfedanfefrdmpas-tamanho-do-campo-sistema/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15908 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fast/ACBrNFeDANFEFRDM.pas
+++ Fontes/ACBrDFe/ACBrNFe/DANFE/NFe/Fast/ACBrNFeDANFEFRDM.pas
@@ -1483,7 +1483,7 @@ begin
         FieldDefs.Add('ResumoCanhoto', ftString, 200);
         FieldDefs.Add('Mensagem0', ftString, 60);
         FieldDefs.Add('Imagem', ftString, 256);
-        FieldDefs.Add('Sistema', ftString, 150);
+        FieldDefs.Add('Sistema', ftString, 300);
         FieldDefs.Add('Usuario', ftString, 60);
         FieldDefs.Add('Fax', ftString, 60);
         FieldDefs.Add('Site', ftString, 60);

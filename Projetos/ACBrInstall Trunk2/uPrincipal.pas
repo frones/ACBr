967264b67d436a1c55a92ecc2bba3e1de3be6d54
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-11-22 11:03:56 +0000

    - acerto para o acbrinstall funcionar com RAD Studio/Delphi 10.3 Rio
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@15940 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrInstall Trunk2/uPrincipal.pas
+++ Projetos/ACBrInstall Trunk2/uPrincipal.pas
@@ -820,7 +820,7 @@ begin
      if VersionNumberStr = 'd16' then
         Sender.Options.Add('-NSData.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell;System;Xml;Data;Datasnap;Web;Soap;Winapi;System.Win');
 
-     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23','d24','d25']) then
+     if MatchText(VersionNumberStr, ['d17','d18','d19','d20','d21','d22','d23','d24','d25','d26']) then
         Sender.Options.Add('-NSWinapi;System.Win;Data.Win;Datasnap.Win;Web.Win;Soap.Win;Xml.Win;Bde;System;Xml;Data;Datasnap;Web;Soap;Vcl;Vcl.Imaging;Vcl.Touch;Vcl.Samples;Vcl.Shell');
 
   end;
@@ -888,7 +888,9 @@ begin
     else if oACBr.Installations[iFor].VersionNumberStr = 'd24' then
       edtDelphiVersion.Items.Add('Delphi 10.1 Berlin')
     else if oACBr.Installations[iFor].VersionNumberStr = 'd25' then
-      edtDelphiVersion.Items.Add('Delphi 10.2 Tokyo');
+      edtDelphiVersion.Items.Add('Delphi 10.2 Tokyo')
+    else if oACBr.Installations[iFor].VersionNumberStr = 'd26' then
+      edtDelphiVersion.Items.Add('Delphi 10.3 Rio');
 
     // -- Evento disparado antes de iniciar a execução do processo.
     oACBr.Installations[iFor].DCC32.OnBeforeExecute := BeforeExecute;
@@ -1433,7 +1435,7 @@ begin
   end;
 
   // C++ Builder a partir do D2006, versões anteriores tem IDE independentes.
-  ckbBCB.Enabled := MatchText(oACBr.Installations[iVersion].VersionNumberStr, ['d10','d11','d12','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25']);
+  ckbBCB.Enabled := MatchText(oACBr.Installations[iVersion].VersionNumberStr, ['d10','d11','d12','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26']);
   if not ckbBCB.Enabled then
      ckbBCB.Checked := False;
 end;

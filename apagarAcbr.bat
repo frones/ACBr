b4a61a7d0957d7b75b123cf5072ac28f766a2750
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2018-12-13 12:57:00 +0000

    13/12/2018 [*] Atualizado, pois estavam faltando os componentes novos. Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@16089 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- apagarAcbr.bat
+++ apagarAcbr.bat
@@ -1,3 +1,6 @@
+@echo off
+C:
 cd\
-del ACBr*.bpl  ACBr*.dcp  ACBr*.dcu PCN*.bpl PCN*.dcp PCN*.dcu SYNA*.bpl SYNA*.dcp SYNA*.dcu pnfs*.dcu pcte*.bpl pcte*.dcp pcte*.dcu pmdfe*.bpl pmdfe*.dcp pmdfe*.dcu pces*.bpl pces*.dcp pces*.dcu /s
-pause
+del ACBr*.bpl ACBr*.dcp ACBr*.dcu PCN*.bpl PCN*.dcp PCN*.dcu SYNA*.bpl SYNA*.dcp SYNA*.dcu pnfs*.dcu pcte*.bpl pcte*.dcp pcte*.dcu pmdfe*.bpl pmdfe*.dcp pmdfe*.dcu pgnre*.bpl pgnre*.dcp pgnre*.dcu pces*.bpl pces*.dcp pces*.dcu pca*.bpl pca*.dcp pca*.dcu /s
+
+

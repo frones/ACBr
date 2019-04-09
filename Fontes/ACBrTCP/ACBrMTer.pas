e96d5080ccd17a43a96cae0f88804b5d65a29101
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-04-09 16:02:51 +0000

    Melhorias no Log de ACBrMTer
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@16855 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTCP/ACBrMTer.pas
+++ Fontes/ACBrTCP/ACBrMTer.pas
@@ -296,7 +296,7 @@ type
 implementation
 
 uses
-  strutils, dateutils, math,
+  strutils, dateutils, math, typinfo,
   ACBrMTerVT100, ACBrMTerPMTG, ACBrMTerStxEtx, ACBrMTerSB100,
   ACBrConsts, ACBrUtil;
 
@@ -831,7 +831,11 @@ begin
 
   wEchoMode := EchoMode;
   if Assigned(fOnRecebeDados) then
+  begin
+    GravaLog( '  OnRecebeDados');
     OnRecebeDados(aIP, DadosRecebidos, wEchoMode);
+    GravaLog( '    EchoMode: '+GetEnumName(TypeInfo(TACBrMTerEchoMode), Integer(wEchoMode)));
+  end;
 
   DadosEcho := fMTer.LimparConteudoParaEnviarEcho(DadosRecebidos);
   case wEchoMode of

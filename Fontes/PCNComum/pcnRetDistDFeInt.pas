0e3c0a4db88edb857c5687e827b1d020d45a0c61
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-04-26 16:52:57 +0000

    26/04/2019 Alterado a condição pois alguns DF-e a tag é diferente.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@16957 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/PCNComum/pcnRetDistDFeInt.pas
+++ Fontes/PCNComum/pcnRetDistDFeInt.pas
@@ -586,7 +586,8 @@ begin
               end;
             end;
 
-            if (oLeitorInfZip.rExtrai(2, 'retEvento' + FtpDFe) <> '') then
+            if (oLeitorInfZip.rExtrai(2, 'retEvento') <> '') or
+               (oLeitorInfZip.rExtrai(2, 'retEvento' + FtpDFe) <> '') then
             begin
               FdocZip.Items[i].FprocEvento.RetinfEvento.FId          := oLeitorInfZip.rAtributo('Id');
               FdocZip.Items[i].FprocEvento.RetinfEvento.FtpAmb       := StrToTpAmb(ok, oLeitorInfZip.rCampo(tcStr, 'tpAmb'));

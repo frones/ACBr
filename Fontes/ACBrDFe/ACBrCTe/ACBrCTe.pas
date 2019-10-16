ea3f0c3deb244b5bad48aa809370129a9c7b3b7f
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-10-16 17:29:41 +0000

    16/10/2019 -- ACBrCTe -- [*] Removido a checagem de Forma de emissão ao gerar a string do QR-Code.     Pois parece que as SVC mudaram as regras de checagem de URL do QR-Code.    Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@17942 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrCTe/ACBrCTe.pas
+++ Fontes/ACBrDFe/ACBrCTe/ACBrCTe.pas
@@ -306,16 +306,18 @@ function TACBrCTe.GetURLQRCode(const CUF: integer;
 var
   idCTe, sEntrada, urlUF, Passo2, sign: String;
 //  VersaoDFe: TVersaoCTe;
-//  ok: Boolean;
 begin
 //  VersaoDFe := DblToVersaoCTe(ok, Versao);  // Deixado para usu futuro
-
+{
   if ( (TipoEmissao in [teSVCRS]) and (CUF in [31,41,50,51]) ) then
   begin
      urlUF := LerURLDeParams('CTe', CUFtoUF(CUF), TipoAmbiente, 'URL-QRCode', 0)
   end
   else
      urlUF := LerURLDeParams('CTe', GetUFFormaEmissao, TipoAmbiente, 'URL-QRCode', 0);
+}
+
+  urlUF := LerURLDeParams('CTe', CUFtoUF(CUF), TipoAmbiente, 'URL-QRCode', 0);
 
   if Pos('?', urlUF) <= 0 then
     urlUF := urlUF + '?';

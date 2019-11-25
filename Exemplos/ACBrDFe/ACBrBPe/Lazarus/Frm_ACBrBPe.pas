476435c760364f2ad20c800aff42c4c65ded1be1
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-11-25 20:03:17 +0000

    25/11/2019 Pequenos ajustes na nova versão. Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@18349 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrDFe/ACBrBPe/Lazarus/Frm_ACBrBPe.pas
+++ Exemplos/ACBrDFe/ACBrBPe/Lazarus/Frm_ACBrBPe.pas
@@ -877,7 +877,7 @@ procedure TfrmACBrBPe.btnConsultarChaveClick(Sender: TObject);
 var
   vChave: String;
 begin
-  if not(InputQuery('WebServices Consultar', 'Chave da BP-e:', vChave)) then
+  if not(InputQuery('WebServices Consultar', 'Chave do BP-e:', vChave)) then
     exit;
 
   ACBrBPe1.Bilhetes.Clear;
@@ -939,7 +939,7 @@ begin
   LoadXML(MemoResp, WBResposta);
 
   MemoDados.Lines.Add('');
-  MemoDados.Lines.Add('Envio NFCe');
+  MemoDados.Lines.Add('Envio BPe');
   MemoDados.Lines.Add('tpAmb: ' + TpAmbToStr(ACBrBPe1.WebServices.Enviar.TpAmb));
   MemoDados.Lines.Add('verAplic: ' + ACBrBPe1.WebServices.Enviar.verAplic);
   MemoDados.Lines.Add('cStat: ' + IntToStr(ACBrBPe1.WebServices.Enviar.cStat));
@@ -1011,24 +1011,24 @@ begin
     CC:=TstringList.Create;
 
     try
-      CC.Add('andrefmoraes@gmail.com'); //especifique um email vÃ¡lido
-      CC.Add('anfm@zipmail.com.br');    //especifique um email vÃ¡lido
+      CC.Add('andrefmoraes@gmail.com'); // especifique um email valido
+      CC.Add('anfm@zipmail.com.br');    // especifique um email valido
 
       ACBrMail1.Host := edtSmtpHost.Text;
       ACBrMail1.Port := edtSmtpPort.Text;
       ACBrMail1.Username := edtSmtpUser.Text;
       ACBrMail1.Password := edtSmtpPass.Text;
       ACBrMail1.From := edtSmtpUser.Text;
-      ACBrMail1.SetSSL := cbEmailSSL.Checked; // SSL - ConexÃ£o Segura
+      ACBrMail1.SetSSL := cbEmailSSL.Checked; // SSL - Conexao Segura
       ACBrMail1.SetTLS := cbEmailSSL.Checked; // Auto TLS
-      ACBrMail1.ReadingConfirmation := False; //Pede confirmaÃ§Ã£o de leitura do email
-      ACBrMail1.UseThread := False;           //Aguarda Envio do Email(nÃ£o usa thread)
+      ACBrMail1.ReadingConfirmation := False; // Pede confirmacao de leitura do email
+      ACBrMail1.UseThread := False;           // Aguarda Envio do Email(nao usa thread)
       ACBrMail1.FromName := 'Projeto ACBr - ACBrBPe';
 
       ACBrBPe1.Bilhetes.Items[0].EnviarEmail( Para, edtEmailAssunto.Text,
                                                mmEmailMsg.Lines
                                                , True  // Enviar PDF junto
-                                               , CC    // Lista com emails que serÃ£o enviado cÃ³pias - TStrings
+                                               , CC    // Lista com emails que serao enviado copias - TStrings
                                                , nil); // Lista de anexos - TStrings
     finally
       CC.Free;
@@ -1071,25 +1071,15 @@ begin
     ACBrBPe1.EventoBPe.LerXML(OpenDialog1.FileName);
 
     CC:=TstringList.Create;
-    CC.Add('andrefmoraes@gmail.com'); //especifique um email vÃ¡lido
-    CC.Add('anfm@zipmail.com.br');    //especifique um email vÃ¡lido
-    //TODO:
-    ////ACBrBPe1.EnviarEmailEvento(edtSmtpHost.Text
-    ////                         , edtSmtpPort.Text
-    ////                         , edtSmtpUser.Text
-    ////                         , edtSmtpPass.Text
-    ////                         , edtSmtpUser.Text
-    ////                         , Para
-    ////                         , edtEmailAssunto.Text
-    ////                         , mmEmailMsg.Lines
-    ////                         , cbEmailSSL.Checked // SSL - ConexÃ£o Segura
-    ////                         , True //Enviar PDF junto
-    ////                         , CC //Lista com emails que serÃ£o enviado cÃ³pias - TStrings
-    ////                         , Evento // Lista de anexos - TStrings
-    ////                         , False  //Pede confirmaÃ§Ã£o de leitura do email
-    ////                         , False  //Aguarda Envio do Email(nÃ£o usa thread)
-    ////                         , 'ACBrBPe2' // Nome do Rementente
-    ////                         , cbEmailSSL.Checked ); // Auto TLS
+    CC.Add('andrefmoraes@gmail.com'); // especifique um email valido
+    CC.Add('anfm@zipmail.com.br');    // especifique um email valido
+
+    ACBrBPe1.EnviarEmailEvento(Para, edtEmailAssunto.Text, mmEmailMsg.Lines,
+                               nil, // Lista com emails que serao enviado copias - TStrings
+                               nil, // Lista de anexos - TStrings
+                               nil  // ReplyTo
+                               );
+
     CC.Free;
     Evento.Free;
   end;
@@ -1354,20 +1344,6 @@ begin
   MemoDados.Lines.Add('tMed: '     +IntToStr(ACBrBPe1.WebServices.StatusServico.TMed));
   MemoDados.Lines.Add('dhRetorno: '+DateTimeToStr(ACBrBPe1.WebServices.StatusServico.dhRetorno));
   MemoDados.Lines.Add('xObs: '     +ACBrBPe1.WebServices.StatusServico.xObs);
-  {
-  if (ACBrBPe1.Integrador= ACBrIntegrador1) then
-  begin
-    if (ACBrIntegrador1.ComandoIntegrador.IntegradorResposta.Codigo <> '') then
-    begin
-      MemoDados.Lines.Add('[Integrador]');
-      MemoDados.Lines.Add('Codigo=' + ACBrIntegrador1.ComandoIntegrador.IntegradorResposta.Codigo);
-      MemoDados.Lines.Add('Valor=' + ACBrIntegrador1.ComandoIntegrador.IntegradorResposta.Valor);
-
-      ACBrIntegrador1.ComandoIntegrador.IntegradorResposta.Codigo := '';
-      ACBrIntegrador1.ComandoIntegrador.IntegradorResposta.Valor := '';
-    end;
-  end;
-  }
 end;
 
 procedure TfrmACBrBPe.btnSubNameClick(Sender: TObject);
@@ -1441,7 +1417,7 @@ end;
 
 procedure TfrmACBrBPe.btnValidarXMLClick(Sender: TObject);
 begin
-  OpenDialog1.Title := 'Selecione a BPe';
+  OpenDialog1.Title := 'Selecione o BPe';
   OpenDialog1.DefaultExt := '*-BPe.XML';
   OpenDialog1.Filter := 'Arquivos BPe (*-BPe.XML)|*-BPe.XML|Arquivos XML (*.XML)|*.XML|Todos os Arquivos (*.*)|*.*';
 
@@ -1694,9 +1670,9 @@ begin
 
     StreamMemo.Free;
 
-    Ini.WriteInteger('DABPe', 'Tipo',       rgTipoDaBPe.ItemIndex);
-    Ini.WriteString( 'DABPe', 'LogoMarca',  edtLogoMarca.Text);
-    Ini.WriteInteger('DABPe', 'TipoDANFCE', rgComponenteDABPE.ItemIndex);
+    Ini.WriteInteger('DABPE', 'Tipo',      rgTipoDaBPe.ItemIndex);
+    Ini.WriteString( 'DABPE', 'LogoMarca', edtLogoMarca.Text);
+    Ini.WriteInteger('DABPE', 'TipoDABPE', rgComponenteDABPE.ItemIndex);
 
     INI.WriteInteger('PosPrinter', 'Modelo',            cbxModeloPosPrinter.ItemIndex);
     INI.WriteString( 'PosPrinter', 'Porta',             cbxPorta.Text);
@@ -1825,9 +1801,9 @@ begin
     mmEmailMsg.Lines.LoadFromStream(StreamMemo);
     StreamMemo.Free;
 
-    rgTipoDaBPe.ItemIndex       := Ini.ReadInteger('DABPe', 'Tipo',       0);
-    edtLogoMarca.Text           := Ini.ReadString( 'DABPe', 'LogoMarca',  '');
-    rgComponenteDABPE.ItemIndex := Ini.ReadInteger('DABPe', 'TipoDANFCE', 0);
+    rgTipoDaBPe.ItemIndex       := Ini.ReadInteger('DABPE', 'Tipo',       0);
+    edtLogoMarca.Text           := Ini.ReadString( 'DABPE', 'LogoMarca',  '');
+    rgComponenteDABPE.ItemIndex := Ini.ReadInteger('DABPE', 'TipoDABPE', 0);
 
     cbxModeloPosPrinter.ItemIndex := INI.ReadInteger('PosPrinter', 'Modelo',            Integer(ACBrPosPrinter1.Modelo));
     cbxPorta.Text                 := INI.ReadString( 'PosPrinter', 'Porta',             ACBrPosPrinter1.Porta);
@@ -1853,10 +1829,7 @@ begin
   ACBrBPe1.Configuracoes.Certificados.Senha       := edtSenha.Text;
   ACBrBPe1.Configuracoes.Certificados.NumeroSerie := edtNumSerie.Text;
 
-//  if rgDANFCE.ItemIndex = 0 then
-//    ACBrBPe1.DABPe := ACBrBPeDANFCeFortes1
-//  else
-    ACBrBPe1.DABPe := ACBrBPeDABPeESCPOS1;
+  ACBrBPe1.DABPe := ACBrBPeDABPeESCPOS1;
 
   ACBrBPe1.SSL.DescarregarCertificado;
 

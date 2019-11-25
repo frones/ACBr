df0224c8aa1ad1cfcaf9e9fe2e71ef1d4097ed3e
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-11-25 21:34:19 +0000

    Otimizações de Waldir Paim
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@18358 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSPED/ACBrSped.pas
+++ Fontes/ACBrTXT/ACBrSPED/ACBrSped.pas
@@ -3,7 +3,8 @@
 {  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
 { mentos de Automação Comercial utilizados no Brasil                           }
 {                                                                              }
-{ Direitos Autorais Reservados (c) 2015   Isaque Pinheiro                      }
+{ Direitos Autorais Reservados (c) 2015 Isaque Pinheiro,                       }
+{                                       Daniel Simoes de Almeida               }
 {                                                                              }
 { Colaboradores nesse arquivo:                                                 }
 {                                                                              }
@@ -26,9 +27,8 @@
 { Você também pode obter uma copia da licença em:                              }
 { http://www.opensource.org/licenses/lgpl-license.php                          }
 {                                                                              }
-{ Daniel Simões de Almeida  -  daniel@djsystem.com.br  -  www.djsystem.com.br  }
-{              Praça Anita Costa, 34 - Tatuí - SP - 18270-410                  }
-{                                                                              }
+{ Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
+{       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
 {******************************************************************************}
 
 {******************************************************************************
@@ -43,7 +43,7 @@ unit ACBrSped;
 interface
 
 uses
-  SysUtils, Classes, DateUtils, ACBrBase, ACBrTXTClass;
+  SysUtils, Classes, DateUtils, ACBrTXTClass;
 
 type
   TWriteRegistroEvent = procedure(var ALinha: String) of object;

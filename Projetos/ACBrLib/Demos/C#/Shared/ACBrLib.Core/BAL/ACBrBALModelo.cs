fde719c1ae39931f3765d598bfad2a2b37784492
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2019-11-28 21:19:49 +0000

    [+] Enviado arquivo faltante para o SVN.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@18383 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/BAL/ACBrBALModelo.cs
@@ -0,0 +1,34 @@
+﻿namespace ACBrLib.Core.BAL
+{
+    public enum ACBrBALModelo
+    {
+        balNenhum,
+        balFilizola,
+        balToledo,
+        balToledo2090,
+        balToledo2180,
+        balUrano,
+        balLucasTec,
+        balMagna,
+        balDigitron,
+        balMagellan,
+        balUranoPOP,
+        balLider,
+        balRinnert,
+        balMuller,
+        balSaturno,
+        balAFTS,
+        balGenerica,
+        balLibratek,
+        balMicheletti,
+        balAlfa,
+        balToledo9091_8530_8540,
+        balWeightechWT1000,
+        balMarelCG62XL,
+        balWeightechWT3000_ABS,
+        balToledo2090N,
+        balToledoBCS21,
+        balPrecision,
+        balDigitron_UL
+    }
+}

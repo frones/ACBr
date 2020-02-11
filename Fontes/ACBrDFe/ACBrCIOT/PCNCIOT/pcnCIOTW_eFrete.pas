16d1cd48f85eea82c749659f9419876df149fada
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-02-11 19:36:26 +0000

    11/02/2020 -- pcnCIOTW_eFrete -- [*] Imposta a condição para gerar a tag <FreteRetorno> somente     quando não for Tac-Agregado.    Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@19084 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrCIOT/PCNCIOT/pcnCIOTW_eFrete.pas
+++ Fontes/ACBrDFe/ACBrCIOT/PCNCIOT/pcnCIOTW_eFrete.pas
@@ -1771,9 +1771,9 @@ begin
             Gerador.wCampoNFSe(tcInt, 'AP258', 'CodigoTipoCarga        ', 01, 01, 1, CodigoTipoCarga);
             Gerador.wCampoNFSe(tcBoolStr, 'AP259', 'AltoDesempenho     ', 01, 01, 1, LowerCase(BoolToStr(AltoDesempenho, True)));
             Gerador.wCampoNFSe(tcBoolStr, 'AP260', 'DestinacaoComercial', 01, 01, 1, LowerCase(BoolToStr(DestinacaoComercial, True)));
+            Gerador.wCampoNFSe(tcBoolStr, 'AP261', 'FreteRetorno       ', 01, 01, 1, LowerCase(BoolToStr(FreteRetorno, True)));
           end;
 
-          Gerador.wCampoNFSe(tcBoolStr, 'AP261', 'FreteRetorno', 01, 01, 1, LowerCase(BoolToStr(FreteRetorno, True)));
           Gerador.wCampoNFSe(tcStr, 'AP262', 'CepRetorno      ', 01, 01, 0, CepRetorno);
           Gerador.wCampoNFSe(tcInt, 'AP263', 'DistanciaRetorno', 01, 01, 1, DistanciaRetorno);
         end;

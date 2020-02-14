eb00de397dc6439c3870b22a681126f171ee2011
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-02-14 14:40:12 +0000

    14/02/2020 -- pcnCIOTW_eFrete -- [-] Corrigido o nome da classe usada para buscar os dados de pagamento     do encerramento.    Por: Italo Jurisato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@19114 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrCIOT/PCNCIOT/pcnCIOT.pas
+++ Fontes/ACBrDFe/ACBrCIOT/PCNCIOT/pcnCIOT.pas
@@ -1,3 +1,35 @@
+{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2020 Daniel Simoes de Almeida               }
+{                                                                              }
+{ Colaboradores nesse arquivo: Italo Jurisato Junior                           }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
+{       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
+{******************************************************************************}
+
 {$I ACBr.inc}
 
 unit pcnCIOT;
@@ -5,10 +37,16 @@ unit pcnCIOT;
 interface
 
 uses
-  SysUtils, Classes, Contnrs,
+  SysUtils, Classes,
 {$IFNDEF VER130}
   Variants,
 {$ENDIF}
+  {$IF DEFINED(NEXTGEN)}
+   System.Generics.Collections, System.Generics.Defaults,
+  {$ELSEIF DEFINED(DELPHICOMPILER16_UP)}
+   System.Contnrs,
+  {$IfEnd}
+  ACBrBase,
   pcnConversao, pcnConversaoCIOT, ACBrUtil;
 
 type
@@ -212,7 +250,7 @@ type
     property CnpjFilialAbastecimento: string read FCnpjFilialAbastecimento write FCnpjFilialAbastecimento;
   end;
 
-  TPagamentoCollection = class(TObjectList)
+  TPagamentoCollection = class(TACBrObjectList)
   private
     function GetItem(Index: Integer): TPagamentoCollectionItem;
     procedure SetItem(Index: Integer; Value: TPagamentoCollectionItem);
@@ -232,7 +270,7 @@ type
     property Placa: String read FPlaca write SetPlaca;
   end;
 
-  TVeiculoCollection = class(TObjectList)
+  TVeiculoCollection = class(TACBrObjectList)
   private
     function GetItem(Index: Integer): TVeiculoCollectionItem;
     procedure SetItem(Index: Integer; Value: TVeiculoCollectionItem);
@@ -366,7 +404,7 @@ type
     property DiferencaDeFrete: TDiferencaDeFrete read FDiferencaDeFrete write FDiferencaDeFrete;
   end;
 
-  TNotaFiscalCollection = class(TObjectList)
+  TNotaFiscalCollection = class(TACBrObjectList)
   private
     function GetItem(Index: Integer): TNotaFiscalCollectionItem;
     procedure SetItem(Index: Integer; Value: TNotaFiscalCollectionItem);
@@ -406,7 +444,7 @@ type
     property NotasFiscais: TNotaFiscalCollection read FNotasFiscais write SetNotasFiscais;
   end;
 
-  TViagemCollection = class(TObjectList)
+  TViagemCollection = class(TACBrObjectList)
   private
     function GetItem(Index: Integer): TViagemCollectionItem;
     procedure SetItem(Index: Integer; Value: TViagemCollectionItem);
@@ -528,7 +566,7 @@ type
     property Mensagem: string read FMensagem write FMensagem;
   end;
 
-  TMensagemCollection = class(TObjectList)
+  TMensagemCollection = class(TACBrObjectList)
   private
     function GetItem(Index: Integer): TMensagemCollectionItem;
     procedure SetItem(Index: Integer; Value: TMensagemCollectionItem);

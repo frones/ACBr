ff902e3760227fbc4ffdb2ef2537d396820d97cf
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-03-03 20:44:39 +0000

    Correção de Erro de compilação...
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@19312 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSEF2/ACBrSEF2Conversao.pas
+++ Fontes/ACBrTXT/ACBrSEF2/ACBrSEF2Conversao.pas
@@ -29,7 +29,7 @@
 { Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
 {       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
 {******************************************************************************}
-*******************************************************************************}
+
 {$I ACBr.inc}
 
 unit ACBrSEF2Conversao;

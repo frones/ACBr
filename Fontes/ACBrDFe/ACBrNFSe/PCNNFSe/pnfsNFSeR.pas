ce2ff49607bd654befb22e774a59f2eae34addd3
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-05-08 15:37:28 +0000

    08/05/2020 -- pnfsNFSeR -- [*] https://www.projetoacbr.com.br/forum/topic/57476-nfse-saatri-iss-retido-consulta-impressão-xml/   Por: Theiller
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@19813 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeR.pas
+++ Fontes/ACBrDFe/ACBrNFSe/PCNNFSe/pnfsNFSeR.pas
@@ -2307,7 +2307,8 @@ begin
 
         if (FProvedor in [proActconv202, proISSe, proVersaTecnologia, proNEAInformatica,
                           proFiorilli, proPronimv2, proVitoria, proSmarAPDABRASF,
-                          proGovDigital, proDataSmart, proTecnos, proRLZ, proSigCorp]) then
+                          proGovDigital, proDataSmart, proTecnos, proRLZ, proSigCorp,
+						  proSaatri]) then
         begin
           if NFSe.Servico.Valores.IssRetido = stRetencao then
             NFSe.Servico.Valores.ValorIssRetido := Leitor.rCampo(tcDe2, 'ValorIss')

b7066b1c55308811f4aa8538a17447e354b0cb87
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-05-18 16:54:28 +0000

    -- ACBrEnterTab -- [*] Ajustes para compatabilização com FMX     (por: DSA)
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@19914 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDiversos/ACBrEnterTab.pas
+++ Fontes/ACBrDiversos/ACBrEnterTab.pas
@@ -30,15 +30,6 @@
 {       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
 {******************************************************************************}
 
-{******************************************************************************
-|* Historico
-|*
-|* 16/10/2004: Primeira Versao
-|*    Daniel Simoes de Almeida
-|*
-|* Implementação desta classe com estudo da JvEnterTab.PAS http://jvcl.sourceforge.net
-******************************************************************************}
-
 {$I ACBr.inc}
 
 unit ACBrEnterTab;
@@ -48,144 +39,227 @@ interface
 uses
  ACBrBase,
  Classes,
- {$IFDEF VisualCLX}
+ {$IF DEFINED(VisualCLX)}
   QControls, QForms, QStdCtrls,
+ {$ELSEIF DEFINED(FMX)}
+  FMX.Controls, FMX.Forms, FMX.StdCtrls, FMX.Types, System.UITypes,
+ {$ELSEIF DEFINED(DELPHICOMPILER16_UP)}
+  Vcl.Controls, Vcl.Forms, Vcl.StdCtrls,
  {$ELSE}
   Controls, Forms, StdCtrls,
- {$ENDIF}
- SysUtils ;
+ {$IFEND}
+ SysUtils;
 
 
 type
   THackForm = class(TForm);
-
-  THackButtomControl = class(TButtonControl);
-
+  {$IfDef FMX}
+   THackButtomControl = class(TCustomButton);
+  {$Else}
+   THackButtomControl = class(TButtonControl);
+  {$EndIf}
 
   { TACBrEnterTab }
 
   {$IFDEF RTL230_UP}
-  [ComponentPlatformsAttribute(piacbrAllPlatforms)]
+  [ComponentPlatformsAttribute(piacbrAllDesktopPlatforms)]
   {$ENDIF RTL230_UP}
   TACBrEnterTab = class ( TACBrComponent )
   private
     FAllowDefault: Boolean;
     FEnterAsTab: Boolean;
-    FOldKeyPreview : Boolean ;
-    FOldOnKeyPress : TKeyPressEvent ;
-    FUseScreenControl: Boolean;
+    FFormOwner: TForm;
+    {$IfDef FMX}
+     FOldOnKeyDown: TKeyEvent;
+    {$Else}
+     FOldKeyPreview : Boolean;
+     FOldOnKeyPress : TKeyPressEvent ;
+     FUseScreenControl: Boolean;
+    {$EndIf}
     procedure SetEnterAsTab(const Value: Boolean);
   public
-    constructor Create(AOwner: TComponent ); override ;
+    constructor Create(AOwner: TComponent); override ;
     destructor Destroy ; override ;
 
-    procedure DoEnterAsTab(AForm : TObject; var Key: Char);
+    {$IfDef FMX}
+     procedure DoKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
+     property EnterAsTab: Boolean read FEnterAsTab write SetEnterAsTab default false ;
+    {$Else}
+     procedure DoEnterAsTab(AForm : TObject; var Key: Char);
+    {$EndIf}
   published
-    property EnterAsTab: Boolean read FEnterAsTab write SetEnterAsTab
-       default false ;
-    property AllowDefault: Boolean read FAllowDefault write FAllowDefault
-       default true ;
-    property UseScreenControl: Boolean read FUseScreenControl write FUseScreenControl
-       default {$IfDef FPC}True{$Else}False{$EndIf};
+    property AllowDefault: Boolean read FAllowDefault write FAllowDefault default true ;
+    {$IfNDef FMX}
+     property EnterAsTab: Boolean read FEnterAsTab write SetEnterAsTab default false ;
+     property UseScreenControl: Boolean read FUseScreenControl write FUseScreenControl
+        default {$IfDef FPC}True{$Else}False{$EndIf};
+    {$EndIf}
   end;
 
 implementation
 
+uses
+  ACBrUtil;
+
 { TACBrEnterTab }
-constructor TACBrEnterTab.Create( AOwner: TComponent );
+constructor TACBrEnterTab.Create(AOwner: TComponent);
 begin
-  if not ( AOwner is TForm ) then
-     raise Exception.Create('"Owner" do componente ACBrEnterTab deve ser do tipo TForm');
+  if not Assigned(AOwner) then
+    raise Exception.Create(ACBrStr('TACBrEnterTab.Owner não informado'));
 
-  inherited Create( AOwner );
-  FEnterAsTab   := false ;
-  FAllowDefault := True ;
-  FUseScreenControl := {$IfDef FPC}True{$Else}False{$EndIf};
+  inherited Create(AOwner);
 
-  { Salvando estado das Propriedades do Form, que serao modificadas }
-  with TForm( Owner ) do
-  begin
-     FOldKeyPreview := KeyPreview ;
-     FOldOnKeyPress := OnKeyPress ;
-  end ;
+  FFormOwner := nil;
+  FEnterAsTab := false;
+  FAllowDefault := True;
+  {$IfDef FMX}
+   FOldOnKeyDown := nil;
+  {$Else}
+   FOldKeyPreview := False;
+   FOldOnKeyPress := nil;
+   FUseScreenControl := {$IfDef FPC}True{$Else}False{$EndIf};
+  {$EndIf}
 end;
-
 
 destructor TACBrEnterTab.Destroy;
 begin
   { Restaurando estado das propriedades de Form modificadas }
-  if Assigned( Owner ) then
-     if not (csFreeNotification in Owner.ComponentState) then
-        with TForm( Owner ) do
-        begin
-           KeyPreview := FOldKeyPreview ;
-           OnKeyPress := FOldOnKeyPress ;
-        end ;
+  if Assigned(FFormOwner) then
+  begin
+    if not (csFreeNotification in FFormOwner.ComponentState) then
+    begin
+      with FFormOwner do
+      begin
+        {$IfDef FMX}
+         OnKeyDown := FOldOnKeyDown;
+        {$Else}
+         KeyPreview := FOldKeyPreview;
+         OnKeyPress := FOldOnKeyPress;
+        {$EndIf}
+      end ;
+    end;
+  end;
 
   inherited Destroy ;
 end;
 
-procedure TACBrEnterTab.DoEnterAsTab(AForm: TObject; var Key: Char);
-Var
-  DoClick : Boolean ;
-begin
-  try
+{$IfDef FMX}
+ procedure TACBrEnterTab.DoKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char; Shift: TShiftState);
+ begin
+   try
+     If Key = vkReturn Then
+     begin
+       if (FFormOwner.ActiveControl is TCustomButton) and FAllowDefault then
+         THackButtomControl( FFormOwner.ActiveControl ).Click
+       else
+       begin
+         Key := vkTab;
+         FFormOwner.KeyDown(Key, KeyChar, Shift);
+       end;
+     end ;
+   finally
+     if Assigned(FOldOnKeyDown) then
+       FOldOnKeyDown(Sender, Key, KeyChar, Shift);
+
+     Key := 0;
+   end;
+ end;
+{$Else}
+ procedure TACBrEnterTab.DoEnterAsTab(AForm: TObject; var Key: Char);
+ Var
+   DoClick : Boolean ;
+ begin
+   try
      if not (AForm is TForm) then
-        exit ;
+       exit ;
 
      If Key = #13 Then
      begin
-        if (TForm(AForm).ActiveControl is TButtonControl) and FAllowDefault then
-        begin
-           {$IFDEF VisualCLX}
-            TButtonControl( TForm(AForm).ActiveControl ).AnimateClick ;
-           {$ELSE}
-             DoClick := True;
-            {$IFDEF FPC}
-             {$IFNDEF Linux}
-              DoClick := False;  // Para evitar Click ocorre 2x em FPC com Win32
-             {$ENDIF}
+       if (TForm(AForm).ActiveControl is TButtonControl) and FAllowDefault then
+       begin
+          {$IFDEF VisualCLX}
+           TButtonControl( TForm(AForm).ActiveControl ).AnimateClick;
+          {$ELSE}
+            DoClick := True;
+           {$IFDEF FPC}
+            {$IFNDEF Linux}
+             DoClick := False;  // Para evitar Click ocorre 2x em FPC com Win32
             {$ENDIF}
-            if DoClick then
-               THackButtomControl( TForm(AForm).ActiveControl ).Click ;
            {$ENDIF}
-           exit ;
-        end ;
+           if DoClick then
+             THackButtomControl( TForm(AForm).ActiveControl ).Click;
+          {$ENDIF}
+          Exit;
+       end ;
 
-        if FUseScreenControl then
-          THackForm( AForm ).SelectNext( Screen.ActiveControl, True, True )
-        else
-          THackForm( AForm ).SelectNext( TForm(AForm).ActiveControl, True, True );
+       if FUseScreenControl then
+         THackForm( AForm ).SelectNext( Screen.ActiveControl, True, True )
+       else
+         THackForm( AForm ).SelectNext( TForm(AForm).ActiveControl, True, True );
      end ;
-  finally
+   finally
      if Assigned( FOldOnKeyPress ) then
-        FOldOnKeyPress( AForm, Key ) ;
+       FOldOnKeyPress( AForm, Key );
 
      If Key = #13 Then
-        Key := #0;
-  end ;
-end;
+       Key := #0;
+   end;
+ end;
+{$EndIf}
 
 procedure TACBrEnterTab.SetEnterAsTab(const Value: Boolean);
+var
+  RealOwner: TComponent;
 begin
-  if Value = FEnterAsTab then exit ;
+  if Value = FEnterAsTab then
+    Exit;
+
+  if Value and (not Assigned(FFormOwner)) then
+  begin
+    RealOwner := Owner;
+    while Assigned(RealOwner) and (not (RealOwner is TCustomForm)) do
+      RealOwner := RealOwner.Owner;
+
+    if (not Assigned(RealOwner)) then
+      raise Exception.Create(ACBrStr('Não foi possível encontrar o Form Pai para TACBrEnterTab'));
+
+    FFormOwner := TForm(RealOwner);
+    { Salvando estado das Propriedades do Form, que serão modificadas }
+    with FFormOwner do
+    begin
+      {$IfDef FMX}
+       FOldOnKeyDown := OnKeyDown;
+      {$Else}
+       FOldKeyPreview := KeyPreview ;
+       FOldOnKeyPress := OnKeyPress ;
+      {$EndIf}
+    end ;
+  end;
 
   if not (csDesigning in ComponentState) then
   begin
-     with TForm( Owner ) do
-     begin
-        if Value then
-         begin
-           KeyPreview := true ;
-           OnKeyPress := DoEnterAsTab ;
-         end
-        else
-         begin
-           KeyPreview := FOldKeyPreview ;
-           OnKeyPress := FOldOnKeyPress ;
-         end ;
-     end ;
-  end ;
+    with TForm(Owner) do
+    begin
+      if Value then
+      begin
+        {$IfDef FMX}
+         OnKeyDown := DoKeyDown;
+        {$Else}
+         KeyPreview := true;
+         OnKeyPress := DoEnterAsTab;
+        {$EndIf}
+      end
+      else
+      begin
+        {$IfDef FMX}
+         OnKeyDown := FOldOnKeyDown;
+        {$Else}
+         KeyPreview := FOldKeyPreview;
+         OnKeyPress := FOldOnKeyPress;
+        {$EndIf}
+      end;
+    end;
+  end;
 
   FEnterAsTab := Value;
 end;

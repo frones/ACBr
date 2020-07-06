e063d6464ad9c6807b90d5cd79c79eba23e34d73
bigwings <bigwings@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-07-06 13:29:28 +0000

    https://www.projetoacbr.com.br/forum/topic/58415-ajuda-com-acbr-posprinter/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@20231 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrSerial/ACBrPosPrinter/Delphi/PosPrinterTeste.dpr
+++ Exemplos/ACBrSerial/ACBrPosPrinter/Delphi/PosPrinterTeste.dpr
@@ -1,13 +1,13 @@
 program PosPrinterTeste;
+
 uses
   Forms,
-  unit1 in 'unit1.pas', {FrPosPrinterTeste}
-  configuraserial in 'configuraserial.pas' {frConfiguraSerial};
+  Unit1 in 'unit1.pas' {FrPosPrinterTeste},
+  ConfiguraSerial in 'configuraserial.pas' {frConfiguraSerial};
 
 {$R *.res}
 
 begin
-  //ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.CreateForm(TFrPosPrinterTeste, FrPosPrinterTeste);
   Application.Run;

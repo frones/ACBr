98a03941e9516963f8dec6a19452e378c6063031
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-07-24 14:48:49 +0000

    [-] Enviando arquivos que estavam faltando.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@20336 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/ETQ/ETQPageCode.cs
@@ -0,0 +1,13 @@
+﻿namespace ACBrLib.Core.ETQ
+{
+    public enum ETQPageCode
+    {
+        pceNone,
+        pce437,
+        pce850,
+        pce852,
+        pce860,
+        pce1250,
+        pce1252
+    }
+}
---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/GNRe/VersaoGNRe.cs
@@ -0,0 +1,8 @@
+﻿namespace ACBrLib.Core.GNRe
+{
+    public enum VersaoGNRe
+    {
+        ve100 = 0,
+        ve200 = 1
+    }
+}

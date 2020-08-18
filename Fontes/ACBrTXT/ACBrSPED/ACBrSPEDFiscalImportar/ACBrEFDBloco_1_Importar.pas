8fdb5952564f581fd764e216c9c6be6a314c0066
embarbosa <embarbosa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-08-18 14:13:40 +0000

    -- ACBrSpedFiscalImportar [+] Adicionados mais registros do bloco 1 por econdotta. Veja: https://www.projetoacbr.com.br/forum/topic/58786-importa%C3%A7%C3%A3o-efdicms-ipi/?do=findComment&comment=385647
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@20469 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTXT/ACBrSPED/ACBrSPEDFiscalImportar/ACBrEFDBloco_1_Importar.pas
+++ Fontes/ACBrTXT/ACBrSPED/ACBrSPEDFiscalImportar/ACBrEFDBloco_1_Importar.pas
@@ -54,6 +54,19 @@ type
     procedure Reg1360;
     procedure Reg1370;
     procedure Reg1600;
+    procedure Reg1900;
+    procedure Reg1910;
+    procedure Reg1920;
+    procedure Reg1921;
+    procedure Reg1922;
+    procedure Reg1923;
+    procedure Reg1925;
+    procedure Reg1926;
+    procedure Reg1960;
+    procedure Reg1970;
+    procedure Reg1975;
+    procedure Reg1980;
+    procedure Reg1990;
   public
     procedure AnalisaRegistro(const inDelimitador: TStrings); override;
   end;
@@ -88,7 +101,33 @@ begin
   else if (vHead = '1370') then
     Reg1370
   else if (vHead = '1600') then
-    Reg1600;
+    Reg1600
+  else If (vHead = '1900') Then
+    Reg1900
+  else If (vHead = '1910') Then
+    Reg1910
+  else If (vHead = '1920') Then
+    Reg1920
+  else If (vHead = '1921') Then
+    Reg1921
+  else If (vHead = '1922') Then
+    Reg1922
+  else If (vHead = '1923') Then
+    Reg1923
+  else If (vHead = '1925') Then
+    Reg1925
+  else If (vHead = '1926') Then
+    Reg1926
+  else If (vHead = '1960') Then
+    Reg1960
+  else If (vHead = '1970') Then
+    Reg1970
+  else If (vHead = '1975') Then
+    Reg1975
+  else If (vHead = '1980') Then
+    Reg1980
+  else If (vHead = '1990') Then
+    Reg1990;
 end;
 
 procedure TACBrSpedFiscalImportar_Bloco1.Reg1001;
@@ -208,4 +247,181 @@ begin
   end;
 end;
 
-end.
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1900;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1900New Do
+   Begin
+      IND_APUR_ICMS        := Valor;
+      DESCR_COMPL_OUT_APUR := Valor;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1910;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1910New Do
+   Begin
+      DT_INI := ValorD;
+      DT_FIN := ValorD;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1920;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1920New Do
+   Begin
+      VL_TOT_TRANSF_DEBITOS_OA  := ValorF;
+      VL_TOT_AJ_DEBITOS_OA      := ValorF;
+      VL_ESTORNOS_CRED_OA       := ValorF;
+      VL_TOT_TRANSF_CREDITOS_OA := ValorF;
+      VL_TOT_AJ_CREDITOS_OA     := ValorF;
+      VL_ESTORNOS_DEB_OA        := ValorF;
+      VL_SLD_CREDOR_ANT_OA      := ValorF;
+      VL_SLD_APURADO_OA         := ValorF;
+      VL_TOT_DED                := ValorF;
+      VL_ICMS_RECOLHER_OA       := ValorF;
+      VL_SLD_CREDOR_TRANSP_OA   := ValorF;
+      DEB_ESP_OA                := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1921;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1921New Do
+   Begin
+      COD_AJ_APUR    := Valor;
+      DESCR_COMPL_AJ := Valor;
+      VL_AJ_APUR     := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1922;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1922New Do
+   Begin
+      NUM_DA    := Valor;
+      NUM_PROC  := Valor;
+      IND_PROC  := Valor;
+      PROC      := Valor;
+      TXT_COMPL := Valor;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1923;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1923New Do
+   Begin
+      COD_PART   := Valor;
+      COD_MOD    := Valor;
+      SER        := Valor;
+      SUB        := Valor;
+      NUM_DOC    := Valor;
+      DT_DOC     := ValorD;
+      COD_ITEM   := Valor;
+      VL_AJ_ITEM := ValorF;
+      CHV_DOCe   := Valor;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1925;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1925New Do
+   Begin
+      COD_INF_ADIC   := Valor;
+      VL_INF_ADIC    := ValorF;
+      DESCR_COMPL_AJ := Valor;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1926;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1926New Do
+   Begin
+      COD_OR    := Valor;
+      VL_OR     := ValorF;
+      DT_VCTO   := ValorD;
+      COD_REC   := Valor;
+      NUM_PROC  := Valor;
+      IND_PROC  := Valor;
+      PROC      := Valor;
+      TXT_COMPL := Valor;
+      MES_REF   := Valor;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1960;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1960New Do
+   Begin
+      IND_AP := Valor;
+      G1_01  := ValorF;
+      G1_02  := ValorF;
+      G1_03  := ValorF;
+      G1_04  := ValorF;
+      G1_05  := ValorF;
+      G1_06  := ValorF;
+      G1_07  := ValorF;
+      G1_08  := ValorF;
+      G1_09  := ValorF;
+      G1_10  := ValorF;
+      G1_11  := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1970;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1970New Do
+   Begin
+      IND_AP := Valor;
+      G3_01  := ValorF;
+      G3_02  := ValorF;
+      G3_03  := ValorF;
+      G3_04  := ValorF;
+      G3_05  := ValorF;
+      G3_06  := ValorF;
+      G3_07  := ValorF;
+      G3_T   := ValorF;
+      G3_08  := ValorF;
+      G3_09  := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1975;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1975New Do
+   Begin
+      ALIQ_IMP_BASE := ValorF;
+      G3_10         := ValorF;
+      G3_11         := ValorF;
+      G3_12         := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1980;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1980New Do
+   Begin
+      IND_AP := Valor;
+      G4_01  := ValorF;
+      G4_02  := ValorF;
+      G4_03  := ValorF;
+      G4_04  := ValorF;
+      G4_05  := ValorF;
+      G4_06  := ValorF;
+      G4_07  := ValorF;
+      G4_08  := ValorF;
+      G4_09  := ValorF;
+      G4_10  := ValorF;
+      G4_11  := ValorF;
+      G4_12  := ValorF;
+   End;
+End;
+
+procedure TACBrSpedFiscalImportar_Bloco1.Reg1990;
+Begin
+   With ACBrSpedFiscal.Bloco_1.Registro1990 Do
+   Begin
+      QTD_LIN_1 := ValorI;
+   End;
+End;
+
+End.

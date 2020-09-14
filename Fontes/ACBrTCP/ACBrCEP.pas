a0f5ff77f68f07879f7b69d4b8072454fa1c9103
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-09-14 18:17:20 +0000

    WebService ACBrCEP com retorno de Latitude e Longitude https://www.projetoacbr.com.br/forum/topic/59336-webservice-acbrcep-com-retorno-de-latitude-e-longitude/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@20602 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTCP/ACBrCEP.pas
+++ Fontes/ACBrTCP/ACBrCEP.pas
@@ -1689,6 +1689,9 @@ begin
         Municipio := AJson.O['cidade'].S['nome'];
         IBGE_Municipio := AJson.O['cidade'].S['ibge'];
         UF := AJson.O['estado'].S['sigla'];
+        Latitude := AJson.S['latitude'];
+        Longitude := AJson.S['longitude'];
+        Altitude := AJson.S['altitude'];
       end;
     finally
       AJSon.Free;
@@ -1708,6 +1711,9 @@ begin
           IBGE_Municipio := Values['ibge'].AsString;
         end;
         UF := AJson.Values['estado'].AsObject.Values['sigla'].AsString;
+        Latitude := AJson.Values['latitude'].AsString;
+        Longitude := AJson.Values['longitude'].AsString;
+        Altitude := AJson.Values['altitude'].AsString;
       end;
     finally
       AJson.Free;

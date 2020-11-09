c9d875b2f2545b8e99e073129713ba8f5bb8428c
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2020-11-09 16:32:30 +0000

    09/11/2020 No cabeçalho ficou faltando as referencias dos primeiros desenvolvedores que colaboraram na criação do componente.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@20898 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesRetEnvioLote.pas
+++ Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesRetEnvioLote.pas
@@ -34,6 +34,15 @@
 {       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
 {******************************************************************************}
 
+{******************************************************************************
+|* Historico
+|*
+|* 27/10/2015: Jean Carlo Cantu, Tiago Ravache
+|*  - Doação do componente para o Projeto ACBr
+|* 28/08/2017: Leivio Fontenele - leivio@yahoo.com.br
+|*  - Implementação comunicação, envelope, status e retorno do componente com webservice.
+******************************************************************************}
+
 {$I ACBr.inc}
 
 unit pcesRetEnvioLote;

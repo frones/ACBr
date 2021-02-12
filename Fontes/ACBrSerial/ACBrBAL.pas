120cb559285d1d53cc12bd7771381c3e35b5571f
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-02-12 21:03:16 +0000

    [+] ACBrBal o Modelo Balança Capital
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@21346 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSerial/ACBrBAL.pas
+++ Fontes/ACBrSerial/ACBrBAL.pas
@@ -62,7 +62,7 @@ TACBrBALModelo = (balNenhum, balFilizola, balToledo, balToledo2090, balToledo218
                   balMicheletti, balAlfa, balToledo9091_8530_8540, balWeightechWT1000,
                   balMarelCG62XL, balWeightechWT3000_ABS, balToledo2090N, balToledoBCS21,
                   balPrecision, balDigitron_UL, balLibratekWT3000IR, balToledoTi420,
-                  balWeightechWT27R_ETH);
+                  balWeightechWT27R_ETH, balCapital);
 TACBrBALLePeso = procedure(Peso : Double; Resposta : AnsiString) of object ;
 
 { Componente ACBrBAL }
@@ -151,7 +151,7 @@ uses
   ACBrBALWeightechWT1000, ACBrBALMarelCG62XL, ACBrBALWeightechWT3000_ABS,
   ACBrBALToledo2090N, ACBrBALToledoBCS21, ACBrBALPrecision,
   ACBrBALDigitron_UL, ACBrBALLibratekWT3000IR, ACBrBALToledoTi420,
-  ACBrBALWeightechWT27R_ETH,
+  ACBrBALWeightechWT27R_ETH, ACBrBALCapital,
   {$IFDEF COMPILER6_UP} StrUtils {$ELSE} ACBrD5{$ENDIF};
 
 { TACBrBAL }
@@ -255,6 +255,7 @@ begin
      balLibratekWT3000IR     : fsBAL := TACBrBALLibratekWT3000IR.Create(Self);
      balToledoTi420          : fsBAL := TACBrBALToledoTi420.Create(self);
      balWeightechWT27R_ETH   : fsBAL := TACBrbalWeightechWT27R_ETH.Create(self);
+     balCapital  : fsBAL := TACBrBALCapital.Create(Self);
   else
      fsBAL := TACBrBALClass.Create(Self);
   end;
---
+++ Fontes/ACBrSerial/ACBrBALCapital.pas
@@ -0,0 +1,112 @@
+{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2021 Daniel Simoes de Almeida               }
+{                                                                              }
+{ Colaboradores nesse arquivo:   Juliomar Marchetti                            }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
+{       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
+{******************************************************************************}
+
+{$I ACBr.inc}
+
+unit ACBrBALCapital;
+
+interface
+
+uses
+  Classes,
+  ACBrBALClass
+  {$IFDEF NEXTGEN}
+   ,ACBrBase
+  {$ENDIF};
+
+type
+    { TACBrBALCapital }
+
+  TACBrBALCapital = class(TACBrBALClass)
+  public
+    constructor Create(AOwner: TComponent);
+    function InterpretarRepostaPeso(const aResposta: AnsiString): Double; override;
+  end;
+
+implementation
+
+uses
+  SysUtils, StrUtils,
+  ACBrConsts, ACBrUtil,
+  {$IFDEF COMPILER6_UP}
+   DateUtils
+  {$ELSE}
+   ACBrD5, Windows
+  {$ENDIF};
+
+{ TACBrBALCapital }
+
+constructor TACBrBALCapital.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+
+  fpModeloStr := 'Capital';
+end;
+
+function TACBrBALCapital.InterpretarRepostaPeso(const aResposta: AnsiString
+  ): Double;
+var
+   wResposta: String;
+   wPosIni, wPosFim: integer;
+begin
+  Result    := 0;
+
+  wPosIni     := PosLast('E', aResposta);
+  wPosFim     := PosEx(CR, aResposta, wPosIni + 1);
+
+  if wPosFim = 0 then
+    wResposta := Trim(Copy(aResposta, wPosIni + 1, Length(aResposta) ))
+  else
+    wResposta := Trim(Copy(aResposta, wPosIni + 1, (wPosFim - wPosIni) - 2));
+
+  if (wResposta = EmptyStr) then
+    Exit;
+
+  { Ajustando o separador de Decimal corretamente }
+  wResposta := StringReplace(wResposta, '.', DecimalSeparator, [rfReplaceAll]);
+  wResposta := StringReplace(wResposta, ',', DecimalSeparator, [rfReplaceAll]);
+
+  try
+    Result := StrToFloat(wResposta);
+  except
+    //case PadLeft(Trim(wResposta),1)[1] of
+      //'I': Result := -1;     { Instavel }
+      //'N': Result := -2;     { Peso Negativo }
+      //'S': Result := -10;  { Sobrecarga de Peso }
+    //else
+      Result := 0;
+    //end;
+  end;
+end;
+
+end.
+

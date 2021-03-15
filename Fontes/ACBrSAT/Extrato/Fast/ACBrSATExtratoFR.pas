7bec4d67b5c4659efb64be4163dd12c67edccc7e
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-03-15 18:07:05 +0000

    [+] Impressão de CFe SAT em FastReport Correções https://www.projetoacbr.com.br/forum/topic/59556-impress%C3%A3o-de-cfe-sat-em-fastreport/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@21520 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSAT/Extrato/Fast/ACBrSATExtratoFR.pas
+++ Fontes/ACBrSAT/Extrato/Fast/ACBrSATExtratoFR.pas
@@ -53,6 +53,7 @@ uses
 	frxBarcode;
 
 type
+  EACBrSATExtratoFR = class(Exception);
 
   { TACBrSATExtratoFR }
   {$IFDEF RTL230_UP}
@@ -60,7 +61,9 @@ type
   {$ENDIF RTL230_UP}
   TACBrSATExtratoFR = class( TACBrSATExtratoReportClass )
   private
-
+	FCFe: TCFe;
+    FCFeCanc: TCFeCanc;
+	
     cdsIdentificacao: TClientDataSet;
     cdsEmitente: TClientDataSet;
     cdsParametros: TClientDataSet;
@@ -77,15 +80,31 @@ type
     frxCalculoImposto: TfrxDBDataset;
     frxFormaPagamento: TfrxDBDataset;
     frxEntrega: TfrxDBDataset;
-		frxReport : TfrxReport;
-		frxPDFExport : TfrxPDFExport;
-		frxBarCodeObject : TfrxBarCodeObject;
+	frxReport : TfrxReport;
+	frxPDFExport : TfrxPDFExport;
+	frxBarCodeObject : TfrxBarCodeObject;
 
     FFastExtrato             : string;
     FFastExtratoResumido     : string;
     FFastExtratoCancelamento : string;
     procedure CriarDataSetsFrx;
-		procedure frxReportBeforePrint(Sender: TfrxReportComponent);
+	procedure SetDataSetsToFrxReport;
+	procedure frxReportBeforePrint(Sender: TfrxReportComponent);
+	function PrepareReport(ACFe: TCFe): Boolean;
+	
+	procedure CarregaIdentificacao;
+    procedure CarregaEmitente;
+    procedure CarregaParametros;
+    procedure CarregaInformacoesAdicionais;
+    procedure CarregaDadosEntrega;
+    procedure CarregaDadosProdutos;
+    procedure CarregaCalculoImposto;
+    procedure CarregaFormaPagamento;
+    procedure AjustaMargensReports;
+
+	procedure CarregaDados;
+    procedure LimpaDados;	
+	
   protected
     procedure Imprimir;
   public
@@ -105,7 +124,7 @@ type
 implementation
 
 uses
-  ACBrDFeReport, ACBrValidador, StrUtils, ACBrDelphiZXingQRCode;
+  ACBrDFeReport, ACBrValidador, StrUtils, ACBrDelphiZXingQRCode, ACBrUtil, ACBrDFeUtil, ACBrSAT;
 
 { TACBrSATExtratoFR }
 
@@ -126,6 +145,123 @@ begin
        PintarQRCode(qrCode, TfrxPictureView(Sender).Picture.Bitmap, qrUTF8NoBOM);
   end;
 end;
+
+procedure TACBrSATExtratoFR.SetDataSetsToFrxReport;
+begin
+  frxReport.EnabledDataSets.Clear;
+
+  frxReport.EnabledDataSets.Add(frxIdentificacao);
+  frxReport.EnabledDataSets.Add(frxEmitente);
+  frxReport.EnabledDataSets.Add(frxParametros);
+  frxReport.EnabledDataSets.Add(frxDadosProdutos);
+  frxReport.EnabledDataSets.Add(frxInformacoesAdicionais);
+  frxReport.EnabledDataSets.Add(frxCalculoImposto);
+  frxReport.EnabledDataSets.Add(frxFormaPagamento);
+  frxReport.EnabledDataSets.Add(frxEntrega);
+end;
+
+procedure TACBrSATExtratoFR.CarregaDados;
+begin
+  LimpaDados;
+
+  CarregaParametros;
+  CarregaIdentificacao;
+  CarregaEmitente;
+  CarregaDadosProdutos;
+  CarregaInformacoesAdicionais;
+  CarregaCalculoImposto;
+  CarregaFormaPagamento;
+  CarregaDadosEntrega
+end;
+
+procedure TACBrSATExtratoFR.CarregaParametros;
+begin
+  with cdsParametros do
+  begin
+    Append;
+
+    FieldByName('QtdeItens').AsInteger := FCFe.Det.Count;
+    FieldByName('Imagem').AsString := Ifthen(Self.Logo <> '', Self.Logo,'');
+    FieldByName('Sistema').AsString := Ifthen(Self.Sistema <> '',Self.Sistema,'Projeto ACBr - https://www.projetoacbr.com.br');
+    FieldByName('Usuario').AsString := Ifthen(Self.Usuario <> '', Self.Usuario,'');
+
+    Post;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.LimpaDados;
+begin
+  cdsIdentificacao.EmptyDataSet;
+  cdsEmitente.EmptyDataSet;
+  cdsParametros.EmptyDataSet;
+  cdsDadosProdutos.EmptyDataSet;
+  cdsInformacoesAdicionais.EmptyDataSet;
+  cdsCalculoImposto.EmptyDataSet;
+  cdsFormaPagamento.EmptyDataSet;
+  cdsEntrega.EmptyDataSet;
+end;
+
+function TACBrSATExtratoFR.PrepareReport(ACFe: TCFe): Boolean;
+var
+  Stream: TStringStream;
+begin
+  Result := False;
+  SetDataSetsToFrxReport;
+
+  if Trim(FastExtrato) <> '' then
+  begin
+    if not (UpperCase(Copy(FastExtrato, Length(FastExtrato)-3, 4)) = '.FR3') then
+    begin
+      Stream := TStringStream.Create(FastExtrato);
+	  try
+		frxReport.FileName := '';
+		frxReport.LoadFromStream(Stream);
+	  finally
+		Stream.Free;
+	  end;
+    end
+    else
+    begin
+      if FileExists(FastExtrato) then
+        frxReport.LoadFromFile(FastExtrato)
+      else
+        raise EACBrSATExtratoFR.CreateFmt('Caminho do arquivo de impressão do Extrato SAT "%s" inválido.', [FastExtrato]);
+    end;
+  end
+  else
+    raise EACBrSATExtratoFR.Create('Caminho do arquivo de impressão do Extrato SAT não assinalado.');
+
+  frxReport.PrintOptions.Copies      := NumCopias;
+  frxReport.PrintOptions.ShowDialog  := MostraSetup;
+  frxReport.ShowProgress             := MostraStatus;
+  frxReport.PreviewOptions.AllowEdit := False;
+
+  // Define a impressora
+  if NaoEstaVazio(frxReport.PrintOptions.Printer) then
+    frxReport.PrintOptions.Printer := Impressora;
+
+  if Assigned(ACFe) then
+  begin
+    FCFe := ACFe;
+    SetDataSetsToFrxReport;
+    Result := frxReport.PrepareReport;
+  end
+  else
+  begin
+    if Assigned(ACBrSAT) then
+    begin
+      FCFe := TACBrSAT (ACBrSAT).CFe;
+      CarregaDados;
+
+      Result := frxReport.PrepareReport(False);
+    end
+    else
+      raise EACBrSATExtratoFR.Create('Propriedade ACBrSAT não assinalada.');
+  end;
+
+  AjustaMargensReports;
+end;
+
 
 constructor TACBrSATExtratoFR.Create(AOwner: TComponent);
 begin
@@ -381,5 +517,225 @@ begin
      DataSet := cdsEntrega;
   end;
 end;
+
+procedure TACBrSATExtratoFR.CarregaIdentificacao;
+var NumExtrato: String;
+begin
+  with cdsIdentificacao do
+  begin
+    Append;
+
+    with FCFe.infCFe do
+    begin
+      FieldByName('Id').AsString    := OnlyNumber(Id);
+      FieldByName('Chave').AsString := FormatarChaveAcesso(Id);
+    end;
+
+    with FCFe.Ide do
+    begin
+      if tpAmb = taHomologacao then
+        NumExtrato := '000,000,000'
+      else
+        NumExtrato := FormatFloat('000,000,000', nCFe);
+
+      FieldByName('nCFe').AsString      := NumExtrato;
+      FieldByName('tpAmb').AsInteger    := StrToIntDef(TpAmbToStr(tpAmb), 0);
+      FieldByName('nserieSAT').AsString := FormatFloat('000,000,000', nserieSAT);
+      FieldByName('dhEmi').AsString     := FormatDateTimeBr(dEmi + hEmi, 'DD/MM/YYYY - hh:nn:ss');
+    end;
+
+    if FCFe.Dest.CNPJCPF <> '' then
+      FieldByName('CPFConsumidor').AsString := Format('CONSUMIDOR - CPF %s', [FormatarCPF(OnlyNumber(CFe.Dest.CNPJCPF))])
+    else
+      FieldByName('CPFConsumidor').AsString := 'CONSUMIDOR NÃO IDENTIFICADO';
+
+    Post;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaEmitente;
+begin
+  with cdsEmitente do
+  begin
+    Append;
+
+    with FCFe.emit do
+    begin
+      FieldByName('CNPJ').AsString  := FormatarCNPJ(CNPJ);
+      FieldByName('IE').AsString    := IE;
+      FieldByName('IM').AsString    := IM;
+      FieldByName('XNome').AsString := xNome;
+      FieldByName('XFant').AsString := XFant;
+
+      with EnderEmit do
+      begin
+        FieldByName('Xlgr').AsString    := XLgr;
+        FieldByName('Nro').AsString     := Nro;
+        FieldByName('XBairro').AsString := XBairro;
+        FieldByName('XMun').AsString    := XMun;
+        FieldByName('CEP').AsString     := FormatarCEP(Poem_Zeros(CEP, 8));
+        FieldByName('email').AsString   := email;
+        FieldByName('site').AsString    := Self.Site;
+      end;
+    end;
+
+    Post;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaDadosProdutos;
+var
+  inItem: Integer;
+begin
+  with cdsDadosProdutos do
+  begin
+    for inItem := 0 to (FCFe.Det.Count - 1) do
+    begin
+      Append;
+
+      with FCFe.Det.Items[inItem] do
+      begin
+        FieldByName('nItem').AsInteger   := inItem + 1;
+        FieldByName('ChaveCFe').AsString := FCFe.infCFe.ID;
+        FieldByName('cProd').AsString    := Trim(Prod.cProd);
+        FieldByName('xProd').AsString    := StringReplace( Prod.xProd, ';', #13, [rfReplaceAll]);
+        FieldByName('CFOP').AsString     := Prod.CFOP;
+        FieldByName('Ucom').AsString     := Prod.UCom;
+        FieldByName('QCom').AsFloat      := Prod.QCom;
+        FieldByName('vUnCom').AsFloat    := Prod.VUnCom;
+        FieldByName('vProd').AsFloat     := Prod.VProd;
+        FieldByName('indRegra').AsString := indRegraToStr(Prod.indRegra);
+        FieldByName('vItem').AsFloat     := Prod.vItem;
+        FieldByName('vDesc').AsString    := FormatFloatBr( Prod.vDesc,',0.00');
+        FieldByName('vOutro').AsString   := FormatFloatBr( Prod.vOutro ,',0.00');
+
+        if Imposto.vItem12741 > 0 then
+          FieldByName('vTR').AsString := ' ('+FormatFloatBr(Imposto.vItem12741)+') '
+        else
+          FieldByName('vTR').AsString := '';
+
+        FieldByName('Valorliquido').AsString    := FormatFloatBr( Prod.vProd - Prod.vDesc ,',0.00');
+        FieldByName('ValorAcrescimos').AsString := FormatFloatBr( Prod.vProd + Prod.vOutro,',0.00');
+
+        FieldByName('vDeducISSQN').AsFloat := Imposto.ISSQN.vDeducISSQN;
+        FieldByName('vBC').AsFloat         := Imposto.ISSQN.vBC;
+      end;
+
+      Post;
+    end;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaInformacoesAdicionais;
+var i: Integer;
+begin
+  with FCFe, cdsInformacoesAdicionais do
+  begin
+    Append;
+
+    if (Emit.cRegTrib = RTSimplesNacional) then
+      FieldByName('ObsFisco').AsString := Msg_ICMS_123_2006;
+
+    for i := 0 to obsFisco.Count - 1 do
+      FieldByName('ObsFisco').AsString := FieldByName('ObsFisco').AsString + obsFisco[i].xCampo + '-' + obsFisco[i].xTexto;
+
+    if (InfAdic.infCpl <> '') or (Self.ImprimeMsgOlhoNoImposto and (Total.vCFeLei12741 > 0)) then
+      FieldByName('infAdic').AsString := StringReplace(InfAdic.infCpl,';',sLineBreak,[rfReplaceAll]);;
+
+    Post;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaCalculoImposto;
+begin
+  with cdsCalculoImposto do
+  begin
+    Append;
+
+    with FCFe.Total do
+    begin
+      FieldByName('VICMS').AsFloat     := ICMSTot.VICMS;
+      FieldByName('VProd').AsFloat     := ICMSTot.VProd;
+      FieldByName('VPIS').AsFloat      := ICMSTot.VPIS;
+      FieldByName('VCOFINS').AsFloat   := ICMSTot.VCOFINS;
+      FieldByName('vPISST').AsFloat    := ICMSTot.vPISST;
+      FieldByName('vCOFINSST').AsFloat := ICMSTot.vCOFINSST;
+
+      FieldByName('vDescSubtot').AsFloat  := DescAcrEntr.vDescSubtot;
+      FieldByName('vAcresSubtot').AsFloat := DescAcrEntr.vAcresSubtot;
+
+      if ICMSTot.vDesc > 0 then
+         FieldByName('vDescAcresItens').AsString := FormatFloatBr(ICMSTot.vDesc, '-,0.00')
+      else
+      if ICMSTot.vOutro > 0 then
+         FieldByName('vDescAcresItens').AsString := FormatFloatBr(ICMSTot.vOutro, '+,0.00');
+
+      FieldByName('vCFe').AsFloat := vCFe;
+      FieldByName('vCFeLei12741').AsFloat := vCFeLei12741;
+    end;
+
+    if FCFe.Pagto.vTroco > 0 then
+    begin
+      FieldByName('vTroco').AsCurrency   := FCFe.Pagto.vTroco;
+      FieldByName('vTotPago').AsCurrency := FCFe.Pagto.vTroco+FieldByName('vCFe').AsFloat;
+    end;
+
+    Post;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaFormaPagamento;
+var 
+	i: Integer;
+begin
+  with cdsFormaPagamento do
+  begin
+    for i := 0 to FCFe.Pagto.Count - 1 do
+    begin
+      Append;
+
+      with FCFe.Pagto.Items[i] do
+      begin
+        FieldByName('tPag').AsString := CodigoMPToDescricao(cMP);
+        FieldByName('vMP').AsFloat   := vMP;
+      end;
+
+      Post;
+    end;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.CarregaDadosEntrega;
+begin
+  with cdsEntrega, FCFe.Entrega do
+  begin
+    if xLgr <> '' then
+    begin
+      Append;
+      FieldByName('EnderecoEntrega').AsString := Format('%s, nº %s - %s - %s', [xLgr, nro, xBairro, xMun]);
+      Post;
+    end;
+  end;
+end;
+
+procedure TACBrSATExtratoFR.AjustaMargensReports;
+var
+  Page: TfrxReportPage;
+  i: Integer;
+begin
+  for i := 0 to (frxReport.PreviewPages.Count - 1) do
+  begin
+    Page := frxReport.PreviewPages.Page[i];
+    if (MargemSuperior > 0) then
+      Page.TopMargin := MargemSuperior;
+    if (MargemInferior > 0) then
+      Page.BottomMargin := MargemInferior;
+    if (MargemEsquerda > 0) then
+      Page.LeftMargin := MargemEsquerda;
+    if (MargemDireita > 0) then
+      Page.RightMargin := MargemDireita;
+  end;
+end;
+
 
 end.

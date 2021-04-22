24021cb536e0798fd00653c826dda24899f53ea4
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-04-22 16:56:09 +0000

    [-] Correção no projeto da ACBrLibCEP. [+] Adicionado classe com imports para testes.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@21741 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Fontes/CEP/ACBrLibCEPStaticImportMT.pas
+++ Projetos/ACBrLib/Fontes/CEP/ACBrLibCEPStaticImportMT.pas
@@ -30,7 +30,7 @@
 {       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
 {******************************************************************************}
 
-unit ACBrLibCEPStaticImport;
+unit ACBrLibCEPStaticImportMT;
 
 {$IfDef FPC}
 {$mode objfpc}{$H+}
@@ -52,53 +52,50 @@ const
   {$EndIf}
  {$Else}
   {$IfDef CPU64}
-  CACBrCEPLIBName = 'ACBrCEP64.so';
+  CACBrCEPLIBName = 'libacbrcep64.so';
   {$Else}
-  CACBrCEPLIBName = 'ACBrCEP32.so';
-
+  CACBrCEPLIBName = 'libacbrcep32.so';
   {$EndIf}
  {$EndIf}
 
-{$I ACBrLibErros.inc}
-
 {%region Constructor/Destructor}
-function CEP_Inicializar(const eArqConfig, eChaveCrypt: PChar): longint;
+function CEP_Inicializar(var libHandle: longint; const eArqConfig, eChaveCrypt: PChar): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_Finalizar: longint;
+function CEP_Finalizar(const libHandle: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 {%endregion}
 
 {%region Versao/Retorno}
-function CEP_Nome(const sNome: PChar; var esTamanho: longint): longint;
+function CEP_Nome(const libHandle: longint; const sNome: PChar; var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_Versao(const sVersao: PChar; var esTamanho: longint): longint;
+function CEP_Versao(const libHandle: longint; const sVersao: PChar; var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_UltimoRetorno(const sMensagem: PChar; var esTamanho: longint): longint;
+function CEP_UltimoRetorno(const libHandle: longint; const sMensagem: PChar; var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 {%endregion}
 
 {%region Ler/Gravar Config }
-function CEP_ConfigLer(const eArqConfig: PChar): longint;
+function CEP_ConfigLer(const libHandle: longint; const eArqConfig: PChar): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_ConfigGravar(const eArqConfig: PChar): longint;
+function CEP_ConfigGravar(const libHandle: longint; const eArqConfig: PChar): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_ConfigLerValor(const eSessao, eChave: PChar; sValor: PChar; var esTamanho: longint): longint;
+function CEP_ConfigLerValor(const libHandle: longint; const eSessao, eChave: PChar; sValor: PChar; var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 
-function CEP_ConfigGravarValor(const eSessao, eChave, eValor: PChar): longint;
+function CEP_ConfigGravarValor(const libHandle: longint; const eSessao, eChave, eValor: PChar): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
 {%endregion}
 
 {%region CEPança}
-function CEP_BuscarPorCEP(eCEP: PChar; var Qtde: Integer;
+function CEP_BuscarPorCEP(const libHandle: longint; eCEP: PChar; var Qtde: Integer;
   const sResposta: PChar; var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;
-function CEP_BuscarPorLogradouro(eCidade, eTipo_Logradouro, eLogradouro, eUF,
+function CEP_BuscarPorLogradouro(const libHandle: longint; eCidade, eTipo_Logradouro, eLogradouro, eUF,
   eBairro: PChar; var Qtde: Integer; const sResposta: PChar;
   var esTamanho: longint): longint;
   {$IfDef STDCALL} stdcall{$Else} cdecl{$EndIf}; external CACBrCEPLIBName;

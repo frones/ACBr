f429c07bf4a704548258e263d8d9ea346e3b4ca5
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-09-27 14:25:24 +0000

    27/09/2021 -- ACBrXmlBase -- [*] Incluido o tratamento para quando existe no XML o caractere #9 usado por     alguns provedores.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@23071 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrXmlBase.pas
+++ Fontes/ACBrDFe/ACBrXmlBase.pas
@@ -161,6 +161,7 @@ begin
   Result := FaststringReplace(Result, ''#$A'', '', [rfReplaceAll]);
   Result := FaststringReplace(Result, ''#$A#$A'', '', [rfReplaceAll]);
   Result := FaststringReplace(Result, '<br >', ';', [rfReplaceAll]);
+  Result := FaststringReplace(Result, #9, '', [rfReplaceAll]);
 end;
 
 function IncluirCDATA(const aXML: string): string;

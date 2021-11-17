dc73d9e5b9e617a3a45d4e49287567eb934786fe
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-11-17 02:39:47 +0000

    Inicio de Pacote para ACBrPIXCD
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@23595 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Pacotes/Lazarus/ACBrPIXCD/ACBr_PIXCD.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ACBr_PIXCD;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ACBrPIXSchemas, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ACBr_PIXCD', @Register);
+end.

3ea020d032f9190f5c431b64d11e0212520aa9ab
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2021-11-18 16:59:03 +0000

    Adicionado Parser de QRCode Estático
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@23622 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrPIXCD/ACBrPIXQRCodeEstatico.pas
+++ Fontes/ACBrPIXCD/ACBrPIXQRCodeEstatico.pas
@@ -51,11 +51,18 @@ resourcestring
   sErroChaveInvalida = 'Chave Inválida: %s';
   sErroMCCForaDaFaixa = 'MCC fora da Faixa, 0100-9999';
   sErroPSSForaDaFaixa = 'Código ISPB fora da Faixa, 0-99999999';
+  sErroPayloadIndicator = 'Indicador de Formato inválido';
+  sErroCRCInvalido = 'CRC inválido';
+  sErroSintaxe = 'Erro na sintaxe';
+  sErroCurrency = 'Moeda %s não é aceita';
+  sErroCountryCode = 'País deve ser BR';
+  sErroGUIInvalido = 'GUI deve ser: '+cGUIPIX;
 
 const
   cMCCMinimo = 100;
   cMCCMaximo = 9999;
   cPSSMaximo = 99999999;
+  cCrcIdAndSize = '6304';
 
 type
 
@@ -63,8 +70,10 @@ type
 
   TACBrPIXQRCodeEstatico = class
   private
+    fCEPRecebedor: String;
     fChavePix: String;
     fCidadeRecebedor: String;
+    fIgnorarErrosQRCode: Boolean;
     finfoAdicional: String;
     fmcc: Integer;
     fNomeRecebedor: String;
@@ -72,27 +81,36 @@ type
     fTipoChavePix: TACBrPIXTipoChave;
     fTxId: String;
     fValor: Currency;
+    procedure DispararErroQRCode(const e: String);
     function GetQRCode: String;
-    procedure SetChavePix(AValue: String);
-    procedure SetCidadeRecebedor(AValue: String);
-    procedure SetinfoAdicional(AValue: String);
+    procedure SetCEPRecebedor(AValue: String);
+    procedure SetChavePix(const AValue: String);
+    procedure SetCidadeRecebedor(const AValue: String);
+    procedure SetinfoAdicional(const AValue: String);
     procedure SetMcc(AValue: Integer);
-    procedure SetNomeRecebedor(AValue: String);
+    procedure SetNomeRecebedor(const AValue: String);
     procedure SetPss(AValue: Integer);
-    procedure SetTxId(AValue: String);
+    procedure SetQRCode(const AValue: String);
+    procedure SetTxId(const AValue: String);
 
   protected
-    function GetMerchantAccountInformation: String;
-    function GetAdditionalDataField: String;
-    function GetCRC(const AQRCode: String): String;
+    procedure ExtrairDadosMerchantAccountInformation(const ADados: String);
+    procedure ExtrairDadosAdditionalDataField(const ADados: String);
+    function GerarMerchantAccountInformation: String;
+    function GerarAdditionalDataField: String;
+    function AdicionarCRC(const ADados: String): String;
 
+    procedure LerProximoEMV(const ADados: String; var p: Integer;
+      out id: String; out TamId: Integer; out ValorId: String);
   public
     constructor Create;
     procedure Clear;
     procedure Assign(Source: TACBrPIXQRCodeEstatico);
+    function VerificarCRC(const ADados: String): Boolean;
 
     property NomeRecebedor: String read fNomeRecebedor write SetNomeRecebedor;
     property CidadeRecebedor: String read fCidadeRecebedor write SetCidadeRecebedor;
+    property CEPRecebedor: String read fCEPRecebedor write SetCEPRecebedor;
     property ChavePix: String read fChavePix write SetChavePix;
     property TipoChavePix: TACBrPIXTipoChave read fTipoChavePix;
     property Valor: Currency read fValor write fValor;
@@ -101,7 +119,8 @@ type
     property pss: Integer read fpss write SetPss;    // Prestador de serviço de saque
     property mcc: Integer read fmcc write SetMcc;    // https://classification.codes/classifications/industry/mcc/
 
-    property QRCode: String read GetQRCode;
+    property QRCode: String read GetQRCode write SetQRCode;
+    property IgnorarErrosQRCode: Boolean read fIgnorarErrosQRCode write fIgnorarErrosQRCode;
   end;
 
 implementation
@@ -121,6 +140,7 @@ procedure TACBrPIXQRCodeEstatico.Clear;
 begin
   fNomeRecebedor := '';
   fCidadeRecebedor := '';
+  fCEPRecebedor := '';
   fChavePix := '';
   fTipoChavePix := tcNenhuma;
   finfoAdicional := '';
@@ -128,18 +148,21 @@ begin
   fValor := 0;
   fpss := -1;
   fmcc := 0;
+  fIgnorarErrosQRCode := False;
 end;
 
 procedure TACBrPIXQRCodeEstatico.Assign(Source: TACBrPIXQRCodeEstatico);
 begin
   fNomeRecebedor := Source.NomeRecebedor;
   fCidadeRecebedor := Source.CidadeRecebedor;
+  fCEPRecebedor := Source.CEPRecebedor;
   ChavePix := Source.ChavePix;
   finfoAdicional := Source.infoAdicional;
   fTxId := Source.TxId;
   fValor := Source.Valor;
   fpss := Source.pss;
   fmcc := Source.mcc;
+  fIgnorarErrosQRCode := Source.IgnorarErrosQRCode;
 end;
 
 function TACBrPIXQRCodeEstatico.GetQRCode: String;
@@ -148,30 +171,31 @@ var
   l: Integer;
 begin
   if (fTipoChavePix = tcNenhuma) then
-    raise EACBrPixException.CreateFmt(sErroChaveInvalida, [fChavePix]);
+    raise EACBrPixException.CreateFmt(ACBrStr(sErroChaveInvalida), [fChavePix]);
 
   if (fValor > 0) then
     vs := FormatarValorPIX(fValor)
   else
     vs := '';
 
-  Result := FormatarQRCodeId( 0, '01') +                          // Payload Format Indicator
-            FormatarQRCodeId(26, GetMerchantAccountInformation) + // Merchant Account Information
-            FormatarQRCodeId(52, IntToStrZero(fmcc, 4)) +         // Merchant Category Code
+  Result := FormatarQRCodeId(0, '01') +                            // Payload Format Indicator
+            FormatarQRCodeId(26, GerarMerchantAccountInformation) + // Merchant Account Information
+            FormatarQRCodeId(52, IntToStrZero(fmcc, 4)) +          // Merchant Category Code
             FormatarQRCodeId(53, IntToStr(cBRCurrency)) +
             FormatarQRCodeId(54, vs) +
             FormatarQRCodeId(58, cBRCountryCode) +
             FormatarQRCodeId(59, NomeRecebedor) +
             FormatarQRCodeId(60, CidadeRecebedor) +
-            FormatarQRCodeId(62, GetAdditionalDataField);
+            FormatarQRCodeId(61, CEPRecebedor) +
+            FormatarQRCodeId(62, GerarAdditionalDataField);
 
-  Result := GetCRC(Result);
+  Result := AdicionarCRC(Result);
 end;
 
-function TACBrPIXQRCodeEstatico.GetMerchantAccountInformation: String;
+function TACBrPIXQRCodeEstatico.GerarMerchantAccountInformation: String;
 var
   spss, id0, id1, id2, id3: String;
-  l, lim: Integer;
+  lt, lim: Integer;
 begin
   if (pss >= 0) then
     spss := IntToStrZero(fpss, 8)
@@ -184,8 +208,8 @@ begin
 
   if (infoAdicional <> '') then
   begin
-    l := Length(id0) + Length(id1) + Length(id3);
-    lim := cEMVLimit - 4 - l;
+    lt := Length(id0) + Length(id1) + Length(id3);
+    lim := cEMVLimit - 4 - lt;
     id2 := FormatarQRCodeId(2, copy(infoAdicional, 1, lim));
   end
   else
@@ -194,21 +218,20 @@ begin
   Result := id0 + id1 + id2 + id3;
 end;
 
-function TACBrPIXQRCodeEstatico.GetAdditionalDataField: String;
+function TACBrPIXQRCodeEstatico.GerarAdditionalDataField: String;
 begin
   Result := FormatarQRCodeId(5, IfEmptyThen(fTxId, cMPMValueNotInformed));
 end;
 
-function TACBrPIXQRCodeEstatico.GetCRC(const AQRCode: String): String;
+function TACBrPIXQRCodeEstatico.AdicionarCRC(const ADados: String): String;
 var
   s: String;
 begin
-  s := AQRCode +
-       '6304';
+  s := ADados + cCrcIdAndSize;
   Result := s + UpperCase(Crc16PIX(s));
 end;
 
-procedure TACBrPIXQRCodeEstatico.SetChavePix(AValue: String);
+procedure TACBrPIXQRCodeEstatico.SetChavePix(const AValue: String);
 var
   TipoChave: TACBrPIXTipoChave;
 begin
@@ -217,7 +240,7 @@ begin
 
   TipoChave := DetectarTipoChave(AValue);
   if (TipoChave = tcNenhuma) then
-    raise EACBrPixException.CreateFmt(sErroChaveInvalida, [AValue]);
+    raise EACBrPixException.CreateFmt(ACBrStr(sErroChaveInvalida), [AValue]);
 
   fChavePix := Trim(AValue);
   fTipoChavePix := TipoChave;
@@ -229,12 +252,12 @@ begin
     Exit;
 
   if (AValue <> 0) and (AValue < cMCCMinimo) or (AValue > cMCCMaximo) then
-    raise EACBrPixException.Create(sErroMCCForaDaFaixa);
+    raise EACBrPixException.Create(ACBrStr(sErroMCCForaDaFaixa));
 
   fmcc := AValue;
 end;
 
-procedure TACBrPIXQRCodeEstatico.SetNomeRecebedor(AValue: String);
+procedure TACBrPIXQRCodeEstatico.SetNomeRecebedor(const AValue: String);
 begin
   if (fNomeRecebedor = AValue) then
     Exit;
@@ -242,15 +265,23 @@ begin
   fNomeRecebedor := copy(Trim(AValue),1,25);
 end;
 
-procedure TACBrPIXQRCodeEstatico.SetCidadeRecebedor(AValue: String);
+procedure TACBrPIXQRCodeEstatico.SetCidadeRecebedor(const AValue: String);
 begin
   if (fCidadeRecebedor = AValue) then
     Exit;
 
-  fCidadeRecebedor := Trim(AValue);
+  fCidadeRecebedor := copy(Trim(AValue),1,15);
 end;
 
-procedure TACBrPIXQRCodeEstatico.SetinfoAdicional(AValue: String);
+procedure TACBrPIXQRCodeEstatico.SetCEPRecebedor(AValue: String);
+begin
+  if fCEPRecebedor = AValue then
+    Exit;
+
+  fCEPRecebedor := OnlyNumber(AValue);
+end;
+
+procedure TACBrPIXQRCodeEstatico.SetinfoAdicional(const AValue: String);
 begin
   if (finfoAdicional = AValue) then
     Exit;
@@ -264,23 +295,196 @@ begin
     Exit;
 
   if (AValue > cPSSMaximo) then
-    raise EACBrPixException.Create(sErroPSSForaDaFaixa);
+    raise EACBrPixException.Create(ACBrStr(sErroPSSForaDaFaixa));
 
   fpss := AValue;
 end;
 
-procedure TACBrPIXQRCodeEstatico.SetTxId(AValue: String);
+procedure TACBrPIXQRCodeEstatico.SetTxId(const AValue: String);
 var
   e: String;
 begin
   if (fTxId = AValue) then
     Exit;
 
+  if (AValue = cMPMValueNotInformed) then
+  begin
+    fTxId := '';
+    Exit;
+  end;
+
   e := ValidarTxId(AValue);
   if (e <> '') then
-    raise EACBrPixException.Create(e);
+    raise EACBrPixException.Create(ACBrStr(e));
 
   fTxId := AValue;
+end;
+
+procedure TACBrPIXQRCodeEstatico.SetQRCode(const AValue: String);
+var
+  s, id, ValorId: String;
+  p, l, TamId: Integer;
+begin
+  Clear;
+
+  s := Trim(AValue);
+  l := Length(s);
+  if not VerificarCRC(s) then
+    DispararErroQRCode(ACBrStr(sErroCRCInvalido));
+
+  p := 1;
+  while (p < l) do
+  begin
+    LerProximoEMV(s, p, id, TamId, ValorId);
+
+    if (id = '00') then
+    begin
+      if (ValorId <> '01') then
+        DispararErroQRCode(ACBrStr(sErroPayloadIndicator));
+    end
+
+    else if (id = '26') then
+      ExtrairDadosMerchantAccountInformation(ValorId)
+
+    else if (id = '52') then
+    begin
+      try
+        mcc := StrToInt(ValorId);
+      except
+        On E: Exception do
+          DispararErroQRCode(E.Message);
+      end;
+    end
+
+    else if (id = '53') then
+    begin
+      if (ValorId <> IntToStr(cBRCurrency)) then
+        DispararErroQRCode(Format(sErroCurrency, [ValorId]));
+    end
+
+    else if (id = '54') then
+    begin
+      try
+        Valor := StringToFloat(ValorId);
+      except
+        On E: Exception do
+          DispararErroQRCode(E.Message);
+      end;
+    end
+
+    else if (id = '58') then
+    begin
+      if (ValorId <> cBRCountryCode) then
+        DispararErroQRCode(sErroCountryCode);
+    end
+
+    else if (id = '59') then
+      NomeRecebedor := ValorId
+
+    else if (id = '60') then
+      CidadeRecebedor := ValorId
+
+    else if (id = '61') then
+      CEPRecebedor := ValorId
+
+    else if (id = '62') then
+      ExtrairDadosAdditionalDataField(ValorId);
+
+  end;
+end;
+
+procedure TACBrPIXQRCodeEstatico.ExtrairDadosMerchantAccountInformation(
+  const ADados: String);
+var
+  p, l, TamId: Integer;
+  id, ValorId: String;
+begin
+  p := 1;
+  l := Length(ADados);
+  while (p < l) do
+  begin
+    LerProximoEMV(ADados, p, id, TamId, ValorId);
+
+    if (id = '00') then
+    begin
+      if (ValorId <> cGUIPIX) then
+        DispararErroQRCode(sErroGUIInvalido);
+    end
+
+    else if (id = '01') then
+    begin
+      try
+        ChavePix := ValorId;
+      except
+        On E: Exception do
+          DispararErroQRCode(E.Message);
+      end;
+    end;
+  end;
+end;
+
+procedure TACBrPIXQRCodeEstatico.ExtrairDadosAdditionalDataField(
+  const ADados: String);
+var
+  p, l, TamId: Integer;
+  id, ValorId: String;
+begin
+  p := 1;
+  l := Length(ADados);
+  while (p < l) do
+  begin
+    LerProximoEMV(ADados, p, id, TamId, ValorId);
+
+    if (id = '05') then
+    begin
+      try
+        TxId := ValorId;
+      except
+        On E: Exception do
+          DispararErroQRCode(E.Message);
+      end;
+    end;
+  end;
+end;
+
+
+procedure TACBrPIXQRCodeEstatico.LerProximoEMV(const ADados: String; var p: Integer;
+  out id: String; out TamId: Integer; out ValorId: String);
+begin
+  id := copy(ADados, p, 2);
+  TamId := StrToIntDef(copy(ADados, p+2, 2), 0);
+  ValorId := '';
+  if (TamId = 0) then
+    if not fIgnorarErrosQRCode then
+      raise EACBrPixException.Create(ACBrStr(sErroSintaxe));
+
+  ValorId := copy(ADados, p+4, TamId);
+  p := p + 4 + TamId;
+end;
+
+procedure TACBrPIXQRCodeEstatico.DispararErroQRCode(const e: String);
+begin
+  if fIgnorarErrosQRCode then
+    Exit;
+
+  raise EACBrPixException.Create(e);
+end;
+
+function TACBrPIXQRCodeEstatico.VerificarCRC(const ADados: String): Boolean;
+var
+  qr, crc1, crc2: String;
+  l: Integer;
+begin
+  qr := Trim(ADados);
+  l := Length(qr);
+  crc1 := UpperCase(copy(qr, l-3, 4));
+  qr := copy(qr, 1, l-4);
+  Result := ((copy(qr, l-3, 4) <> cCrcIdAndSize));
+  if Result then
+  begin
+    crc2 := UpperCase(Crc16PIX(qr));
+    Result := (crc1 = crc2);
+  end;
 end;
 
 end.

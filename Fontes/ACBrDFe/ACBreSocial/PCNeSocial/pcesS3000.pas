3aaf820164b066902f64c81ecc493eb66ea9d173
embarbosa <embarbosa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-01-24 16:39:39 +0000

    -- ACBreSocial -- [-] Correção para  Evento S-3000. por ledware Veja: https://www.projetoacbr.com.br/forum/topic/65412-exclus%C3%A3o-do-evento-registros-do-evento-s-1260-comercializa%C3%A7%C3%A3o-da-produ%C3%A7%C3%A3o-rural-pessoa-f%C3%ADsica/?do=findComment&comment=427991&_rid=196
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24394 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesS3000.pas
+++ Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesS3000.pas
@@ -233,6 +233,8 @@ begin
         else
           GerarIdeFolhaPagto2(self.InfoExclusao.IdeFolhaPagto);
       end
+    else if ( self.InfoExclusao.tpEvento In [teS1280] ) then  //Fazendo isso, irá atender o layout novo e o antigo.
+       GerarIdeFolhaPagto(self.InfoExclusao.IdeFolhaPagto)
     else
       begin
         if ( self.InfoExclusao.IdeFolhaPagto.perApur = '' ) then

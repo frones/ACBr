e5a5278428e5d8e9b05261e90f35e39fd851d2a8
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-01-28 18:29:49 +0000

    [-] Adicionados os tipos de eventos comuns da NFe.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24464 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/NFe/Imports/Dinamico/Shared/NotaFiscal/TipoEventoNFe.cs
+++ Projetos/ACBrLib/Demos/C#/NFe/Imports/Dinamico/Shared/NotaFiscal/TipoEventoNFe.cs
@@ -16,8 +16,62 @@ namespace ACBrLib.NFe
         [EnumValue("110112")]
         teCancSubst,
 
-        [EnumValue("110113")]
-        teEPEC,
+        [EnumValue("110140")]
+        teEPECNFe,
+
+        [EnumValue("111500")]
+        tePedProrrog1,
+
+        [EnumValue("111501")]
+        tePedProrrog2,
+
+        [EnumValue("111502")]
+        teCanPedProrrog1,
+
+        [EnumValue("111503")]
+        teCanPedProrrog2,
+
+        [EnumValue("210210")]
+        teManifDestCiencia,
+
+        [EnumValue("210220")]
+        teManifDestDesconhecimento,
+
+        [EnumValue("210240")]
+        teManifDestOperNaoRealizada,
+
+        [EnumValue("610600")]
+        teRegistroCTe,
+
+        [EnumValue("610614")]
+        teMDFeAutorizadoComCTe,
+
+        [EnumValue("790700")]
+        teAverbacaoExportacao,
+
+        [EnumValue("990900")]
+        teVistoriaSuframa,
+
+        [EnumValue("990910")]
+        teConfInternalizacao,
+
+        [EnumValue("110180")]
+        teComprEntrega,
+
+        [EnumValue("110181")]
+        teCancComprEntrega,
+
+        [EnumValue("610554")]
+        teRegPasAutMDFeComCte,
+
+        [EnumValue("610510")]
+        teRegPasNfeProMDFe,
+
+        [EnumValue("610615")]
+        teCancelamentoMDFeAutComCTe,
+
+        [EnumValue("610610")]
+        teMDFeAutorizado,
 
         [EnumValue("110130")]
         teComprEntregaNFe,
@@ -28,16 +82,13 @@ namespace ACBrLib.NFe
         [EnumValue("110150")]
         teAtorInteressadoNFe,
 
-        [EnumValue("210200")]
-        teManifDestConfirmacao,
+        [EnumValue("610130")]
+        teComprEntregaCTe,
 
-        [EnumValue("210210")]
-        teManifDestCiencia,
+        [EnumValue("610131")]
+        teCancComprEntregaCTe,
 
-        [EnumValue("210220")]
-        teManifDestDesconhecimento,
-
-        [EnumValue("210240")]
-        teManifDestOperNaoRealizada,
+        [EnumValue("610601")]
+        teCTeCancelado,
     }
 }

97fe2885a56751f4f44c5b109d23666b37da5ac9
rfteno <rfteno@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-02-09 13:19:28 +0000

    [*] Atualizado a lista de bancos da ACBrLib.Boleto em C#.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24589 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/Boleto/Demo/ACBrLibBoleto.Demo/Program.cs
+++ Projetos/ACBrLib/Demos/C#/Boleto/Demo/ACBrLibBoleto.Demo/Program.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using ACBrLib.Boleto;
 
 namespace ACBrLibBoleto.Demo
 {
-    static class Program
+    internal static class Program
     {
+        private static ACBrBoleto bol;
+
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bol = new ACBrBoleto("[Memory]");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmMain());

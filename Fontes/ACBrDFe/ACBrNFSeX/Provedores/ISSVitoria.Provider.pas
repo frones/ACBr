4a89ea8046cd0b4479d343907ed2babe80c86311
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-02-28 17:37:46 +0000

    28/02/2022 -- ISSVitoria.Provider -- [*] Possível correção no TratarXmlRetornado    Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24743 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/ISSVitoria.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/ISSVitoria.Provider.pas
@@ -297,6 +297,7 @@ function TACBrNFSeXWebserviceISSVitoria200.TratarXmlRetornado(
 begin
   Result := inherited TratarXmlRetornado(aXML);
 
+  Result := RemoverCaracteresDesnecessarios(Result);
   Result := ParseText(AnsiString(Result), True, False);
   Result := RemoverDeclaracaoXML(Result);
   Result := RemoverIdentacao(Result);

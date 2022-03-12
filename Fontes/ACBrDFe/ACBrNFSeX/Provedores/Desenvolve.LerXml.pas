b48b3d8d2580f99db9a50b88bd89af2628cc4cd9
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-03-12 15:33:16 +0000

    12/03/2022 -- Desenvolve.LerXml -- [*] Acrescentado o NormatizarXml antes da leitura do XML.    Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24879 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Desenvolve.LerXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Desenvolve.LerXml.pas
@@ -46,15 +46,26 @@ type
   TNFSeR_Desenvolve203 = class(TNFSeR_ABRASFv2)
   protected
 
+    function NormatizarXml(const aXml: string): string; override;
   public
 
   end;
 
 implementation
 
+uses
+  ACBrUtil;
+
 //==============================================================================
 // Essa unit tem por finalidade exclusiva ler o XML do provedor:
 //     Desenvolve
 //==============================================================================
+
+{ TNFSeR_Desenvolve203 }
+
+function TNFSeR_Desenvolve203.NormatizarXml(const aXml: string): string;
+begin
+  Result := TiraAcentos(aXml);
+end;
 
 end.

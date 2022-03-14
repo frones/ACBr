d08b2848b0a320ce358a369f8766b01242a25970
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-03-14 15:13:13 +0000

    14/03/2022 -- pcnConversao -- [*] Novos enumerados visando os novos eventos do MDF-e.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24888 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/PCNComum/pcnConversao.pas
+++ Fontes/PCNComum/pcnConversao.pas
@@ -138,7 +138,8 @@ type
                   teInclusaoDFe, teAutorizadoSubstituicao, teAutorizadoAjuste,
                   teLiberacaoPrazoCancelado, tePagamentoOperacao, teExcessoBagagem,
                   teEncerramentoFisco, teComprEntregaNFe, teCancComprEntregaNFe,
-                  teAtorInteressadoNFe, teComprEntregaCTe, teCancComprEntregaCTe);
+                  teAtorInteressadoNFe, teComprEntregaCTe, teCancComprEntregaCTe,
+                  teConfirmaServMDFe, teAlteracaoPagtoServMDFe);
 
   TpcnIndicadorEmissor = (ieTodos, ieRaizCNPJDiferente);
   TpcnIndicadorContinuacao = (icNaoPossuiMaisDocumentos, icPossuiMaisDocumentos);
@@ -216,7 +217,7 @@ type
   end;
 
 const
-  TpcnTpEventoString : array[0..68] of String =('-99999', '110110', '110111',
+  TpcnTpEventoString : array[0..70] of String =('-99999', '110110', '110111',
                                                 '210200', '210210', '210220',
                                                 '210240', '110112', '110113',
                                                 '110114', '110160', '310620',
@@ -238,7 +239,8 @@ const
                                                 '110115', '240140', '240150',
                                                 '240170', '110116', '110117',
                                                 '310112', '110130', '110131',
-                                                '110150', '610130', '610131');
+                                                '110150', '610130', '610131',
+                                                '110117', '110118');
 
   DFeUF: array[0..26] of String =
   ('AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA',

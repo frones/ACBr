7abe5417b5fcf415f4f66f7fcc692e5295af6c95
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-03-22 11:37:14 +0000

    22/03/2022 -- Saatri.Provider -- [*] Incluido a função TratarXmlRetornado.    Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@24973 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Saatri.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Saatri.Provider.pas
@@ -58,6 +58,8 @@ type
     function Cancelar(ACabecalho, AMSG: String): string; override;
     function SubstituirNFSe(ACabecalho, AMSG: String): string; override;
 
+    function TratarXmlRetornado(const aXML: string): string; override;
+
     property DadosUsuario: string read GetDadosUsuario;
   end;
 
@@ -74,7 +76,7 @@ type
 implementation
 
 uses
-  ACBrNFSeX, ACBrDFeException,
+  ACBrUtil, ACBrDFeException, ACBrNFSeX,
   Saatri.GravarXml, Saatri.LerXml;
 
 { TACBrNFSeProviderSaatri201 }
@@ -332,6 +334,14 @@ begin
                      DadosUsuario,
                      ['outputXML', 'SubstituirNfseResposta'],
                      ['xmlns:nfse="http://nfse.abrasf.org.br"']);
+end;
+
+function TACBrNFSeXWebserviceSaatri201.TratarXmlRetornado(
+  const aXML: string): string;
+begin
+  Result := inherited TratarXmlRetornado(aXML);
+
+  Result := ParseText(AnsiString(Result), True, False);
 end;
 
 end.

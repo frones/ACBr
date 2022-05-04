565f17e3bfb5a1327f0da96a8a4bad7f29a4dbec
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-05-04 21:08:05 +0000

    --ACBrBancoCredisis-- [-] TK-2597 Ajuste na propriedade de geração de data de Multa na remessa do CNAB240   Contribuição D'Avila Junior   https://www.projetoacbr.com.br/forum/topic/67103-remessa-banco-credisis-acbrbancocredisis/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@25426 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBancoCredisis.pas
+++ Fontes/ACBrBoleto/ACBrBancoCredisis.pas
@@ -455,7 +455,7 @@ begin
               PadRight('',15,' ')    {valor/percentual 3, ainda não implementado}      + // 51 - 65 Valor/Percentual Desconto 3,  *C023 (Manual)
               IfThen((PercentualMulta <> null) and (PercentualMulta > 0), '2', '1')    + // 66 - 66 Código da Multa 1-Valor Fixo / 2-Percentual
               IfThen((PercentualMulta <> null) and (PercentualMulta > 0),
-                    FormatDateTime('ddmmyyyy', DataMoraJuros), PadRight('',8,' '))     + // 67 - 74 Se cobrar informe a data para iniciar a cobrança ou informe EM BRANCO para CONSIDERAR a DATA DO VENCIMENTO
+                    FormatDateTime('ddmmyyyy', DataMulta), PadRight('',8,' '))         + // 67 - 74 Se cobrar informe a data para iniciar a cobrança ou informe EM BRANCO para CONSIDERAR a DATA DO VENCIMENTO
               IfThen(PercentualMulta > 0,
                      IntToStrZero(round(PercentualMulta * 100), 13) + '00',
                      PadRight('', 15, '0'))                                            + // 75 - 89 Percentual de multa. Informar zeros se não cobrar

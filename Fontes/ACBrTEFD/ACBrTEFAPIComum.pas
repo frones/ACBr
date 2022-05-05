09ef1e051c1795e8864c7ebfdc846107e0b7216f
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-05-05 22:30:14 +0000

    -- ACBrTEFAPIComum -- [*] Propriedade "ImprimeViaClienteReduzida: Boolean", modificada de Default "True"     para "False"     (por: DSA)
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@25436 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTEFD/ACBrTEFAPIComum.pas
+++ Fontes/ACBrTEFD/ACBrTEFAPIComum.pas
@@ -113,7 +113,7 @@ type
     Property SuportaSaque: Boolean read fSuportaSaque write fSuportaSaque default False;
     Property SuportaDesconto: Boolean read fSuportaDesconto write fSuportaDesconto default False;
     property ImprimeViaClienteReduzida: Boolean read fImprimeViaClienteReduzida
-      write fImprimeViaClienteReduzida default True;
+      write fImprimeViaClienteReduzida default False;
     property SuportaViasDiferenciadas: Boolean read fSuportaViasDiferenciadas
       write fSuportaViasDiferenciadas default True;
     property UtilizaSaldoTotalVoucher: Boolean read fUtilizaSaldoTotalVoucher
@@ -442,7 +442,7 @@ begin
   fNomeSoftwareHouse := '';
   fNomeAplicacao := '';
   fVersaoAplicacao := '';
-  fImprimeViaClienteReduzida := True;
+  fImprimeViaClienteReduzida := False;
   fSuportaDesconto := False;
   fSuportaSaque := False;
   fSuportaViasDiferenciadas := True;

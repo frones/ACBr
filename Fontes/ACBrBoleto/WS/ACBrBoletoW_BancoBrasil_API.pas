a2ac07c7e6639563b8c2c9054f644ce8fe5f85ce
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-06-15 18:19:20 +0000

    15/06/2022 --ACBrBoletoW_BancoBrasil_API-- [*] TK-2784 BBAPI Ajuste no tratamento de caracteres nas mensagens de envio   Por : Pandaaa / Aggille Sistemas de Gestão
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@25868 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/WS/ACBrBoletoW_BancoBrasil_API.pas
+++ Fontes/ACBrBoleto/WS/ACBrBoletoW_BancoBrasil_API.pas
@@ -354,7 +354,7 @@ begin
         Json.Add('indicadorPermissaoRecebimentoParcial').Value.AsString := 'S';
 
       Json.Add('numeroTituloBeneficiario').Value.AsString               := Copy(Trim(UpperCase(Titulos.NumeroDocumento)),0,15);
-      Json.Add('campoUtilizacaoBeneficiario').Value.AsString            := Copy(Trim(StringReplace(UpperCase(Titulos.Mensagem.Text),'\r\n',' ',[rfReplaceAll])),0,30);
+      Json.Add('campoUtilizacaoBeneficiario').Value.AsString            := Copy(Trim(StringReplace(UpperCase(OnlyAlphaNum(Titulos.Mensagem.Text)),'\r\n',' ',[rfReplaceAll])),0,30);
       Json.Add('numeroTituloCliente').Value.AsString                    := Boleto.Banco.MontarCampoNossoNumero(Titulos);
       Json.Add('mensagemBloquetoOcorrencia').Value.AsString             := UpperCase(Copy(Trim(Titulos.Instrucao1 +' '+Titulos.Instrucao2+' '+Titulos.Instrucao3),0,165));
       GerarDesconto(Json);

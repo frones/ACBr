5d880fa2323e705b8459d3385a1337b70d6d0260
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-07-25 21:19:37 +0000

    25/07/2022 Correção no sobrenome do colaborador.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@26217 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNF3e/DANF3e/EscPos/ACBrNF3eDANF3eESCPOSReg.pas
+++ Fontes/ACBrDFe/ACBrNF3e/DANF3e/EscPos/ACBrNF3eDANF3eESCPOSReg.pas
@@ -3,9 +3,9 @@
 {  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
 { mentos de Automação Comercial utilizados no Brasil                           }
 {                                                                              }
-{ Direitos Autorais Reservados (c) 2020 Daniel Simoes de Almeida               }
+{ Direitos Autorais Reservados (c) 2022 Daniel Simoes de Almeida               }
 {                                                                              }
-{ Colaboradores nesse arquivo: Italo Jurisato Junior                           }
+{ Colaboradores nesse arquivo: Italo Giurizzato Junior                         }
 {                                                                              }
 {  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
 { Componentes localizado em      http://www.sourceforge.net/projects/acbr      }

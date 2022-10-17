fbd81926f14c3c0d3537a5feba6eacce3971c98c
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-10-17 13:26:24 +0000

    ACBrLibNFe ajustes na demo do NodeJS - #TK-256
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@27068 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/Node.js/NFe/lib_test_teste-acbrlibnfe.js
+++ Projetos/ACBrLib/Demos/Node.js/NFe/lib_test_teste-acbrlibnfe.js
@@ -46,16 +46,117 @@ var eChaveCrypt = '';
 
 async function getNFe() {
   var libm = ffi.Library(pathDllACBrLibNFe, {
+
+    //Métodos da Biblioteca
+
+    // NFE_Inicializar([eArqConfig, eChaveCrypt]);
     NFE_Inicializar: ['int', ['string', 'string']],
-    NFE_ConfigLer: ['int', ['string']],
-    NFE_CarregarINI: ['int', ['string']],
-    NFE_CarregarXML: ['int', ['string']],
-    NFE_UltimoRetorno: ['int', ['string', 'string']],
-    NFE_ImprimirPDF: ['int', []],
+    // NFE_Finalizar();
     NFE_Finalizar: ['int', []],
-    NFE_Assinar: ['int', []],
-    NFE_Validar: ['int', []],
+    // NFE_UltimoRetorno(sMensagem, ref esTamanho);
+    NFE_UltimoRetorno: ['int', ['string', 'string']],
+    // NFE_Nome(sNome, ref esTamanho);
+    NFE_Nome: ['int',['string','string']],
+    // NFE_Versao(sVersao, ref esTamanho);
+    NFE_Versao: ['int', ['string','string']],
+
+    //Métodos de Configuração
+
+    // NFE_ConfigLer([eArqConfig]);
+    NFE_ConfigLer: ['int', ['string']],
+    // NFE_ConfigGravar([eArqConfig]);
+    NFE_ConfigGravar: ['int', ['string']],
+    // NFE_ConfigLerValor(eSessao, eChave, sValor, esTamanho);
+    NFE_ConfigLerValor: ['int',['string','string','string','string']],
+    // NFE_ConfigGravarValor(eSessao, eChave, sValor);
     NFE_ConfigGravarValor: ['int', ['string', 'string', 'string']],
+    // NFE_ConfigImportar([eArqConfig]);
+    NFE_ConfigImportar: ['int',['string']],
+    // NFE_ConfigExportar(sMensagem, ref esTamanho);
+    NFE_ConfigExportar: ['int',['string','string']],
+
+    //Métodos NFe
+
+    // NFE_CarregarXML(eArquivoOuXML);
+    NFE_CarregarXML: ['int', ['string']], 
+    // NFE_CarregarINI(eArquivoOuINI);
+    NFE_CarregarINI: ['int', ['string']],
+    // NFE_ObterXml(AIndex, sResposta, esTamanho);
+    NFE_ObterXml: ['int',['int', 'string', 'string']],
+    // NFE_GravarXml(AIndex, [eNomeArquivo], [ePathArquivo]);
+    NFE_GravarXml: ['int', ['int','string','string']],
+    // NFE_ObterIni(AIndex, sResposta, esTamanho);
+    NFE_ObterIni: ['int', ['int','string','string']],
+    // NFE_GravarIni(AIndex, eNomeArquivo, [ePathArquivo]);
+    NFE_GravarIni: ['int',['int', 'string', 'string']],
+    // NFE_CarregarEventoXML(eArquivoOuXML);
+    NFE_CarregarEventoXML: ['int',['string']],
+    // NFE_CarregarEventoINI(eArquivoOuINI);
+    NFE_CarregarEventoINI: ['int',['string']],
+    // NFE_LimparLista();
+    NFE_LimparLista: ['int',[]],
+    // NFE_LimparListaEventos();
+    NFE_LimparListaEventos: ['int',[]],
+    // NFE_Assinar();
+    NFE_Assinar: ['int', []],
+    // NFE_Validar();
+    NFE_Validar: ['int', []],
+    // NFE_ValidarRegrasdeNegocios(sResposta, esTamanho);
+    NFE_ValidarRegrasdeNegocios: ['int',['string','string']],
+    // NFE_VerificarAssinatura(sResposta, esTamanho);
+    NFE_VerificarAssinatura: ['int',['string','string']],
+    // NFE_GerarChave(ACodigoUF, ACodigoNumerico, AModelo, ASerie, ANumero, ATpEmi, AEmissao, ACNPJCPF, sResposta, esTamanho);
+    NFE_GerarChave: ['int',['int','int','int','int','int','int','string','string','string','string']],
+    // NFE_ObterCertificados(sResposta, esTamanho);
+    NFE_ObterCertificados: ['int',['string','string']],
+    // NFE_GetPath(ATipo, sResposta, esTamanho);
+    NFE_GetPath: ['int',['int','string','string']],
+    // NFE_GetPathEvento(ACodEvento, sResposta, esTamanho);
+    NFE_GetPathEvento: ['int',['int','string','string']],
+    // NFE_StatusServico(sResposta, esTamanho);
+    NFE_StatusServico: ['int', ['status','string']],
+    // NFE_Consultar( eChaveOuNFe, AExtrairEventos, sResposta, esTamanho);
+    NFE_Consultar: ['int',['string','bool','string','string']],
+    // NFE_ConsultarRecibo(ARecibo, sResposta, esTamanho);
+    NFE_ConsultarRecibo: ['int',['string','string','string']],
+    // NFE_ConsultaCadastro(cUF, nDocumento, nIE, sResposta, esTamanho);
+    NFE_ConsultaCadastro: ['int',['string','string','bool','string','string']],
+    // NFE_Inutilizar(ACNPJ, AJustificativa, Ano, Modelo, Serie, NumeroInicial, NumeroFinal, sResposta, esTamanho);
+    NFE_Inutilizar: ['int',['string','string','int','int','int','int','int','string','string']],
+    // NFE_Enviar( (ALote, AImprimir, ASincrono, AZipado, sResposta, esTamanho);
+    NFE_Enviar: ['int', ['int','bool','bool','bool','string','string']],
+    // NFE_Cancelar(eChave, eJustificativa, eCNPJ, ALote, sResposta, esTamanho);
+    NFE_Cancelar: ['int',['string','string','string','int','string','string']],
+    // NFE_EnviarEvento(idLote, sResposta, esTamanho);
+    NFE_EnviarEvento: ['int',['int','string','string']],
+    // NFE_DistribuicaoDFePorUltNSU(AcUFAutor, eCNPJCPF, eultNSU, sResposta, esTamanho);
+    NFE_DistribuicaoDFePorUltNSU: ['int',['int','string','string','string','string']],
+    // NFE_DistribuicaoDFePorNSU(AcUFAutor, eCNPJCPF, eNSU, sResposta, esTamanho);
+    NFE_DistribuicaoDFePorNSU: ['int',['int','string','string','string','string']],
+    // NFE_DistribuicaoDFePorChave(AcUFAutor, eCNPJCPF, eChave, sResposta, esTamanho);
+    NFE_DistribuicaoDFePorChave: ['int',['int','string','string','string','string']],
+    // NFE_EnviarEmail(ePara, eXMLNFe, AEnviaPDF, eAssunto, eCC, eAnexos, eMensagem);
+    NFE_EnviarEmail: ['int',['string','string','bool','string','string','string','string']],
+    // NFE_EnviarEmailEvento(ePara, eChaveEvento, eChaveNFe, AEnviaPDF, eAssunto, eCC, eAnexos, eMensagem);
+    NFE_EnviarEmailEvento: ['int',['string','string','string','bool','string','string','string','string']],
+    // NFE_Imprimir([cImpressora], [nNumCopias], [cProtocolo], [bMostrarPreview], [cMarcaDagua], [bViaConsumidor], [bSimplificado]);
+    NFE_Imprimir: ['int',['string','int','string','string','string','string','string',]],
+    // NFE_ImprimirPDF();
+    NFE_ImprimirPDF: ['int', []],
+    // NFE_SalvarPDF(sResposta, esTamanho);
+    NFE_SalvarPDF: ['int',['string','string']],
+    // NFE_ImprimirEvento(eArquivoXmlNFe, eArquivoXmlEvento);
+    NFE_ImprimirEvento: ['int',['string','string']],
+    // NFE_ImprimirEventoPDF(eArquivoXmlNFe, eArquivoXmlEvento);
+    NFE_ImprimirEventoPDF: ['int',['string','string']],
+    // NFE_SalvarEventoPDF(eArquivoXmlNFe, eArquivoXmlEvento);
+    NFE_SalvarEventoPDF: ['int',['string','string']],
+    // NFE_ImprimirInutilizacao(eArquivoXml);
+    NFE_ImprimirInutilizacao: ['int', ['string']],
+    // NFE_ImprimirInutilizacaoPDF(eArquivoXml);
+    NFE_ImprimirInutilizacaoPDF: ['int',['string']],
+    // NFE_SalvarInutilizacaoPDF(eArquivoXml);
+    NFE_SalvarInutilizacaoPDF: ['int',['string']],     
   });
 
   var inicio = 2;
@@ -65,43 +166,43 @@ async function getNFe() {
   let aloc_esTamanho = ref.alloc('int', buflength);
 
   inicio = libm.NFE_Inicializar(eArqConfig, eChaveCrypt);
-  console.log('iniciou >>>>>>> ${inicio}');
+  console.log(`iniciou >>>>>>> ${inicio}`);
 
   inicio = libm.NFE_ConfigGravarValor('DFe', 'ArquivoPFX', path.join(path.resolve(__dirname, 'certificado'), 'nome_do_certificado.pfx'));
   inicio = libm.NFE_ConfigGravarValor('DFe', 'Senha', '1234');
   inicio = libm.NFE_ConfigGravarValor('NFe', 'PathSchemas', path.resolve(__dirname, 'Schemas', 'NFe'));
-  console.log('Set Configurações ${inicio}');
+  console.log(`Set Configurações ${inicio}`);
 
   inicio = libm.NFE_CarregarXML(pathXML);
-  console.log('carregar xml >>>>>>> ${inicio}');
+  console.log(`carregar xml >>>>>>> ${inicio}`);
 
   inicio = libm.NFE_UltimoRetorno(aloc_sResposta, aloc_esTamanho);
-  console.log('ultmio retorno >>>>>>>> ${inicio}');
-  console.log('Mensagem Ultimo Retorno: ', aloc_sResposta.toString());
+  console.log(`ultmio retorno >>>>>>>> ${inicio}`);
+  console.log(`Mensagem Ultimo Retorno: `, aloc_sResposta.toString());
 
   inicio = libm.NFE_Assinar();
-  console.log('assinar xml >>>>>>> ${inicio}');
+  console.log(`assinar xml >>>>>>> ${inicio}`);
 
   aloc_sResposta = Buffer.alloc(buflength);
   aloc_esTamanho = ref.alloc('int', buflength);
 
   inicio = libm.NFE_UltimoRetorno(aloc_sResposta, aloc_esTamanho);
-  console.log('ultmio retorno apos assinar >>>>>>>> ${inicio}');
-  console.log('Mensagem: ', aloc_sResposta.toString('ascii'));
+  console.log(`ultimo retorno apos assinar >>>>>>>> ${inicio}`);
+  console.log(`Mensagem: `, aloc_sResposta.toString('ascii'));
 
   inicio = libm.NFE_Validar();
-  console.log('validar xml >>>>>>> ${inicio}');
+  console.log(`validar xml >>>>>>> ${inicio}`);
 
 
   aloc_sResposta = Buffer.alloc(buflength);
   aloc_esTamanho = ref.alloc('int', buflength);
 
   inicio = libm.NFE_UltimoRetorno(aloc_sResposta, aloc_esTamanho);
-  console.log('ultmio retorno apos validar >>>>>>>> ${inicio}');
-  console.log('Mensagem: ', aloc_sResposta.toString());
+  console.log(`ultmio retorno apos validar >>>>>>>> ${inicio}`);
+  console.log(`Mensagem: `, aloc_sResposta.toString());
 
   inicio = libm.NFE_Finalizar();
-  console.log('finalizar >>>>>>>> ${inicio}');
+  console.log(`finalizar >>>>>>>> ${inicio}`);
 }
 
 getNFe();

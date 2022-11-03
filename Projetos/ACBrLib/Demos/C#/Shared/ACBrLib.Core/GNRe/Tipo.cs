d4a68b195f58770af1e7a95867a021e0f4e8a2be
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-11-03 14:52:59 +0000

    Atualização classes ACBrLibGNRe
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@27283 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/GNRe/Tipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACBrLib.Core.GNRe
+{
+    public enum Tipo
+    {
+        CNPJ = 1,
+        CPF = 2
+    }
+}

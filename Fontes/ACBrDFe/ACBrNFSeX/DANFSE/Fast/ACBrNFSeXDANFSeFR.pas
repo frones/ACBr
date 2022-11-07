f72ae70a1a02fce55608526d96dcada29f94850f
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-11-07 12:00:25 +0000

    [+] Adição propriedade incorporar fonte e backgroud na NFSeX em Fast
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@27304 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
@@ -94,6 +94,8 @@ type
     frxServicos: TfrxDBDataset;
     frxParametros: TfrxDBDataset;
     frxItensServico: TfrxDBDataset;
+		FIncorporarFontesPdf: Boolean;
+		FIncorporarBackgroundPdf:Boolean;
 
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -104,6 +106,8 @@ type
   published
     property FastFile: String read FFastFile write FFastFile;
     property EspessuraBorda: Integer read FEspessuraBorda write FEspessuraBorda;
+		property IncorporarFontesPdf : boolean read FIncorporarFontesPdf write FIncorporarFontesPdf;
+		property IncorporarBackgroundPdf : boolean read FIncorporarBackgroundPdf write FIncorporarBackgroundPdf;
   end;
 
 implementation
@@ -121,6 +125,8 @@ begin
   FFastFile := '';
   FEspessuraBorda := 1;
   CriarDataSetsFrx;
+	FIncorporarFontesPdf := false;
+	FIncorporarBackgroundPdf := false;
 end;
 
 destructor TACBrNFSeXDANFSeFR.Destroy;
@@ -190,7 +196,8 @@ begin
     frxPDFExport.Creator := Sistema;
     frxPDFExport.Subject := TITULO_PDF;
     frxPDFExport.EmbeddedFonts := False;
-    frxPDFExport.Background := False;
+    frxPDFExport.Background := IncorporarBackgroundPdf;
+		frxPDFExport.EmbeddedFonts := IncorporarFontesPdf;
 
     OldShowDialog := frxPDFExport.ShowDialog;
     try

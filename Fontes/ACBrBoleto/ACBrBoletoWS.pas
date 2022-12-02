63641382c569e3bfa8bf7b911e897ecfa6312cdd
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2022-12-02 15:09:18 +0000

    --ACBrBoletoWS-- Revert 25580 [*]Correção de AV quando WS com erro e não houver JSON  por : Victor H Gonzales - Pandaaa
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@27657 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBoletoWS.pas
+++ Fontes/ACBrBoleto/ACBrBoletoWS.pas
@@ -477,9 +477,8 @@ begin
 
         end
         else
-          FErroComunicacao := 'HTTP_Code='+ IntToStr(FSSL.HTTPResultCode);
-          if (AJson <> nil) then
-            FErroComunicacao := FErroComunicacao + ' Erro='+ AJson.S['error_description'];
+          FErroComunicacao := 'HTTP_Code='+ IntToStr(FSSL.HTTPResultCode)
+                             + ' Erro='+ AJson.S['error_description'];
 
       finally
         AJSon.Free;

d40e03d960021a2b03f4ba7a059507a3e54af10a
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-01-09 20:11:19 +0000

    Ajuste classe ProcessoEmissao.cs
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@28129 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/DFe/ProcessoEmissao.cs
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/DFe/ProcessoEmissao.cs
@@ -3,7 +3,7 @@
     public enum ProcessoEmissao
     {
         peAplicativoContribuinte = 0,
-        peAvulsaFisco = 1,
+		peAvulsaFisco = 1,
         peAvulsaContribuinte = 2,
         peContribuinteAplicativoFisco = 3
     }

f5b38e4316406691b507d52af8e1f7b850a2d9bf
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-03-22 17:00:27 +0000

    22/03/2023 --ACBrNFSeXDANFSeFR-- [-]TK-3741 Correção na leitura da aliquota, 2 propriedades mesmo nome   por: Victor H Gonzales - Pandaaa
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@28821 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
@@ -1270,7 +1270,8 @@ begin
     LCDS.FieldByName('ValorServicos').AsFloat := LValores.ValorServicos;
     LCDS.FieldByName('ValorIss').AsFloat      := LValoresNFSe.ValorIss;
     LCDS.FieldByName('BaseCalculo').AsFloat   := LValoresNFSe.BaseCalculo;
-    LCDS.FieldByName('Aliquota').AsFloat      := LValoresNFSe.Aliquota;
+    if LValoresNFSe.Aliquota <> 0 then
+      LCDS.FieldByName('Aliquota').AsFloat      := LValoresNFSe.Aliquota;
 
     if LValoresNFSe.ValorLiquidoNfse = 0 then
       LValoresNFSe.ValorLiquidoNfse := LValoresNFSe.BaseCalculo;

9f156b99873aaedb40357c497f6e44dd00ec3d2b
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-03-23 19:37:50 +0000

    Testes unitários ACBrLibNFSe
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@28838 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Testes/NFSe/ACBrLibNFSeTestCase.pas
+++ Projetos/ACBrLib/Testes/NFSe/ACBrLibNFSeTestCase.pas
@@ -57,6 +57,16 @@ type
     procedure Test_NFSE_Versao;
     procedure Test_NFSE_ConfigLerValor;
     procedure Test_NFSE_ConfigGravarValor;
+    procedure Test_NFSE_LimparLista;
+    procedure Test_NFSE_CarregarXML;
+    procedure Test_NFSE_CarregarINI;
+    procedure Test_NFSE_ObterXml;
+    procedure Test_NFSE_GravarXml;
+    procedure Test_NFSE_ObterIni;
+    procedure Test_NFSE_GravarIni;
+    procedure Test_NFSE_ObterCertificados;
+    procedure Test_NFSE_Emitir;
+
   end;
 
 implementation
@@ -81,39 +91,34 @@ procedure TTestACBrNFSeLib.Test_NFSE_Inicializar;
 var
   Handle: THandle;
 begin
-  AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
-  AssertEquals(ErrOk, NFSE_Finalizar(Handle));
+    AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
+    AssertEquals(ErrOk, NFSE_Finalizar(Handle));
 end;
 
 procedure TTestACBrNFSeLib.Test_NFSE_Inicializar_Ja_Inicializado;
 var
   Handle: THandle;
 begin
-  AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
-  AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
-  AssertEquals(ErrOk, NFSE_Finalizar(Handle));
+    AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
+    AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
+    AssertEquals(ErrOk, NFSE_Finalizar(Handle));
 end;
 
 procedure TTestACBrNFSeLib.Test_NFSE_Finalizar;
 var
   Handle: THandle;
 begin
-  AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
-  AssertEquals(ErrOk, NFSE_Finalizar(Handle));
+    AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
+    AssertEquals(ErrOk, NFSE_Finalizar(Handle));
 end;
 
 procedure TTestACBrNFSeLib.Test_NFSE_Finalizar_Ja_Finalizado;
 var
   Handle: THandle;
 begin
-  try
     AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
     AssertEquals(ErrOk, NFSE_Finalizar(Handle));
     //AssertEquals(ErrOk, NFSE_Finalizar(Handle));
-  except
-  on E: Exception do
-    ShowMessage( 'Error: '+ E.ClassName + #13#10 + E.Message );
-  end;
 end;
 
 procedure TTestACBrNFSeLib.Test_NFSE_Nome_Obtendo_LenBuffer;
@@ -124,9 +129,11 @@ var
 begin
   // Obtendo o Tamanho //
   AssertEquals(ErrOk, NFSE_Inicializar(Handle,'',''));
+
   Bufflen := 0;
   AssertEquals(ErrOk, NFSE_Nome(Handle,Nil, Bufflen));
   AssertEquals(Length(CLibNFSeNome), Bufflen);
+
   AssertEquals(ErrOk, NFSE_Finalizar(Handle));
 end;
 
@@ -137,11 +144,13 @@ var
   Handle: THandle;
 begin
   AssertEquals(ErrOK, NFSE_Inicializar(Handle, '',''));
+
   Bufflen := Length(CLibNFSeNome);
   AStr := Space(Bufflen);
   AssertEquals(ErrOk, NFSE_Nome(Handle, PChar(AStr), Bufflen));
   AssertEquals(Length(CLibNFSeNome), Bufflen);
   AssertEquals(CLibNFSeNome, AStr);
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 
@@ -152,12 +161,14 @@ var
   Handle: THandle;
 begin
   AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
   Bufflen := Length(CLibNFSeNome)*2;
   AStr := Space(Bufflen);
   AssertEquals(ErrOk, NFSE_Nome(Handle, PChar(AStr), Bufflen));
   AStr := copy(AStr, 1, Bufflen);
   AssertEquals(Length(CLibNFSeNome), Bufflen);
   AssertEquals(CLibNFSeNome, AStr);
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 
@@ -168,11 +179,13 @@ var
   Handle: THandle;
 begin
   AssertEquals(ErrOK, NFSE_Inicializar(Handle,  '', ''));
-  Bufflen := 14;
+
+  Bufflen := 11;
   AStr := Space(Bufflen);
   AssertEquals(ErrOk, NFSE_Nome(Handle,  PChar(AStr), Bufflen));
-  AssertEquals(14, Bufflen);
-  AssertEquals(copy(CLibNFSeNome,1,14), AStr);
+  AssertEquals(11, Bufflen);
+  AssertEquals(copy(CLibNFSeNome,1,11), AStr);
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 
@@ -184,6 +197,7 @@ var
 begin
   // Obtendo o Tamanho //
   AssertEquals(ErrOK, NFSE_Inicializar(Handle,  '', ''));
+
   Bufflen := 0;
   AssertEquals(ErrOk, NFSE_Versao(Handle,  Nil, Bufflen));
   AssertEquals(Length(CLibNFSeVersao), Bufflen);
@@ -193,6 +207,7 @@ begin
   AssertEquals(ErrOk, NFSE_Versao(Handle, PChar(AStr), Bufflen));
   AssertEquals(Length(CLibNFSeVersao), Bufflen);
   AssertEquals(CLibNFSeVersao, AStr);
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 
@@ -204,11 +219,13 @@ var
 begin
   // Obtendo o Tamanho //
   AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
   Bufflen := 255;
   AStr := Space(Bufflen);
   AssertEquals(ErrOk, NFSE_ConfigLerValor(Handle, CSessaoVersao, CLibNFSeNome, PChar(AStr), Bufflen));
   AStr := copy(AStr,1,Bufflen);
   AssertEquals(CLibNFSeVersao, AStr);
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 
@@ -220,6 +237,7 @@ var
 begin
     // Gravando o valor
   AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
   AssertEquals('Erro ao Mudar configuração', ErrOk, NFSE_ConfigGravarValor(Handle, CSessaoPrincipal, CChaveLogNivel, '4'));
 
   // Checando se o valor foi atualizado //
@@ -228,6 +246,138 @@ begin
   AssertEquals(ErrOk, NFSE_ConfigLerValor(Handle, CSessaoPrincipal, CChaveLogNivel, PChar(AStr), Bufflen));
   AStr := copy(AStr,1,Bufflen);
   AssertEquals('Erro ao Mudar configuração', '4', AStr);
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSe_LimparLista;
+var
+  Handle: THandle;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
+  AssertEquals('Erro ao Limpar Lista NFSe', ErrOK, NFSE_LimparLista(Handle));
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSe_CarregarXML;
+var
+  Handle: THandle;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
+  AssertEquals('Erro ao carregar XML NFSe', ErrOK,
+   NFSE_CarregarXML(Handle, 'C:\ProjetoACBr\ACBr\Projetos\ACBrLib\Testes\NFSe\bin\NFSe.xml'));
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSe_CarregarINI;
+var
+  Handle: THandle;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
+  AssertEquals('Erro ao carregar o INI NFSe', ErrExecutandoMetodo,
+  NFSE_CarregarINI(Handle, 'C:\ProjetoACBr\ACBr\Projetos\ACBrLib\Testes\NFSe\bin\NFSe.ini'));
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_ObterXml;
+var
+  Handle: THandle;
+  Resposta: PChar;
+  Tamanho: Longint;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+  Resposta:= '';
+  Tamanho:= 0;
+
+  AssertEquals('Erro ao ObterXML', ErrIndex,
+  NFSE_ObterXml(Handle, 0, Resposta, Tamanho));
+  AssertEquals('Resposta= ' + AnsiString(Resposta), '', '');
+  AssertEquals('Tamanho= ' + IntToStr(Tamanho), '', '');
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_GravarXml;
+var
+  Handle: THandle;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
+  AssertEquals('Erro ao Gravar XML', ErrIndex,
+  NFSE_GravarXml(Handle, 0, 'NFSeTeste', 'C:\ProjetoACBr\ACBr\Projetos\ACBrLib\Testes\NFSe\bin'));
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_ObterIni;
+var
+  Handle: THandle;
+  Resposta: PChar;
+  Tamanho: Longint;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+  Resposta:= '';
+  Tamanho:= 0;
+
+  AssertEquals('Erro ao Obter Ini', ErrIndex,
+  NFSE_ObterIni(Handle, 0, Resposta, Tamanho));
+  AssertEquals('Resposta= ' + AnsiString(Resposta), '', '');
+  AssertEquals('Tamanho= ' + IntToStr(Tamanho), '', '');
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_GravarIni;
+var
+  Handle: THandle;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+
+  AssertEquals('Erro ao Gravar Ini', ErrIndex,
+  NFSE_GravarIni(Handle, 0, 'NFSeTeste', 'C:\ProjetoACBr\ACBr\Projetos\ACBrLib\Testes\NFSe\bin'));
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_ObterCertificados;
+var
+  Handle: THandle;
+  Resposta: PChar;
+  Tamanho: Longint;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+  Resposta:= '';
+  Tamanho:= 0;
+
+  AssertEquals('Erro ao Obter Certificados', ErrExecutandoMetodo,
+  NFSE_ObterCertificados(Handle, Resposta, Tamanho));
+  AssertEquals('Resposta= ' + AnsiString(Resposta), '', '');
+  AssertEquals('Tamanho= ' + IntToStr(Tamanho), '', '');
+
+  AssertEquals(ErrOK, NFSE_Finalizar(Handle));
+end;
+
+procedure TTestACBrNFSeLib.Test_NFSE_Emitir;
+var
+  Handle: THandle;
+  Resposta: PChar;
+  Tamanho: Longint;
+begin
+  AssertEquals(ErrOK, NFSE_Inicializar(Handle, '', ''));
+    Resposta:= '';
+  Tamanho:= 0;
+
+  AssertEquals('Erro ao Emitir NFSe', ErrExecutandoMetodo,
+  NFSE_Emitir(Handle, '1', 0, False, Resposta, Tamanho));
+  AssertEquals('Resposta= ' + AnsiString(Resposta), '', '');
+  AssertEquals('Tamanho= ' + IntToStr(Tamanho), '', '');
+
   AssertEquals(ErrOK, NFSE_Finalizar(Handle));
 end;
 

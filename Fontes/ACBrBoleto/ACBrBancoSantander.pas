da4de4cc98bb271655c101dd7a37db7364d7a9f8
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-03-31 19:34:22 +0000

    -- ACBrBancoSantander -- [*]TK-3727 Contribuição para sair desconto2 e desconto3 que não estavam sendo gerado na remessa.   Cliente validou com 0 (zeros) quando não informado.   por : Allan Almeida / Verificado: Daniel Morais
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@28941 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBancoSantander.pas
+++ Fontes/ACBrBoleto/ACBrBancoSantander.pas
@@ -718,13 +718,12 @@ begin
                   'R'                                                        + // 014 - 014 / Cód. Segmento do registro detalhe
                   Space(1)                                                   + // 015 - 015 / Reservado (uso Banco)
                   sCodMovimento                                              + // 016 - 017 / Código de movimento remessa
-                  '0'                                                        + // 018 - 018 / Código do desconto 2
-                  PadLeft('', 8, '0')                                        + // 019 - 026 / Data do desconto 2
-                  IntToStrZero(0, 15)                                        + // 027 - 041 / Valor/Percentual a ser concedido 2
-                  '0'                                                        + // 042 - 042 / Código do desconto 3
-                  PadLeft('', 8, '0')                                        + // 043 - 050 / Data do desconto 3
-                  IntToStrZero(0, 15)                                        + // 051 - 065 / Valor/Percentual a ser concedido 3
-
+                  TipoDescontoToString(TipoDesconto2)                        + // 018 - 018 / Código do desconto 2
+                  PadLeft(IfThen(TipoDesconto2<>tdNaoConcederDesconto,IfThen(DataDesconto2 > 0, FormatDateTime( 'ddmmyyyy', DataDesconto2),''),''),8,'0')      + // 019 - 026 / Data do desconto 2
+                  PadLeft(IfThen(TipoDesconto2<>tdNaoConcederDesconto,IfThen(ValorDesconto2 > 0, IntToStrZero(round(ValorDesconto2 * 100), 15),''),''),15,'0') + // 027 - 041 / Valor/Percentual a ser concedido 2
+                  TipoDescontoToString(TipoDesconto3)                        + // 042 - 042 / Código do desconto 3
+                  PadLeft(IfThen(TipoDesconto3<>tdNaoConcederDesconto,IfThen(DataDesconto3 > 0, FormatDateTime( 'ddmmyyyy', DataDesconto3),''),''),8,'0')      + // 043 - 050 / Data do desconto 3
+                  PadLeft(IfThen(TipoDesconto3<>tdNaoConcederDesconto,IfThen(ValorDesconto3 > 0, IntToStrZero(round(ValorDesconto3 * 100), 15),''),''),15,'0') + // 051 - 065 / Valor/Percentual a ser concedido 3
                   IfThen((PercentualMulta > 0),
                          IfThen(MultaValorFixo,'1','2'), '2')                                           + // 66 - 66 1-Cobrar Multa Valor Fixo / 2-Percentual / 0-Não cobrar multa
                   IfThen((PercentualMulta > 0),

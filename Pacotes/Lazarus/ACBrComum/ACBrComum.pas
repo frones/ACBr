22ecf7e29e115870cc7ab98d728293e6b7358023
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-04-28 19:04:09 +0000

    Ajustes em ACBr_Comum.dpk para incluir Units da FPDF-Pascal
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29281 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrComum/ACBrComum.pas
+++ Pacotes/Lazarus/ACBrComum/ACBrComum.pas
@@ -9,10 +9,10 @@ interface
 
 uses
   ACBrBase, ACBrConsts, ACBrUtil, ACBrReg, ACBrAAC, ACBrPAFClass, 
-  ACBrCompress, ACBrImage, StrUtilsEx, ACBrDelphiZXingQRCode, 
-  ACBrUtil.FilesIO, ACBrUtil.Math, ACBrUtil.Strings, ACBrUtil.XMLHTML, 
-  ACBrUtil.DateTime, ACBrUtil.Compatibilidade, ACBrUtil.Base, Jsons, ACBrJSON, 
-  LazarusPackageIntf;
+  ACBrCompress, ACBrImage, StrUtilsEx, ACBr_fpdf, ACBr_fpdf_ext, 
+  ACBrDelphiZXingQRCode, ACBrUtil.FilesIO, ACBrUtil.Math, ACBrUtil.Strings, 
+  ACBrUtil.XMLHTML, ACBrUtil.DateTime, ACBrUtil.Compatibilidade, 
+  ACBrUtil.Base, Jsons, ACBrJSON, LazarusPackageIntf;
 
 implementation
 

db1054a23cbf2ae70dc3ba081ed61501b743ddf1
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-05-17 12:02:57 +0000

    Correção compilação D7
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29466 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/FC/FPDF/ACBrBoletoFPDF.pas
+++ Fontes/ACBrBoleto/FC/FPDF/ACBrBoletoFPDF.pas
@@ -43,7 +43,8 @@ uses
   ACBrBoleto,
   StrUtils,
   ACBrBoletoConversao,
-  ACBr_fpdf_ext;
+  ACBr_fpdf_ext,
+  ACBrBoletoFPDFConst;
 
 type
   EACBrBoletoFPDF = class(Exception);
@@ -54,32 +55,6 @@ type
 {$ENDIF RTL230_UP}
 
   TACBrBoletoFPDF = class(TACBrBoletoFCClass)
-  CONST
-    LOCAL_PAGAMENTO             = 'Local de Pagamento';
-    NOME_BENEFICIARIO           = 'Nome do Beneficiário/CNPJ/CPF/Endereço';
-    NOME_PAGADOR                = 'Nome do Pagador/CPF/CNPJ/Endereço/Cidade/UF/CEP';
-    BENFICIARIO_FINAL           = 'Beneficiário Final';
-    DATA_DOCUMENTO              = 'Data do Documento';
-    USO_BANCO                   = 'Uso do Banco';
-    CARTEIRA                    = 'Carteira';
-    ESPECIE                     = 'Espécie';
-    QUANTIDADE                  = 'Quantidade';
-    XVALOR                      = 'x Valor';
-    ACEITE                      = 'Aceite';
-    NR_DOCUMENTO                = 'Nr do Documento';
-    ESPECIE_DOC                 = 'Espécie DOC';
-    DATA_PROCESSAMENTO          = 'Data Processamento';
-    DATA_VENCIMENTO             = 'Data Vencimento';
-    NOSSO_NUMERO                = 'Nosso Número';
-    AGENCIA_CODIGO_BENEFICIARIO = 'Agência / Código do Beneficiário';
-    VALOR_DOCUMENTO             = '(=) Valor Documento';
-    DESCONTO_ABATIMENTO         = '(-) Desconto/Abatimento';
-    JUROS_MULTA                 = '(+) Juros/Multa';
-    VALOR_PAGO                  = '(=) Valor Pago';
-    AUTENTICACAO_MECANICA       = 'Autenticação Mecanica';
-    FICHA_COMPENSACAO           = 'Ficha de Compensação';
-    INSTRUCOES_PAGAMENTO        = 'Informações de responsabilidades do beneficiário:';
-
   private
     FPDF               : TFPDFExt;
     FLinhaDigitavel    : String;
@@ -120,7 +95,7 @@ constructor TACBrBoletoFPDF.Create(AOwner: TComponent);
 begin
   inherited Create(AOwner);
 
-  FormatSettings.DecimalSeparator := ',';
+  DecimalSeparator := ',';
 end;
 
 destructor TACBrBoletoFPDF.Destroy;
---
+++ Fontes/ACBrBoleto/FC/FPDF/ACBrBoletoFPDFConst.pas
@@ -0,0 +1,32 @@
+unit ACBrBoletoFPDFConst;
+
+interface
+CONST
+    LOCAL_PAGAMENTO             = 'Local de Pagamento';
+    NOME_BENEFICIARIO           = 'Nome do Beneficiário/CNPJ/CPF/Endereço';
+    NOME_PAGADOR                = 'Nome do Pagador/CPF/CNPJ/Endereço/Cidade/UF/CEP';
+    BENFICIARIO_FINAL           = 'Beneficiário Final';
+    DATA_DOCUMENTO              = 'Data do Documento';
+    USO_BANCO                   = 'Uso do Banco';
+    CARTEIRA                    = 'Carteira';
+    ESPECIE                     = 'Espécie';
+    QUANTIDADE                  = 'Quantidade';
+    XVALOR                      = 'x Valor';
+    ACEITE                      = 'Aceite';
+    NR_DOCUMENTO                = 'Nr do Documento';
+    ESPECIE_DOC                 = 'Espécie DOC';
+    DATA_PROCESSAMENTO          = 'Data Processamento';
+    DATA_VENCIMENTO             = 'Data Vencimento';
+    NOSSO_NUMERO                = 'Nosso Número';
+    AGENCIA_CODIGO_BENEFICIARIO = 'Agência / Código do Beneficiário';
+    VALOR_DOCUMENTO             = '(=) Valor Documento';
+    DESCONTO_ABATIMENTO         = '(-) Desconto/Abatimento';
+    JUROS_MULTA                 = '(+) Juros/Multa';
+    VALOR_PAGO                  = '(=) Valor Pago';
+    AUTENTICACAO_MECANICA       = 'Autenticação Mecanica';
+    FICHA_COMPENSACAO           = 'Ficha de Compensação';
+    INSTRUCOES_PAGAMENTO        = 'Informações de responsabilidades do beneficiário:';
+
+implementation
+
+end.

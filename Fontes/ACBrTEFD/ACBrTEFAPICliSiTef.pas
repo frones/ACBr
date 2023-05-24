36687515e9d93498eb5c7f52b2c9ed8dcacc2544
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-05-24 19:04:57 +0000

    [+] possível correção https://www.projetoacbr.com.br/forum/topic/71980-acbrtefapi-clisitef-exibir-qrcode-na-tela-n%C3%A3o-esta-funcionando/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29552 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrTEFD/ACBrTEFAPICliSiTef.pas
+++ Fontes/ACBrTEFD/ACBrTEFAPICliSiTef.pas
@@ -848,6 +848,9 @@ begin
   Restricoes := '';
   if (pos('[', fParamAdicFuncao.Text) = 0) then   // Não especificou Restrições de Menu ?
   begin
+    if (TACBrTEFAPI(fpACBrTEFAPI).ExibicaoQRCode = qrapiExibirAplicacao) then
+       Restricoes := Restricoes + '{DevolveStringQRCode=1};';
+
     if (Op <> 1) then       // Débito
       Restricoes := Restricoes + CSITEF_RestricoesCueque + ';';
 
@@ -872,8 +875,7 @@ begin
       Restricoes := Restricoes + CSITEF_RestricoesAVista + ';';
       Restricoes := Restricoes + CSITEF_RestricoesParcelaAministradora + ';';
     end;
-    if (TACBrTEFAPI(fpACBrTEFAPI).ExibicaoQRCode = qrapiExibirAplicacao) then
-       Restricoes := Restricoes + '{DevolveStringQRCode=1};';
+
 
     SL := TStringList.Create;
     try

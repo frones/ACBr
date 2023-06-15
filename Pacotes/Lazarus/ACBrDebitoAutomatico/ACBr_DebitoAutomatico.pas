99534e0fa7497365a67bd2810c7c056e86864459
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-06-15 10:21:47 +0000

    15/06/2023 [+] Pacote de instalação do ACBrDebitoAutomatico - Novo componente Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29767 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Pacotes/Lazarus/ACBrDebitoAutomatico/ACBr_DebitoAutomatico.pas
@@ -0,0 +1,43 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ACBr_PagFor;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ACBrPagForReg, ACBrPagForArquivo, ACBrPagForArquivoClass, 
+  PagFor.BancoCECRED.GravarTxtRemessa, PagFor.BancoCECRED.LerTxtRetorno, 
+  PagFor.BancoCECRED.Provider, PagFor.BancodoBrasil.GravarTxtRemessa, 
+  PagFor.BancodoBrasil.LerTxtRetorno, PagFor.BancodoBrasil.Provider, 
+  PagFor.Bradesco.GravarTxtRemessa, PagFor.Bradesco.LerTxtRetorno, 
+  PagFor.Bradesco.Provider, PagFor.HSBC.GravarTxtRemessa, 
+  PagFor.HSBC.LerTxtRetorno, PagFor.HSBC.Provider, 
+  PagFor.Itau.GravarTxtRemessa, PagFor.Itau.LerTxtRetorno, 
+  PagFor.Itau.Provider, PagFor.Safra.GravarTxtRemessa, 
+  PagFor.Safra.LerTxtRetorno, PagFor.Safra.Provider, 
+  PagFor.Santander.GravarTxtRemessa, PagFor.Santander.LerTxtRetorno, 
+  PagFor.Santander.Provider, PagFor.Sicredi.GravarTxtRemessa, 
+  PagFor.Sicredi.LerTxtRetorno, PagFor.Sicredi.Provider, ACBrPagForClass, 
+  ACBrPagForConversao, ACBrPagForParametros, ACBrPagForGravarTxt, 
+  ACBrPagForInterface, ACBrPagForLerTxt, ACBrPagForProviderBase, 
+  ACBrPagForProviderManager, CNAB240.GravarTxtRemessa, CNAB240.LerTxtRetorno, 
+  CNAB240.Provider, PagFor.Modelo.GravarTxtRemessa, 
+  PagFor.Modelo.LerTxtRetorno, PagFor.Modelo.Provider, 
+  PagFor.Banrisul.GravarTxtRemessa, PagFor.Banrisul.LerTxtRetorno, 
+  PagFor.Banrisul.Provider, PagFor.UnicredCooperativa.GravarTxtRemessa, 
+  PagFor.UnicredCooperativa.LerTxtRetorno, PagFor.UnicredCooperativa.Provider, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('ACBrPagForReg', @ACBrPagForReg.Register);
+end;
+
+initialization
+  RegisterPackage('ACBr_PagFor', @Register);
+end.

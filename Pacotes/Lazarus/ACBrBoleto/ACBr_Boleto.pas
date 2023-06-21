2ea7efd953b7014274916743112223dcbbe81819
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-06-21 14:00:33 +0000

    [+] Correção no pacote. como tem o path dos fontes não é necessário ter cada unit de banco adicionado. ficando clean igual as impressões do boleto.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29813 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrBoleto/ACBr_Boleto.pas
+++ Pacotes/Lazarus/ACBrBoleto/ACBr_Boleto.pas
@@ -8,30 +8,7 @@ unit ACBr_Boleto;
 interface
 
 uses
-  ACBrBoleto, ACBrBancoBradesco, ACBrBancoBrasil, ACBrBancoItau, 
-  ACBrBancoSicredi, ACBrBancoMercantil, ACBrBancoBanrisul, ACBrBancoSantander, 
-  ACBrBancoBancoob, ACBrBancoHSBC, ACBrBancoNordeste, ACBrBancoBRB, 
-  ACBrBancoBic, ACBrBancoBanestes, ACBrBancoCecred, ACBrBancoCaixa, 
-  ACBrBancoCaixaSICOB, ACBrBancoBrasilSicoob, ACBrBancoCitiBank, 
-  ACBrBancoPine, ACBrBancoPineBradesco, ACBrBoletoReg, ACBrBoletoWS, 
-  ACBrBoletoConversao, ACBrBoletoPcnConsts, ACBrBoletoW_Caixa, 
-  ACBrBoletoRet_Caixa, ACBrBoletoRetorno, ACBrBoletoW_BancoBrasil, 
-  ACBrBoletoRet_BancoBrasil, ACBrBoletoW_BancoBrasil_API, 
-  ACBrBoletoRet_BancoBrasil_API, ACBrBancoPenseBank, 
-  ACBrBoletoW_PenseBank_API, ACBrBoletoRet_PenseBank_API, 
-  ACBrBoletoW_Santander, ACBrBoletoRet_Santander, ACBrBancoABCBrasil, 
-  ACBrBancoAlfa, ACBrBancoAmazonia, ACBrBancoBanese, 
-  ACBrBancoBradescoMoneyPlus, ACBrBancoBradescoSICOOB, ACBrBancoBS2, 
-  ACBrBancoBTGPactual, ACBrBancoC6, ACBrBancoCredisis, ACBrBancoCresol, 
-  ACBrBancoCresolSCRS, ACBrBancoDaycoval, ACBrBancoInter, ACBrBancoOriginal, 
-  ACBrBancoRendimento, ACBrBancoSafra, ACBrBancoSafraBradesco, 
-  ACBrBancoSofisaSantander, ACBrBancoUnicredES, ACBrBancoUnicredRS, 
-  ACBrBancoUnicredSC, ACBrBoletoWS.Rest.OAuth, ACBrBoletoWS.Rest, 
-  ACBrBoletoWS.SOAP, ACBrUniprime, ACBrUniprimeNortePR, 
-  ACBrBoletoRet_Credisis, ACBrBoletoRet_Inter_API, ACBrBoletoRet_Itau, 
-  ACBrBoletoRet_Sicredi_APIECOMM, ACBrBoletoW_Credisis, ACBrBoletoW_Inter_API, 
-  ACBrBoletoW_Itau, ACBrBoletoW_Sicredi_APIECOMM, ACBrBancoVotorantim, 
-  ACBrBoletoRet_Sicredi_APIV2, ACBrBoletoW_Sicredi_APIV2, LazarusPackageIntf;
+  ACBrBoleto, ACBrBoletoReg, LazarusPackageIntf;
 
 implementation
 

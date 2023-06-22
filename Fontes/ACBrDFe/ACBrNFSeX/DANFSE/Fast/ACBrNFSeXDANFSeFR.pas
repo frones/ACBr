e8f3ff550cbb902caa26dcfb2e1181c004f9567c
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-06-22 16:31:04 +0000

    22/06/2023 -- ACBrNFSeXDANFSeFR -- [*] Alteração visando imprimir a data do Rps em vez da nota caso o XML seja do Rps.   Por: Neski Soluções
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29823 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/DANFSE/Fast/ACBrNFSeXDANFSeFR.pas
@@ -977,7 +977,17 @@ begin
   if (Provedor in [ proGINFES, proBetha, proDSF ]) then
     LCDS.FieldByName('DataEmissao').AsString := FormatDateTimeBr(ANFSe.DataEmissao)
   else
-    LCDS.FieldByName('DataEmissao').AsString := FormatDateBr(ANFSe.DataEmissao);
+  begin
+    if ANFSe.Numero <> '' then
+      LCDS.FieldByName('DataEmissao').AsString := FormatDateBr(ANFSe.DataEmissao)
+    else
+    begin
+      if frxReport.findcomponent('Memo12') <> nil then
+        TfrxMemoView(frxReport.findcomponent('Memo12')).Text := 'Data do RPS';
+
+      LCDS.FieldByName('DataEmissao').AsString := FormatDateBr(ANFSe.DataEmissaoRps);
+    end;
+  end;
 
   LCDS.FieldByName('CodigoVerificacao').AsString := ANFSe.CodigoVerificacao;
   LCDS.FieldByName('LinkNFSe').AsString          := ANFSe.Link;

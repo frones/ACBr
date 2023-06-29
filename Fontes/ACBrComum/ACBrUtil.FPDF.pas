dcc1ebb5cada934f42a450ea11b532898384a53f
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-06-29 11:58:12 +0000

    [+] Adicionado a e fazendo override Classe para compatibilizar com o Lazarus e Delphi com as units do projeto a acentuação
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@29943 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Fontes/ACBrComum/ACBrUtil.FPDF.pas
@@ -0,0 +1,66 @@
+{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+{ Direitos Autorais Reservados (c) 2023 Daniel Simoes de Almeida               }
+
+
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+
+{ Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
+{       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
+{******************************************************************************}
+
+{  Algumas funçoes dessa Unit foram extraidas de outras Bibliotecas, veja no   }
+{ cabeçalho das Funçoes no código abaixo a origem das informaçoes, e autores...}
+
+{******************************************************************************}
+
+{$I ACBr.inc}
+
+unit ACBrUtil.FPDF;
+
+interface
+
+uses ACBr_fpdf,
+     ACBr_fpdf_ext,
+     ACBrUtil.Strings;
+
+type
+  TACBrFPDFExt = class(TFPDFExt)
+    public
+      procedure Cell(vWidth: Double; vHeight: Double = 0; const vText: String = '';
+        const vBorder: String = '0'; vLineBreak: Integer = 0; const vAlign: String = '';
+        vFill: Boolean = False; vLink: String = ''); override;
+  end;
+
+implementation
+
+{ TACBrFPDFExt }
+
+procedure TACBrFPDFExt.Cell(vWidth, vHeight: Double; const vText,
+  vBorder: String; vLineBreak: Integer; const vAlign: String; vFill: Boolean;
+  vLink: String);
+begin
+  inherited Cell(vWidth, vHeight, NativeStringToAnsi(vText),
+  vBorder, vLineBreak, vAlign, vFill, vLink);
+end;
+
+end.

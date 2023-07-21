923623e28e9c7fc68c468bcb576ccc8072032934
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-07-21 17:57:00 +0000

    21/07/2023 -- Tecnos.GravarXml -- [*] Implementado a sobrecarga da function: GerarEnderecoTomador.     O provedor existe a presença da tag CodigoPais para o tomador.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@30186 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Tecnos.GravarXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Tecnos.GravarXml.pas
@@ -48,6 +48,8 @@ type
 
   TNFSeW_Tecnos201 = class(TNFSeW_ABRASFv2)
   protected
+    function GerarEnderecoTomador: TACBrXmlNode; override;
+
     function DefinirNameSpaceDeclaracao: string; override;
 
     function GerarInfDeclaracaoPrestacaoServico: TACBrXmlNode; override;
@@ -126,6 +128,48 @@ begin
   NrOcorrRespRetencao := 1;
   
   GerarTagServicos := False;
+end;
+
+function TNFSeW_Tecnos201.GerarEnderecoTomador: TACBrXmlNode;
+begin
+  Result := nil;
+
+  if (NFSe.Tomador.Endereco.Endereco <> '') or (NFSe.Tomador.Endereco.Numero <> '') or
+     (NFSe.Tomador.Endereco.Bairro <> '') or (NFSe.Tomador.Endereco.CodigoMunicipio <> '') or
+     (NFSe.Tomador.Endereco.UF <> '') or (NFSe.Tomador.Endereco.CEP <> '') then
+  begin
+    Result := CreateElement('Endereco');
+
+    Result.AppendChild(AddNode(tcStr, '#39', 'Endereco', 1, 125, NrOcorrEndereco,
+                                     NFSe.Tomador.Endereco.Endereco, DSC_XLGR));
+
+    Result.AppendChild(AddNode(tcStr, '#39', 'TipoLogradouro', 1, 50, NrOcorrTipoLogradouro,
+                               NFSe.Tomador.Endereco.TipoLogradouro, DSC_XLGR));
+
+    Result.AppendChild(AddNode(tcStr, '#39', 'Logradouro', 1, 125, NrOcorrLogradouro,
+                                     NFSe.Tomador.Endereco.Endereco, DSC_XLGR));
+
+    Result.AppendChild(AddNode(tcStr, '#40', 'Numero', 1, 10, 0,
+                                        NFSe.Tomador.Endereco.Numero, DSC_NRO));
+
+    Result.AppendChild(AddNode(tcStr, '#41', 'Complemento', 1, 60, NrOcorrComplTomador,
+                                  NFSe.Tomador.Endereco.Complemento, DSC_XCPL));
+
+    Result.AppendChild(AddNode(tcStr, '#42', 'Bairro', 1, 60, 0,
+                                    NFSe.Tomador.Endereco.Bairro, DSC_XBAIRRO));
+
+    Result.AppendChild(AddNode(tcStr, '#43', 'CodigoMunicipio', 7, 7, 0,
+                  OnlyNumber(NFSe.Tomador.Endereco.CodigoMunicipio), DSC_CMUN));
+
+    Result.AppendChild(AddNode(tcStr, '#44', 'Uf', 2, 2, NrOcorrUFTomador,
+                                             NFSe.Tomador.Endereco.UF, DSC_UF));
+
+    Result.AppendChild(AddNode(tcInt, '#44', 'CodigoPais', 4, 4, NrOcorrCodigoPaisTomador,
+                                  NFSe.Tomador.Endereco.CodigoPais, DSC_CPAIS));
+
+    Result.AppendChild(AddNode(tcStr, '#45', 'Cep', 8, 8, NrOcorrCepTomador,
+                               OnlyNumber(NFSe.Tomador.Endereco.CEP), DSC_CEP));
+  end;
 end;
 
 procedure TNFSeW_Tecnos201.DefinirIDDeclaracao;

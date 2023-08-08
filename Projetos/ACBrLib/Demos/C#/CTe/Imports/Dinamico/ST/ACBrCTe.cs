dcd1365ff59883280062a58d1eeac65d87c1acc7
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-08-08 18:30:44 +0000

    [+] Adiciona as rotinas CarregarCTe e ObterCTe, conforme exemplos da classe ACBrNFe Por: Lindomar S. Menezes https://www.projetoacbr.com.br/forum/topic/73235-m%C3%A9todo-load-da-classe-ctecs/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@30278 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/ST/ACBrCTe.cs
+++ Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/ST/ACBrCTe.cs
@@ -129,6 +129,9 @@ namespace ACBrLib.CTe
 
         #endregion Ini
 
+        public void CarregarCTe(CTe cte) => CarregarINI(cte.ToString());
+
+        public CTe ObterCTe(int aIndex) => CTe.Load(ObterIni(aIndex));
         public void CarregarXML(string eArquivoOuXml)
         {
             var method = GetMethod<CTE_CarregarXML>();

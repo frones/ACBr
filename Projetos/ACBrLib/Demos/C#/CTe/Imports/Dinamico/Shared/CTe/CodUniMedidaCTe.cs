7aacdc88a7f49fa0ace8026b9091ca1af302fc65
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-08-18 12:11:26 +0000

    [*] Adiciona EnumValue para correta geração no INI e posterior conversão no XML. Por: Diego Folieni #TK-4290 https://www.projetoacbr.com.br/forum/topic/73496-c-infq-cunid/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@30361 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/CodUniMedidaCTe.cs
+++ Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/CodUniMedidaCTe.cs
@@ -8,11 +8,22 @@ namespace ACBrLib.Core.CTe
 {
     public enum CodUniMedidaCTe
     {
+        [EnumValue("00")]
         M3 = 00,
+
+        [EnumValue("01")]
         KG = 01,
+
+        [EnumValue("02")]
         TON = 02,
+
+        [EnumValue("03")]
         UNIDADE = 03,
+
+        [EnumValue("04")]
         LITROS = 04,
+
+        [EnumValue("05")]
         MMBTU = 05
     }
 }

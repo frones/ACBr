bc3f498c107af324889fec0ab4e685b0dc175483
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-09-06 20:32:45 +0000

    [*] Melhoria no enumerado. Por: Lindomar S. Menezes https://www.projetoacbr.com.br/forum/topic/73958-c-cte-infoutros/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@30566 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/TipoDocCTe.cs
+++ Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/TipoDocCTe.cs
@@ -8,10 +8,19 @@ namespace ACBrLib.Core.CTe
 {
     public enum TipoDocCTe
     {
+        [EnumValue("00")]
         declaracao = 00,
+
+        [EnumValue("10")]
         dutoviario = 10,
+
+        [EnumValue("59")]
         CFeSAT = 59,
+
+        [EnumValue("65")]
         NFCe = 65,
+
+        [EnumValue("99")]
         outros = 99
     }
 }

be281feee79ed38ff4fc2553b1988ab791dc333f
renatorubinho <renatorubinho@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-09-20 21:32:15 +0000

    20/09/2023 -- pcnReinfR4020 -- [*] Corrigido número de ocorrências do cnpjBenef no R-4020   Por: Renato Rubinho
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@30712 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrReinf/PCNReinf/pcnReinfR4020.pas
+++ Fontes/ACBrDFe/ACBrReinf/PCNReinf/pcnReinfR4020.pas
@@ -524,7 +524,7 @@ begin
 
   with Self.ideEstab do
   begin
-    Gerador.wCampo(tcStr, '', 'cnpjBenef', 14, 14, 1, ideBenef.cnpjBenef);
+    Gerador.wCampo(tcStr, '', 'cnpjBenef', 14, 14, 0, ideBenef.cnpjBenef);
     Gerador.wCampo(tcStr, '', 'nmBenef',   1,  70, 0, ideBenef.nmBenef);
     Gerador.wCampo(tcStr, '', 'isenImun',  1,   1, 0, tpIsencaoImunidadeToStr(ideBenef.isenImun));
     if TACBrReinf(FACBrReinf).Configuracoes.Geral.VersaoDF >= v2_01_02 then

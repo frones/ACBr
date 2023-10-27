4e3bb6aa9c78aaed2258016d9482d77fda1b0445
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-10-27 12:56:40 +0000

    [*] Adiciona EnumValues para correta conversão. Por: Lindomar S. Menezes #TK-4586 https://www.projetoacbr.com.br/forum/topic/74687-c-acbrcte-iddocantpap/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@31039 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/TipoDocAnteriorCTe.cs
+++ Projetos/ACBrLib/Demos/C#/CTe/Imports/Dinamico/Shared/CTe/TipoDocAnteriorCTe.cs
@@ -8,12 +8,25 @@ namespace ACBrLib.Core.CTe
 {
     public enum TipoDocAnteriorCTe
     {
+        [EnumValue("07")]
         ATRE = 07,
+
+        [EnumValue("08")]
         DTA = 08,
+
+        [EnumValue("09")]
         ConhecimentoAereoInternacional = 09,
+
+        [EnumValue("10")]
         ConhecimentoCartaPorteInternacional = 10,
+
+        [EnumValue("11")]
         ConhecimentoAvulso = 11,
+
+        [EnumValue("12")]
         TIF = 12,
+
+        [EnumValue("13")]
         BL = 13
     }
 }

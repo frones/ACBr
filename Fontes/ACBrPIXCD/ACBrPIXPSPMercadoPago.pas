8c77d813b8ac199e40e1bbd0cd92c5dc9a52a569
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2023-12-04 21:15:31 +0000

    https://www.projetoacbr.com.br/forum/topic/75077-acbrpixcd-psp-mercado-pago-erro-ao-solicitar-devolu%C3%A7%C3%A3o-pix/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@31450 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrPIXCD/ACBrPIXPSPMercadoPago.pas
+++ Fontes/ACBrPIXCD/ACBrPIXPSPMercadoPago.pas
@@ -1,3 +1,35 @@
+{******************************************************************************}
+{ Projeto: Componentes ACBr                                                    }
+{  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
+{ mentos de Automação Comercial utilizados no Brasil                           }
+{                                                                              }
+{ Direitos Autorais Reservados (c) 2022 Daniel Simoes de Almeida               }
+{                                                                              }
+{ Colaboradores nesse arquivo:                                                 }
+{                                                                              }
+{  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
+{ Componentes localizado em      http://www.sourceforge.net/projects/acbr      }
+{                                                                              }
+{  Esta biblioteca é software livre; você pode redistribuí-la e/ou modificá-la }
+{ sob os termos da Licença Pública Geral Menor do GNU conforme publicada pela  }
+{ Free Software Foundation; tanto a versão 2.1 da Licença, ou (a seu critério) }
+{ qualquer versão posterior.                                                   }
+{                                                                              }
+{  Esta biblioteca é distribuída na expectativa de que seja útil, porém, SEM   }
+{ NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU      }
+{ ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral Menor}
+{ do GNU para mais detalhes. (Arquivo LICENÇA.TXT ou LICENSE.TXT)              }
+{                                                                              }
+{  Você deve ter recebido uma cópia da Licença Pública Geral Menor do GNU junto}
+{ com esta biblioteca; se não, escreva para a Free Software Foundation, Inc.,  }
+{ no endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.          }
+{ Você também pode obter uma copia da licença em:                              }
+{ http://www.opensource.org/licenses/lgpl-license.php                          }
+{                                                                              }
+{ Daniel Simões de Almeida - daniel@projetoacbr.com.br - www.projetoacbr.com.br}
+{       Rua Coronel Aureliano de Camargo, 963 - Tatuí - SP - 18270-170         }
+{******************************************************************************}
+
 {$I ACBr.inc}
 
 unit ACBrPIXPSPMercadoPago;
@@ -11,7 +43,6 @@ uses
   ACBrPIXSchemasProblema, ACBrPIXSchemasCob, ACBrPIXSchemasDevolucao;
 
 const
-  cMercadoPagoURLSandbox = ''; //Não possui SandBox
   cMercadoPagoURLProducao = 'https://api.mercadopago.com';
   cMercadoPagoEndPointPayment = '/v1/payments';
   cMercadoPagoEndPointRefund = '/refunds';
@@ -452,13 +483,9 @@ begin
   end;
 end;
 
-function TACBrPSPMercadoPago.ObterURLAmbiente(
-  const Ambiente: TACBrPixCDAmbiente): string;
+function TACBrPSPMercadoPago.ObterURLAmbiente(const Ambiente: TACBrPixCDAmbiente): string;
 begin
-  if (Ambiente = ambProducao) then
-    Result := cMercadoPagoURLProducao
-  else
-    Result := cMercadoPagoURLSandbox;
+  Result := cMercadoPagoURLProducao
 end;
 
 function TACBrPSPMercadoPago.CalcularEndPointPath(const aMethod, aEndPoint: string): string;
@@ -495,8 +522,8 @@ begin
     begin
       wDevolucao := URLPathParams[0];
       URLPathParams.Clear;
-      URLPathParams.Add(cMercadoPagoEndPointRefund);
       URLPathParams.Add(wDevolucao);
+      URLPathParams.Add(cMercadoPagoEndPointRefund);
     end
     else
       URLPathParams.Clear;

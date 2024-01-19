1a5dd88980997a2eceecfaf8a440203f55ae9e8c
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-01-19 19:07:19 +0000

    19/01/2024 [+] Pacotes de Instalação para o Delphi e Lazarus do componente ACBrNFCom Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@32051 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Pacotes/Lazarus/ACBrDFe/ACBrNFCom/ACBr_NFCom.pas
@@ -0,0 +1,27 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ACBr_NFCom;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ACBrNFCom, ACBrNFComReg, ACBrNFComNotasFiscais, ACBrNFComConfiguracoes, 
+  ACBrNFComWebServices, ACBrNFComDANFComClass, ACBrNFComClass, 
+  ACBrNFComConsts, ACBrNFComConversao, ACBrNFComProc, ACBrNFComXmlReader, 
+  ACBrNFComXmlWriter, ACBrNFComConsSit, ACBrNFComConsStatServ, 
+  ACBrNFComEnvEvento, ACBrNFComEventoClass, ACBrNFComRetConsSit, 
+  ACBrNFComRetConsStatServ, ACBrNFComRetEnvEvento, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('ACBrNFComReg', @ACBrNFComReg.Register);
+end;
+
+initialization
+  RegisterPackage('ACBr_NFCom', @Register);
+end.

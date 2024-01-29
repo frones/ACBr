cc21d85892205f9ef8c5b2162fcd9682c11a3e45
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-01-29 00:34:01 +0000

    Continuação do Demo ACBrAbecsPinPad
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@32154 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrSerial/ACBrAbecsPinPad/Lazarus/formprincipal.pas
+++ Exemplos/ACBrSerial/ACBrAbecsPinPad/Lazarus/formprincipal.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  Buttons, Spin, ComCtrls, Grids, ACBrAbecsPinPad;
+  Buttons, Spin, ComCtrls, Grids, ExtDlgs, ACBrAbecsPinPad;
 
 const
   CSerialSection = 'Serial';
@@ -43,10 +43,13 @@ type
     btRMC: TButton;
     btSaveParams: TBitBtn;
     btSearchSerialPorts: TSpeedButton;
+    btSearchSerialPorts1: TSpeedButton;
     btSerial: TSpeedButton;
     cbSecure: TCheckBox;
     cbxMsgAlign: TComboBox;
     cbxPort: TComboBox;
+    cbGIXAll: TCheckBox;
+    edGIXValue: TEdit;
     edLogFile: TEdit;
     edMediaLoad: TEdit;
     edtCLOMsg1: TEdit;
@@ -62,21 +65,32 @@ type
     GroupBox1: TGroupBox;
     GroupBox2: TGroupBox;
     gbDSP: TGroupBox;
+    gbGIN: TGroupBox;
+    gbGIX: TGroupBox;
     ImageList1: TImageList;
     imgMedia: TImage;
     Label1: TLabel;
     Label13: TLabel;
     Label14: TLabel;
     Label2: TLabel;
+    Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
     lbCLXMedias: TListBox;
+    lbLMFMedias: TListBox;
+    lbGIXParams: TListBox;
     mCLX: TMemo;
     mDEX: TMemo;
+    mGIXResponse: TMemo;
+    mGINResponse: TMemo;
     mExponent: TMemo;
     mLog: TMemo;
     mModulus: TMemo;
+    OpenPictureDialog1: TOpenPictureDialog;
+    pGIXParams: TPanel;
+    pLMFMediaTitle: TPanel;
+    pCLXMediaTitle: TPanel;
     pgCLX: TPageControl;
     pCommands: TPanel;
     pCancelar: TPanel;
@@ -94,7 +108,7 @@ type
     sbResponse: TStatusBar;
     sbShowLogFile: TSpeedButton;
     seLogLevel: TSpinEdit;
-    sgMedia: TStringGrid;
+    seGIN_ACQIDX: TSpinEdit;
     Splitter2: TSplitter;
     TabSheet1: TTabSheet;
     tsCLOLines: TTabSheet;
@@ -131,11 +145,13 @@ type
     procedure btMNUClick(Sender: TObject);
     procedure btOPNClick(Sender: TObject);
     procedure btReadParmsClick(Sender: TObject);
+    procedure btSearchSerialPorts1Click(Sender: TObject);
     procedure btSearchSerialPortsClick(Sender: TObject);
     procedure btRMCClick(Sender: TObject);
     procedure btSaveParamsClick(Sender: TObject);
     procedure btSerialClick(Sender: TObject);
     procedure btCancelClick(Sender: TObject);
+    procedure cbGIXAllChange(Sender: TObject);
     procedure cbSecureChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure sbCleanMemoLogClick(Sender: TObject);
@@ -153,8 +169,8 @@ type
     procedure ConfigPanelCancel(IsCancel: Boolean);
     procedure ConfigACBrAbecsPinPad;
 
-    function GetSelectedMedia: String;
     procedure LoadMediaNames;
+    function PP_StrToInt(const PP: String): Word;
   public
 
   end;
@@ -313,34 +329,160 @@ begin
   ACBrAbecsPinPad1.MsgAlign := TACBrAbecsMsgAlign(cbxMsgAlign.ItemIndex);
 end;
 
-function TfrMain.GetSelectedMedia: String;
-begin
-  Result := '';
-  if (sgMedia.Row > 0) then
-    Result := sgMedia.Rows[sgMedia.Row].Text;
-end;
-
 procedure TfrMain.LoadMediaNames;
 var
   sl: TStringList;
-  i: Integer;
 begin
   ACBrAbecsPinPad1.LMF;
 
   sl := TStringList.Create;
   try
     ACBrAbecsPinPad1.Response.GetResponseFromTagValue(PP_MFNAME, sl);
-    lbCLXMedias.Items.Assign(sl);
+    lbLMFMedias.Items.Assign(sl);
+    if (sl.Count < 1) then
+      pLMFMediaTitle.Caption := 'No media files'
+    else
+      pLMFMediaTitle.Caption := Format('%d media file(s)', [sl.Count]);
 
-    sgMedia.RowCount := 1;
-    for i := 0 to sl.Count-1 do
-    begin
-      sgMedia.RowCount := i+2;
-      sgMedia.Rows[i+1].Text := sl[i];
-    end;
+    pCLXMediaTitle.Caption := pLMFMediaTitle.Caption;
+    lbCLXMedias.Items.Assign(sl);
   finally
     sl.Free;
   end;
+end;
+
+function TfrMain.PP_StrToInt(const PP: String): Word;
+var
+  s: String;
+begin
+  s := UpperCase(Trim(PP));
+
+  if (s = 'PP_SERNUM') then
+    Result := PP_SERNUM
+  else if (s = 'PP_PARTNBR') then
+    Result := PP_PARTNBR
+  else if (s = 'PP_MODEL') then
+    Result := PP_MODEL
+  else if (s = 'PP_MNNAME') then
+    Result := PP_MNNAME
+  else if (s = 'PP_CAPAB') then
+    Result := PP_CAPAB
+  else if (s = 'PP_SPECVER') then
+    Result := PP_SPECVER
+  else if (s = 'PP_MANVERS') then
+    Result := PP_MANVERS
+  else if (s = 'PP_APPVERS') then
+    Result := PP_APPVERS
+  else if (s = 'PP_GENVERS') then
+    Result := PP_GENVERS
+  else if (s = 'PP_KRNLVER') then
+    Result := PP_KRNLVER
+  else if (s = 'PP_CTLSVER') then
+    Result := PP_CTLSVER
+  else if (s = 'PP_MCTLSVER') then
+    Result := PP_MCTLSVER
+  else if (s = 'PP_VCTLSVER') then
+    Result := PP_VCTLSVER
+  else if (s = 'PP_AECTLSVER') then
+    Result := PP_AECTLSVER
+  else if (s = 'PP_DPCTLSVER') then
+    Result := PP_DPCTLSVER
+  else if (s = 'PP_PUREVER') then
+    Result := PP_PUREVER
+  else if (s = 'PP_DSPTXTSZ') then
+    Result := PP_DSPTXTSZ
+  else if (s = 'PP_DSPGRSZ') then
+    Result := PP_DSPGRSZ
+  else if (s = 'PP_MFSUP') then
+    Result := PP_MFSUP
+  else if (s = 'PP_MKTDESP') then
+    Result := PP_MKTDESP
+  else if (s = 'PP_MKTDESD') then
+    Result := PP_MKTDESD
+  else if (s = 'PP_DKPTTDESP') then
+    Result := PP_DKPTTDESP
+  else if (s = 'PP_DKPTTDESD') then
+    Result := PP_DKPTTDESD
+  else if (s = 'PP_EVENT') then
+    Result := PP_EVENT
+  else if (s = 'PP_TRK1INC') then
+    Result := PP_TRK1INC
+  else if (s = 'PP_TRK2INC') then
+    Result := PP_TRK2INC
+  else if (s = 'PP_TRK3INC') then
+    Result := PP_TRK3INC
+  else if (s = 'PP_TRACK1') then
+    Result := PP_TRACK1
+  else if (s = 'PP_TRACK2') then
+    Result := PP_TRACK2
+  else if (s = 'PP_TRACK3') then
+    Result := PP_TRACK3
+  else if (s = 'PP_TRK1KSN') then
+    Result := PP_TRK1KSN
+  else if (s = 'PP_TRK2KSN') then
+    Result := PP_TRK2KSN
+  else if (s = 'PP_TRK3KSN') then
+    Result := PP_TRK3KSN
+  else if (s = 'PP_ENCPAN') then
+    Result := PP_ENCPAN
+  else if (s = 'PP_ENCPANKSN') then
+    Result := PP_ENCPANKSN
+  else if (s = 'PP_KSN') then
+    Result := PP_KSN
+  else if (s = 'PP_VALUE') then
+    Result := PP_VALUE
+  else if (s = 'PP_DATAOUT') then
+    Result := PP_DATAOUT
+  else if (s = 'PP_CARDTYPE') then
+    Result := PP_CARDTYPE
+  else if (s = 'PP_ICCSTAT') then
+    Result := PP_ICCSTAT
+  else if (s = 'PP_AIDTABINFO') then
+    Result := PP_AIDTABINFO
+  else if (s = 'PP_PAN') then
+    Result := PP_PAN
+  else if (s = 'PP_PANSEQNO') then
+    Result := PP_PANSEQNO
+  else if (s = 'PP_EMVDATA') then
+    Result := PP_EMVDATA
+  else if (s = 'PP_CHNAME') then
+    Result := PP_CHNAME
+  else if (s = 'PP_GOXRES') then
+    Result := PP_GOXRES
+  else if (s = 'PP_PINBLK') then
+    Result := PP_PINBLK
+  else if (s = 'PP_FCXRES') then
+    Result := PP_FCXRES
+  else if (s = 'PP_ISRESULTS') then
+    Result := PP_ISRESULTS
+  else if (s = 'PP_BIGRAND') then
+    Result := PP_BIGRAND
+  else if (s = 'PP_LABEL') then
+    Result := PP_LABEL
+  else if (s = 'PP_ISSCNTRY') then
+    Result := PP_ISSCNTRY
+  else if (s = 'PP_CARDEXP') then
+    Result := PP_CARDEXP
+  else if (s = 'PP_MFNAME') then
+    Result := PP_MFNAME
+  else if (s = 'PP_DEVTYPE') then
+    Result := PP_DEVTYPE
+  else if (s = 'PP_TLRMEM') then
+    Result := PP_TLRMEM
+  else if (s = 'PP_ENCKRAND') then
+    Result := PP_ENCKRAND
+  else if (s = 'PP_KSNTDESP00') then
+    Result := PP_KSNTDESP00
+  else if (s = 'PP_KSNTDESP63') then
+    Result := PP_KSNTDESP63
+  else if (s = 'PP_KSNTDESD00') then
+    Result := PP_KSNTDESD00
+  else if (s = 'PP_KSNTDESD63') then
+    Result := PP_KSNTDESD63
+  else if (s = 'PP_TABVER00') then
+    Result := PP_TABVER00
+  else if (s = 'PP_TABVER63') then
+    Result := PP_TABVER63;
 end;
 
 procedure TfrMain.btSerialClick(Sender: TObject);
@@ -438,33 +580,46 @@ end;
 
 procedure TfrMain.btDMFClick(Sender: TObject);
 var
+  sl: TStringList;
   s: String;
 begin
-  s := GetSelectedMedia;
-  if (s <> '') then
-  begin
-    if (MessageDlg( 'DELETE MEDIA?',
-                    Format('Are you shure to delete Media %s',[s]),
-                    mtConfirmation,
-                    mbYesNo, 0, mbNo) = mrYes) then
-      ACBrAbecsPinPad1.DMF(s);
-  end
-  else
+  if (lbLMFMedias.SelCount < 1) then
     raise Exception.Create('No Media selected');
 
-  // Example how to delete multiple files (using Array of String)
-  //ACBrAbecsPinPad1.DMF(['LOGOACBR', 'IMAGE01', 'QRCODE02']);
+  if (MessageDlg( 'DELETE MEDIA?',
+                  Format('Delete %d Media file(s)',[lbLMFMedias.SelCount]),
+                  mtConfirmation,
+                  mbYesNo, 0, mbNo) <> mrYes) then
+    Exit;
+
+  s := lbLMFMedias.GetSelectedText;
+
+  if (lbLMFMedias.SelCount = 1) then
+    ACBrAbecsPinPad1.DMF(s)
+  else
+  begin
+    sl := TStringList.Create;
+    try
+      sl.Text := s;
+      ACBrAbecsPinPad1.DMF(sl);
+    finally
+      sl.Free;
+    end;
+  end;
 end;
 
 procedure TfrMain.btDSIClick(Sender: TObject);
 var
-  s: String;
+  i: Integer;
 begin
-  s := GetSelectedMedia;
-  if (s <> '') then
-    ACBrAbecsPinPad1.DSI(s)
-  else
+  if (lbLMFMedias.SelCount < 1) then
     raise Exception.Create('No Media selected');
+
+  for i := 0 to lbLMFMedias.Count-1 do
+  begin
+    if (lbLMFMedias.Selected[i]) then
+      ACBrAbecsPinPad1.DSI(lbLMFMedias.Items[i])
+  end;
 end;
 
 procedure TfrMain.btDEXClearClick(Sender: TObject);
@@ -494,15 +649,66 @@ end;
 
 procedure TfrMain.btGINClick(Sender: TObject);
 begin
-  ACBrAbecsPinPad1.GIN(2);
-  ACBrAbecsPinPad1.GIN(3);
-  ACBrAbecsPinPad1.GIN;
+  ACBrAbecsPinPad1.GIN(seGIN_ACQIDX.Value);
+  mGINResponse.Lines.Text := ACBrAbecsPinPad1.Response.GetResponseData;
 end;
 
 procedure TfrMain.btGIXClick(Sender: TObject);
+var
+  i: Integer;
+  p: Word;
+  PP_DATA: Array of Word;
+  sl: TStringList;
+  s, n: String;
+
+  procedure AddParam(AParam: Word);
+  var
+    l: Integer;
+  begin
+    l := Length(PP_DATA);
+    SetLength(PP_DATA, l+1);
+    PP_DATA[l] := AParam;
+  end;
+
 begin
-  ACBrAbecsPinPad1.GIX([PP_MODEL]);
-  ACBrAbecsPinPad1.GIX;
+  if (lbGIXParams.Count < 1) then
+    ACBrAbecsPinPad1.GIX
+  else
+  begin
+    for i := 0 to lbGIXParams.Count-1 do
+    begin
+      if (lbGIXParams.Selected[i]) then
+       AddParam(PP_StrToInt(lbGIXParams.Items[i]));
+    end;
+
+    if (edGIXValue.Text <> '') then
+    begin
+      p := StrToIntDef(edGIXValue.Text, 0);
+      if (p > 0) then
+        AddParam(p);
+    end;
+
+    ACBrAbecsPinPad1.GIX(PP_DATA);
+  end;
+
+  mGIXResponse.Lines.Clear;
+  sl := TStringList.Create;
+  try
+    ACBrAbecsPinPad1.Response.GetResponseAsValues(sl);
+    for i := 0 to sl.Count-1 do
+    begin
+      n := sl.Names[i];
+      p := StrToIntDef(n, -1);
+      if (p > 0) then
+        s := PP_ToStr(p)
+      else
+        s := n;
+
+      mGIXResponse.Lines.Add(s+' => '+sl.Values[n]);
+    end;
+  finally
+    sl.Free;
+  end;
 end;
 
 procedure TfrMain.btGKYClick(Sender: TObject);
@@ -517,13 +723,16 @@ end;
 
 procedure TfrMain.btMediaLoadClick(Sender: TObject);
 var
-  FS: TFileStream;
+  ms: TMemoryStream;
 begin
-  FS := TFileStream.Create('C:\Pascal\Comp\ACBr\trunk2\Fontes\Imagens\Android\Quadrado\ACBr_192_192.png', fmOpenRead);
+  ms := TMemoryStream.Create;
   try
-    ACBrAbecsPinPad1.LoadMedia('LOGOACBR', FS, mtPNG);
+    imgMedia.Picture.SaveToStream(ms);
+    ACBrAbecsPinPad1.LoadMedia( edMediaLoad.Text,
+                                ms,
+                                TACBrAbecsPinPadMediaType(imgMedia.Tag) );
   finally
-    FS.Free;
+    ms.Free;
   end;
 end;
 
@@ -565,9 +774,42 @@ begin
   ReadParams;
 end;
 
+procedure TfrMain.btSearchSerialPorts1Click(Sender: TObject);
+var
+  ext: String;
+begin
+  if OpenPictureDialog1.Execute then
+  begin
+    imgMedia.Picture.Clear;
+    imgMedia.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+    ext := LowerCase(ExtractFileExt(OpenPictureDialog1.FileName));
+    if (ext = '.gif') then
+      imgMedia.Tag := Integer(mtGIF)
+    else if (ext = '.png') then
+      imgMedia.Tag := Integer(mtPNG)
+    else
+      imgMedia.Tag := Integer(mtJPG);
+  end;
+end;
+
 procedure TfrMain.btCancelClick(Sender: TObject);
 begin
   ConfigPanelCancel(True);
+end;
+
+procedure TfrMain.cbGIXAllChange(Sender: TObject);
+begin
+  if (cbGIXAll.Checked) then
+  begin
+    lbGIXParams.ClearSelection;
+    lbGIXParams.Enabled := False;
+    edGIXValue.Enabled := False;
+  end
+  else
+  begin
+    lbGIXParams.Enabled := True;
+    edGIXValue.Enabled := True;
+  end;
 end;
 
 procedure TfrMain.cbSecureChange(Sender: TObject);
@@ -582,15 +824,13 @@ end;
 
 procedure TfrMain.btCLXClick(Sender: TObject);
 begin
-  if pgCLX.ActivePageIndex = 0 then
+  if (pgCLX.ActivePageIndex = 0) then
     ACBrAbecsPinPad1.CLX(mCLX.Lines.Text)
   else
   begin
     if (lbCLXMedias.ItemIndex >= 0) then
       ACBrAbecsPinPad1.CLX(lbCLXMedias.Items[lbCLXMedias.ItemIndex]);
   end;
-
-  //ACBrAbecsPinPad1.CLX('LOGOACBR');
 end;
 
 end.

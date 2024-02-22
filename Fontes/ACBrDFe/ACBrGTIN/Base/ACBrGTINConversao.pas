c60ede82696a9d5d5a57f344afca93ffd398eb85
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-02-22 19:39:32 +0000

    22/02/2024 -- ACBrGTINConversao -- [*] Refactoring na unit ACBrGTINConversao.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@32599 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrGTIN/Base/ACBrGTINConversao.pas
+++ Fontes/ACBrDFe/ACBrGTIN/Base/ACBrGTINConversao.pas
@@ -41,29 +41,39 @@ uses
   ACBrXmlBase;
 
 type
-  TVersaoGTIN     = (ve100);
-
-  TLayOutGTIN     = (LayGTINConsulta);
-
-  TSchemaGTIN     = (schErro, schconsGTIN);
-
   TStatusACBrGTIN = (stGTINIdle, stGTINConsulta, stGTINEnvioWebService);
 
-function StrToEnumerado(out ok: boolean; const s: string; const AString: array of string;
-  const AEnumerados: array of variant): variant;
-function EnumeradoToStr(const t: variant; const AString:
-  array of string; const AEnumerados: array of variant): variant;
+  TVersaoGTIN     = (ve100);
 
+const
+  TVersaoGTINArrayStrings: array[TVersaoGTIN] of string = ('1.00');
+  TVersaoGTINArrayDouble: array[TVersaoGTIN] of Double = (1.00);
+
+type
+  TSchemaGTIN     = (schErro, schconsGTIN);
+
+const
+  TSchemaGTINArrayStrings: array[TSchemaGTIN] of string = ('', '');
+
+type
+  TLayOutGTIN     = (LayGTINConsulta);
+
+const
+  TLayOutGTINArrayStrings: array[TLayOutGTIN] of string = ('GTINConsulta');
+
+{
+  Declaração das funções de conversão
+}
 function LayOutToSchema(const t: TLayOutGTIN): TSchemaGTIN;
 
-function LayOutToServico(const t: TLayOutGTIN): String;
-function ServicoToLayOut(out ok: Boolean; const s: String): TLayOutGTIN;
+function LayOutToServico(const t: TLayOutGTIN): string;
+function ServicoToLayOut(out ok: Boolean; const s: string): TLayOutGTIN;
 
-function SchemaGTINToStr(const t: TSchemaGTIN): String;
-function StrToSchemaGTIN(const s: String): TSchemaGTIN;
+function SchemaGTINToStr(const t: TSchemaGTIN): string;
+function StrToSchemaGTIN(const s: string): TSchemaGTIN;
 
-function StrToVersaoGTIN(out ok: Boolean; const s: String): TVersaoGTIN;
-function VersaoGTINToStr(const t: TVersaoGTIN): String;
+function StrToVersaoGTIN(out ok: Boolean; const s: string): TVersaoGTIN;
+function VersaoGTINToStr(const t: TVersaoGTIN): string;
 
 function DblToVersaoGTIN(out ok: Boolean; const d: Double): TVersaoGTIN;
 function VersaoGTINToDbl(const t: TVersaoGTIN): Double;
@@ -74,36 +84,8 @@ function StrToTipoAmb(out ok: boolean; const s: string): TACBrTipoAmbiente;
 implementation
 
 uses
-  typinfo;
-
-function StrToEnumerado(out ok: boolean; const s: string; const AString:
-  array of string; const AEnumerados: array of variant): variant;
-var
-  i: integer;
-begin
-  result := -1;
-
-  for i := Low(AString) to High(AString) do
-    if AnsiSameText(s, AString[i]) then
-      result := AEnumerados[i];
-
-  ok := result <> -1;
-
-  if not ok then
-    result := AEnumerados[0];
-end;
-
-function EnumeradoToStr(const t: variant; const AString:
-  array of string; const AEnumerados: array of variant): variant;
-var
-  i: integer;
-begin
-  result := '';
-
-  for i := Low(AEnumerados) to High(AEnumerados) do
-    if t = AEnumerados[i] then
-      result := AString[i];
-end;
+  typinfo,
+  ACBrBase;
 
 function LayOutToSchema(const t: TLayOutGTIN): TSchemaGTIN;
 begin
@@ -114,28 +96,39 @@ begin
   end;
 end;
 
-function LayOutToServico(const t: TLayOutGTIN): String;
+function LayOutToServico(const t: TLayOutGTIN): string;
 begin
-  Result := EnumeradoToStr(t, ['GTINConsulta'],
-                              [LayGTINConsulta]);
+  result := TLayOutGTINArrayStrings[t];
 end;
 
-function ServicoToLayOut(out ok: Boolean; const s: String): TLayOutGTIN;
+function ServicoToLayOut(out ok: Boolean; const s: string): TLayOutGTIN;
+var
+  idx: TLayOutGTIN;
 begin
-  Result := StrToEnumerado(ok, s, ['GTINConsulta'],
-                                  [LayGTINConsulta]);
+  ok := True;
+
+  for idx := Low(TLayOutGTINArrayStrings) to High(TLayOutGTINArrayStrings) do
+  begin
+    if (TLayOutGTINArrayStrings[idx] = s) then
+    begin
+      result := idx;
+      exit;
+    end;
+  end;
+
+  raise EACBrException.CreateFmt('Valor string inválido para TLayOutGTIN: %s', [s]);
 end;
 
-function SchemaGTINToStr(const t: TSchemaGTIN): String;
+function SchemaGTINToStr(const t: TSchemaGTIN): string;
 begin
   Result := GetEnumName(TypeInfo(TSchemaGTIN), Integer(t));
   Result := copy(Result, 4, Length(Result)); // Remove prefixo "sch"
 end;
 
-function StrToSchemaGTIN(const s: String): TSchemaGTIN;
+function StrToSchemaGTIN(const s: string): TSchemaGTIN;
 var
   P: Integer;
-  SchemaStr: String;
+  SchemaStr: string;
   CodSchema: Integer;
 begin
   P := pos('_', s);
@@ -156,14 +149,27 @@ begin
   Result := TSchemaGTIN(CodSchema);
 end;
 
-function StrToVersaoGTIN(out ok: Boolean; const s: String): TVersaoGTIN;
+function StrToVersaoGTIN(out ok: Boolean; const s: string): TVersaoGTIN;
+var
+  idx: TVersaoGTIN;
 begin
-  Result := StrToEnumerado(ok, s, ['1.00'], [ve100]);
+  ok := True;
+
+  for idx := Low(TVersaoGTINArrayStrings) to High(TVersaoGTINArrayStrings) do
+  begin
+    if (TVersaoGTINArrayStrings[idx] = s) then
+    begin
+      result := idx;
+      exit;
+    end;
+  end;
+
+  raise EACBrException.CreateFmt('Valor string inválido para TVersaoGTIN: %s', [s]);
 end;
 
-function VersaoGTINToStr(const t: TVersaoGTIN): String;
+function VersaoGTINToStr(const t: TVersaoGTIN): string;
 begin
-  Result := EnumeradoToStr(t, ['1.00'], [ve100]);
+  result := TVersaoGTINArrayStrings[t];
 end;
 
 function DblToVersaoGTIN(out ok: Boolean; const d: Double): TVersaoGTIN;
@@ -181,23 +187,30 @@ end;
 
 function VersaoGTINToDbl(const t: TVersaoGTIN): Double;
 begin
-  case t of
-    ve100: Result := 1.0;
-  else
-    Result := 0;
-  end;
-end;
+ result := TVersaoGTINArrayDouble[t];
+ end;
 
 function TipoAmbToStr(const t: TACBrTipoAmbiente): string;
 begin
-  result := EnumeradoToStr(t, ['1', '2'],
-                              [taProducao, taHomologacao]);
+  result := TACBrTipoAmbienteArrayStrings[t];
 end;
 
 function StrToTipoAmb(out ok: boolean; const s: string): TACBrTipoAmbiente;
+var
+  idx: TACBrTipoAmbiente;
 begin
-  result := StrToEnumerado(ok, s, ['1', '2'],
-                                  [taProducao, taHomologacao]);
+  ok := True;
+
+  for idx := Low(TACBrTipoAmbienteArrayStrings) to High(TACBrTipoAmbienteArrayStrings) do
+  begin
+    if (TACBrTipoAmbienteArrayStrings[idx] = s) then
+    begin
+      result := idx;
+      exit;
+    end;
+  end;
+
+  raise EACBrException.CreateFmt('Valor string inválido para TACBrTipoAmbiente: %s', [s]);
 end;
 
 end.

686d32483242a2d94faff5db32a1e85dcfe54726
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-03-08 11:44:07 +0000

    [+] Problemas na geração de BPE - ACBrBPeXmlWriter.pas https://www.projetoacbr.com.br/forum/topic/76697-problemas-na-gera%C3%A7%C3%A3o-de-bpe-acbrbpexmlwriterpas/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@32824 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrBPe/Base/ACBrBPeXmlWriter.pas
+++ Fontes/ACBrDFe/ACBrBPe/Base/ACBrBPeXmlWriter.pas
@@ -699,7 +699,7 @@ begin
     BPe.infPassagem.dhValidade), DSC_DHVALIDADE));
 
   if BPe.infPassagem.infPassageiro.xNome <> '' then
-    Gerar_InfPassageiro;
+    Result.AppendChild(Gerar_InfPassageiro);//Gerar_InfPassageiro;
 end;
 
 function TBPeXmlWriter.Gerar_InfPassageiro: TACBrXmlNode;
@@ -1397,7 +1397,7 @@ begin
 
   if (BPe.infRespTec.CNPJ <> '') then
   begin
-    Result := FDocument.CreateElement('gRespTec');
+    Result := FDocument.CreateElement('infRespTec');
 
     Result.AppendChild(AddNodeCNPJ('#311', BPe.infRespTec.CNPJ, CODIGO_BRASIL, True));
 

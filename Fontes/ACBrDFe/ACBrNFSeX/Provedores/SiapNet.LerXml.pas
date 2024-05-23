4df7ede02476d6d63cc6cf87afe9e5c037e31ab7
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-05-23 17:38:31 +0000

    23/05/2024 -- SiapNet.LerXml -- [+] Sobre escrito as procedures: LerEnderecoPrestadorServico, LerEnderecoTomador e LerServico,     visando o uso da função GerarDigitoMunicipio.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@33770 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/SiapNet.LerXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/SiapNet.LerXml.pas
@@ -38,6 +38,7 @@ interface
 
 uses
   SysUtils, Classes, StrUtils,
+  ACBrXmlBase, ACBrXmlDocument,
   ACBrNFSeXLerXml_ABRASFv2;
 
 type
@@ -45,16 +46,179 @@ type
 
   TNFSeR_SiapNet200 = class(TNFSeR_ABRASFv2)
   protected
-
+    procedure LerEnderecoPrestadorServico(const ANode: TACBrXmlNode; const aTag: string); override;
+    procedure LerEnderecoTomador(const ANode: TACBrXmlNode); override;
+    procedure LerServico(const ANode: TACBrXmlNode); override;
   public
 
   end;
 
 implementation
 
+uses
+  ACBrDFeUtil,
+  ACBrNFSeXConversao;
+
 //==============================================================================
 // Essa unit tem por finalidade exclusiva ler o XML do provedor:
 //     SiapNet
 //==============================================================================
+
+{ TNFSeR_SiapNet200 }
+
+procedure TNFSeR_SiapNet200.LerEnderecoPrestadorServico(
+  const ANode: TACBrXmlNode; const aTag: string);
+var
+  AuxNode: TACBrXmlNode;
+  xUF: string;
+begin
+  if not Assigned(ANode) then Exit;
+
+  AuxNode := ANode.Childrens.FindAnyNs(aTag);
+
+  if AuxNode <> nil then
+  begin
+    with NFSe.Prestador.Endereco do
+    begin
+      Endereco := ObterConteudo(AuxNode.Childrens.FindAnyNs('Endereco'), tcStr);
+      Numero := ObterConteudo(AuxNode.Childrens.FindAnyNs('Numero'), tcStr);
+      Complemento := ObterConteudo(AuxNode.Childrens.FindAnyNs('Complemento'), tcStr);
+      Bairro := ObterConteudo(AuxNode.Childrens.FindAnyNs('Bairro'), tcStr);
+      CodigoMunicipio := GerarDigitoMunicipio(ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoMunicipio'), tcStr));
+      UF := ObterConteudo(AuxNode.Childrens.FindAnyNs('Uf'), tcStr);
+      CodigoPais := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoPais'), tcInt);
+      CEP := ObterConteudo(AuxNode.Childrens.FindAnyNs('Cep'), tcStr);
+      xMunicipio := ObterNomeMunicipioUF(StrToIntDef(CodigoMunicipio, 0), xUF);
+
+      if UF = '' then
+        UF := xUF;
+    end;
+  end;
+end;
+
+procedure TNFSeR_SiapNet200.LerEnderecoTomador(const ANode: TACBrXmlNode);
+var
+  AuxNode: TACBrXmlNode;
+  xUF, xEndereco: string;
+begin
+  if not Assigned(ANode) then Exit;
+
+  AuxNode := ANode.Childrens.FindAnyNs('Endereco');
+
+  if AuxNode <> nil then
+  begin
+    with NFSe.Tomador.Endereco do
+    begin
+      Endereco := ObterConteudo(AuxNode.Childrens.FindAnyNs('Endereco'), tcStr);
+      Numero := ObterConteudo(AuxNode.Childrens.FindAnyNs('Numero'), tcStr);
+      Complemento := ObterConteudo(AuxNode.Childrens.FindAnyNs('Complemento'), tcStr);
+      Bairro := ObterConteudo(AuxNode.Childrens.FindAnyNs('Bairro'), tcStr);
+      CodigoMunicipio := GerarDigitoMunicipio(ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoMunicipio'), tcStr));
+      UF := ObterConteudo(AuxNode.Childrens.FindAnyNs('Uf'), tcStr);
+      CEP := ObterConteudo(AuxNode.Childrens.FindAnyNs('Cep'), tcStr);
+      xMunicipio := ObterNomeMunicipioUF(StrToIntDef(CodigoMunicipio, 0), xUF);
+
+      if UF = '' then
+        UF := xUF;
+    end;
+  end;
+
+  AuxNode := ANode.Childrens.FindAnyNs('EnderecoExterior');
+
+  if AuxNode <> nil then
+  begin
+    with NFSe.Tomador.Endereco do
+    begin
+      xEndereco := ObterConteudo(AuxNode.Childrens.FindAnyNs('EnderecoCompletoExterior'), tcStr);
+
+      if xEndereco <> '' then
+        Endereco := xEndereco;
+
+      CodigoPais := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoPais'), tcInt);
+
+      if (CodigoPais <> 1058) and (CodigoPais > 0) then
+        UF := 'EX';
+    end;
+  end;
+end;
+
+procedure TNFSeR_SiapNet200.LerServico(const ANode: TACBrXmlNode);
+var
+  AuxNode, AuxNode2: TACBrXmlNode;
+  Ok: Boolean;
+  CodigoItemServico, Responsavel, xUF: string;
+  ValorLiq: Double;
+begin
+  if not Assigned(ANode) then Exit;
+
+  AuxNode := ANode.Childrens.FindAnyNs('Servico');
+
+  if AuxNode <> nil then
+  begin
+    LerValores(AuxNode);
+
+    CodigoItemServico := ObterConteudo(AuxNode.Childrens.FindAnyNs('ItemListaServico'), tcStr);
+
+    with NFSe.Servico do
+    begin
+      Responsavel := ObterConteudo(AuxNode.Childrens.FindAnyNs('ResponsavelRetencao'), tcStr);
+
+      if Responsavel = '' then
+        ResponsavelRetencao := rtNenhum
+      else
+        ResponsavelRetencao := FpAOwner.StrToResponsavelRetencao(Ok, Responsavel);
+
+      ItemListaServico          := NormatizarItemListaServico(CodigoItemServico);
+      xItemListaServico         := ItemListaServicoDescricao(ItemListaServico);
+      CodigoCnae                := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoCnae'), tcStr);
+      CodigoTributacaoMunicipio := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoTributacaoMunicipio'), tcStr);
+      CodigoNBS                 := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoNbs'), tcStr);
+      Discriminacao             := ObterConteudo(AuxNode.Childrens.FindAnyNs('Discriminacao'), tcStr);
+      Discriminacao := StringReplace(Discriminacao, FpQuebradeLinha,
+                                      sLineBreak, [rfReplaceAll, rfIgnoreCase]);
+
+      VerificarSeConteudoEhLista(Discriminacao);
+
+      CodigoMunicipio := GerarDigitoMunicipio(ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoMunicipio'), tcStr));
+
+      MunicipioPrestacaoServico := ObterNomeMunicipioUF(StrToIntDef(CodigoMunicipio, 0), xUF);
+      MunicipioPrestacaoServico := MunicipioPrestacaoServico + '/' + xUF;
+
+      CodigoPais          := ObterConteudo(AuxNode.Childrens.FindAnyNs('CodigoPais'), tcInt);
+      ExigibilidadeISS    := FpAOwner.StrToExigibilidadeISS(Ok, ObterConteudo(AuxNode.Childrens.FindAnyNs('ExigibilidadeISS'), tcStr));
+      IdentifNaoExigibilidade := ObterConteudo(AuxNode.Childrens.FindAnyNs('IdentifNaoExigibilidade'), tcStr);
+
+      MunicipioIncidencia := StrToIntDef(GerarDigitoMunicipio(ObterConteudo(AuxNode.Childrens.FindAnyNs('MunicipioIncidencia'), tcStr)), 0);
+
+      xMunicipioIncidencia := ObterNomeMunicipioUF(MunicipioIncidencia, xUF);
+      xMunicipioIncidencia := xMunicipioIncidencia + '/' + xUF;
+
+      NumeroProcesso := ObterConteudo(AuxNode.Childrens.FindAnyNs('NumeroProcesso'), tcStr);
+
+      if Valores.IssRetido = stNenhum then
+        Valores.IssRetido := FpAOwner.StrToSituacaoTributaria(Ok, ObterConteudo(AuxNode.Childrens.FindAnyNs('IssRetido'), tcStr));
+
+      if Valores.IssRetido = stRetencao then
+        Valores.ValorIssRetido := Valores.ValorIss
+      else
+        Valores.ValorIssRetido := 0;
+
+      Valores.RetencoesFederais := Valores.ValorPis + Valores.ValorCofins +
+                                   Valores.ValorInss + Valores.ValorIr +
+                                   Valores.ValorCsll + Valores.ValorCpp;
+
+      ValorLiq := Valores.ValorServicos - Valores.RetencoesFederais -
+                  Valores.OutrasRetencoes - Valores.ValorIssRetido -
+                  Valores.DescontoIncondicionado - Valores.DescontoCondicionado;
+
+      if (Valores.ValorLiquidoNfse = 0) or (Valores.ValorLiquidoNfse > ValorLiq) then
+        Valores.ValorLiquidoNfse := ValorLiq;
+
+      Valores.ValorTotalNotaFiscal := Valores.ValorServicos -
+                                      Valores.DescontoCondicionado -
+                                      Valores.DescontoIncondicionado;
+    end;
+  end;
+end;
 
 end.

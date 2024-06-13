a26f86ede40bef691af4c33c9bb0455e3c2fde64
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-06-13 12:44:57 +0000

    13/06/2024 -- ACBrNFSeXServicos -- [*] A cidade de Carmo do Rio Claro/MG, trocou de provedor, agora é: Fiorilli.   Por: João Antônio
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@33943 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/IPM.Provider.pas
@@ -71,7 +71,8 @@ type
     function CriarServiceClient(const AMetodo: TMetodo): TACBrNFSeXWebservice; override;
 
     function GerarXMLNota(const AXmlRps: String; const Response: TNFSeWebserviceResponse): String;
-    procedure MontarXMLNFSe(const ANode: TACBrXmlNode; const Response: TNFSeWebserviceResponse);
+    procedure MontarXMLNFSe(const ANode: TACBrXmlNode;
+      const Response: TNFSeWebserviceResponse; AResumo: TNFSeResumoCollectionItem);
 
     function PrepararRpsParaLote(const aXml: string): string; override;
 
@@ -295,7 +296,8 @@ begin
   end;
 end;
 
-procedure TACBrNFSeProviderIPM.MontarXMLNFSe(const ANode: TACBrXmlNode; const Response: TNFSeWebserviceResponse);
+procedure TACBrNFSeProviderIPM.MontarXMLNFSe(const ANode: TACBrXmlNode;
+  const Response: TNFSeWebserviceResponse; AResumo: TNFSeResumoCollectionItem);
 var
   AuxNode: TACBrXmlNode;
   NumRps, LXmlNota: String;
@@ -321,6 +323,8 @@ begin
       begin
         ANota.XmlNfse := LXmlNota;
         SalvarXmlNfse(ANota);
+
+        AResumo.NomeArq := ANota.NomeArq;
       end;
     end;
   end;
@@ -575,8 +579,7 @@ begin
         AResumo.Situacao := Response.Situacao;
         AResumo.DescSituacao := Response.DescSituacao;
 
-        MontarXMLNFSe(ANode, Response);
-
+        MontarXMLNFSe(ANode, Response, AResumo);
       end;
     except
       on E:Exception do
@@ -723,7 +726,7 @@ begin
         AResumo.Situacao := Response.Situacao;
         AResumo.DescSituacao := Response.DescSituacao;
 
-        MontarXMLNFSe(ANode, Response);
+        MontarXMLNFSe(ANode, Response, AResumo);
       end;
     except
       on E:Exception do
@@ -898,8 +901,7 @@ begin
         AResumo.Situacao := Response.Situacao;
         AResumo.DescSituacao := Response.DescSituacao;
 
-        MontarXMLNFSe(ANode, Response);
-
+        MontarXMLNFSe(ANode, Response, AResumo);
       end;
     except
       on E:Exception do
@@ -1093,8 +1095,7 @@ begin
         AResumo.Situacao := Response.Situacao;
         AResumo.DescSituacao := Response.DescSituacao;
 
-        MontarXMLNFSe(ANode, Response);
-
+        MontarXMLNFSe(ANode, Response, AResumo);
       end;
     except
       on E:Exception do
@@ -1354,8 +1355,7 @@ begin
         AResumo.Situacao := Response.Situacao;
         AResumo.DescSituacao := Response.DescSituacao;
 
-        MontarXMLNFSe(ANode, Response);
-
+        MontarXMLNFSe(ANode, Response, AResumo);
       end;
     except
       on E:Exception do

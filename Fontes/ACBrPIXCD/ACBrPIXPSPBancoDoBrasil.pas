7f78ed78e6fbfb485056a816d2cee9d677e30078
juliomar <juliomar@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-06-20 22:12:59 +0000

    [+] Pix Banco do Brasil v2 - Alteração da identificação da sw https://www.projetoacbr.com.br/forum/topic/77011-pix-banco-do-brasil-v2-altera%C3%A7%C3%A3o-da-identifica%C3%A7%C3%A3o-da-sw/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@34038 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrPIXCD/ACBrPIXPSPBancoDoBrasil.pas
+++ Fontes/ACBrPIXCD/ACBrPIXPSPBancoDoBrasil.pas
@@ -92,6 +92,9 @@ type
     procedure ConfigurarQueryParameters(const Method, EndPoint: String); override;
     procedure TratarRetornoComErro(ResultCode: Integer; const RespostaHttp: AnsiString;
       Problema: TACBrPIXProblema); override;
+
+    procedure ConfigurarHeaders(const Method, AURL: String); override;
+
   public
     constructor Create(AOwner: TComponent); override;
     procedure Autenticar; override;
@@ -298,6 +301,13 @@ begin
     if (aEndPoint = cEndPointCob) then
       Result := URLComDelimitador(Result);
   end;
+end;
+
+procedure TACBrPSPBancoDoBrasil.ConfigurarHeaders(const Method, AURL: String);
+begin
+   inherited ConfigurarHeaders(Method, AURL);
+   if ACBrPixCD.DadosAutomacao.CNPJSoftwareHouse <> '' then
+     Http.Headers.Add( 'x-bb-portal-devx-cnpj-parceiro:' + ACBrPixCD.DadosAutomacao.CNPJSoftwareHouse );
 end;
 
 procedure TACBrPSPBancoDoBrasil.ConfigurarQueryParameters(const Method, EndPoint: String);

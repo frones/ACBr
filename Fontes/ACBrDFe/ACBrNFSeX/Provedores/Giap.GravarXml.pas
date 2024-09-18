f825a41185934b471309a1906c179860738622b5
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-09-18 19:37:14 +0000

    18/09/2024 -- Giap.GravarXml -- [*] #TK-5978: Possível solução para gerar a tag Aliquota com 4 casas decimais em função da cidade.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@35298 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Giap.GravarXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Giap.GravarXml.pas
@@ -46,6 +46,8 @@ type
 
   TNFSeW_Giap = class(TNFSeWClass)
   protected
+    procedure Configuracao; override;
+
     function GerarDadosPrestador: TACBrXmlNode;
     function GerarDadosServico: TACBrXmlNode;
     function GerarDadosTomador: TACBrXmlNode;
@@ -68,6 +70,16 @@ uses
 //==============================================================================
 
 { TNFSeW_Giap }
+
+procedure TNFSeW_Giap.Configuracao;
+begin
+  inherited Configuracao;
+
+  FormatoAliq := tcDe2;
+
+  if FpAOwner.ConfigGeral.Params.TemParametro('Aliquota4Casas') then
+    FormatoAliq := tcDe4;
+end;
 
 function TNFSeW_Giap.GerarXml: Boolean;
 var
@@ -242,7 +254,7 @@ function TNFSeW_Giap.GerarItem: TACBrXmlNode;
 begin
   Result := CreateElement('item');
 
-  Result.AppendChild(AddNode(tcDe2, '#1', 'aliquota', 1, 15, 1,
+  Result.AppendChild(AddNode(FormatoAliq, '#1', 'aliquota', 1, 15, 1,
                                             NFSe.Servico.Valores.Aliquota, ''));
 
   Result.AppendChild(AddNode(tcInt, '#1', 'cnae', 1, 8, 0,

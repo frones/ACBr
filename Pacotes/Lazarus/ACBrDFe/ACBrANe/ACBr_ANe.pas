9d784fb6a51d973eaffd6e520172e98c7606f727
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-10-09 19:56:57 +0000

    09/10/2024 [*] Alterações nos pacotes em Delphi e Lazarus do componente ACBrANe por conta do Refactoring. Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@35564 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBrDFe/ACBrANe/ACBr_ANe.pas
+++ Pacotes/Lazarus/ACBrDFe/ACBrANe/ACBr_ANe.pas
@@ -4,11 +4,16 @@
 
 unit ACBr_ANe;
 
+{$warn 5023 off : no warning about unused units}
 interface
 
 uses
   ACBrANe, ACBrANeConfiguracoes, ACBrANeDocumentos, ACBrANeReg, pcaRetEnvANe, 
-  pcaANe, pcaANeR, pcaANeW, pcaConversao, LazarusPackageIntf;
+  pcaANe, pcaANeR, pcaANeW, pcaConversao, ACBrANeInterface, ACBrANeParametros, 
+  ACBrANe.Classes, ACBrANe.Consts, ACBrANe.Conversao, ACBrANe.ProviderBase, 
+  ACBrANe.ProviderManager, ACBrANe.ProviderProprio, ACBrANe.WebServicesBase, 
+  ACBrANe.WebServicesResponse, ATM.Provider, ELT.Provider, 
+  PortoSeguro.Provider, LazarusPackageIntf;
 
 implementation
 

113c3f87216b4c2787bdab34b980fbbd53a9a4f8
renatorubinho <renatorubinho@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-11-08 00:13:01 +0000

    07/11/2024 -- pcesS3500 -- [+] TK-6073: Ajuste conforme NT 01/2024 Por: Renato Rubinho https://www.projetoacbr.com.br/forum/topic/80075-publicada-nota-t%C3%A9cnica-sobre-reonera%C3%A7%C3%A3o-gradual-da-folha-de-pagamentos/
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@35988 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesS3500.pas
+++ Fontes/ACBrDFe/ACBreSocial/PCNeSocial/pcesS3500.pas
@@ -237,7 +237,7 @@ begin
     begin
       Gerador.wCampo(tcStr, '', 'perApurPgto',   7,  7, 1, self.InfoExclusao.ideProcTrab.perApurPgto);
       if VersaoDF > veS01_02_00 then
-        Gerador.wCampo(tcInt, '', 'ideSeqProc', 3, 3, 0, Self.infoExclusao.ideProcTrab.ideSeqProc);
+        Gerador.wCampo(tcInt, '', 'ideSeqProc', 1, 3, 0, Self.infoExclusao.ideProcTrab.ideSeqProc);
     end;
 
     Gerador.wGrupo('/ideProcTrab');

afbe546e9db809334f9a93cc64415725d1529597
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2024-12-17 20:03:52 +0000

    [*]TK-6103 ACBrBoleto - Ajustes para API Itaú   por BrunoD
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@36580 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/WS/ACBrBoletoW_Itau_API.pas
+++ Fontes/ACBrBoleto/WS/ACBrBoletoW_Itau_API.pas
@@ -523,10 +523,10 @@ begin
     if (ATitulo.Instrucao1) <> '' then
     begin
       LJsonDados.AddPair('codigo_instrucao_cobranca', Copy(trim((ATitulo.Instrucao1)), 1, 1));
-      if Boleto.Cedente.CedenteWS.IndicadorPix then
-        LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao1)), 3, 2))
-      else
-        LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao1)), 3, 2));
+      //if Boleto.Cedente.CedenteWS.IndicadorPix then
+      LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao1)), 3, 2))
+      //else
+      //  LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao1)), 3, 2));
       LJsonDados.AddPair('dia_util', StrToBool(IfThen(ATitulo.TipoDiasProtesto = diUteis,'True','False')));
       LJsonArray.AddElementJSON(LJsonDados);
     end;
@@ -534,10 +534,10 @@ begin
     begin
       LJsonDados2 := TACBrJSONObject.Create;
       LJsonDados2.AddPair('codigo_instrucao_cobranca', Copy(trim((ATitulo.Instrucao2)), 1, 1));
-      if Boleto.Cedente.CedenteWS.IndicadorPix then
-        LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao2)), 3, 2))
-      else
-        LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao2)), 3, 2));
+      //if Boleto.Cedente.CedenteWS.IndicadorPix then
+      LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao2)), 3, 2))
+      //else
+      //LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao2)), 3, 2));
       LJsonDados2.AddPair('dia_util', StrToBool(IfThen(ATitulo.TipoDiasProtesto = diUteis,'True','False')));
       LJsonArray.AddElementJSON(LJsonDados2);
     end;
@@ -545,10 +545,10 @@ begin
     begin
       LJsonDados3 := TACBrJSONObject.Create;
       LJsonDados3.AddPair('codigo_instrucao_cobranca', Copy(trim((ATitulo.Instrucao3)), 1,1));
-      if Boleto.Cedente.CedenteWS.IndicadorPix then
-        LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao3)), 3, 2))
-      else
-        LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao3)), 3, 2));
+      //if Boleto.Cedente.CedenteWS.IndicadorPix then
+      LJsonDados.AddPair('quantidade_dias_apos_vencimento', Copy(trim((ATitulo.Instrucao3)), 3, 2))
+      //else
+      //LJsonDados.AddPair('quantidade_dias_instrucao_cobranca', Copy(trim((ATitulo.Instrucao3)), 3, 2));
       LJsonDados3.AddPair('dia_util', StrToBool(IfThen(ATitulo.TipoDiasProtesto = diUteis,'True','False')));
       LJsonArray.AddElementJSON(LJsonDados3);
     end;

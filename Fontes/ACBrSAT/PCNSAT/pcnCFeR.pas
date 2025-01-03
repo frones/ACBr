af44c724ff9e8d080c57c62a9f47447e85d10cca
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-01-03 17:11:52 +0000

    03/01/2025 [*] Retificação do sobrenome.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@36788 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSAT/PCNSAT/pcnCFeR.pas
+++ Fontes/ACBrSAT/PCNSAT/pcnCFeR.pas
@@ -3,8 +3,9 @@
 {  Biblioteca multiplataforma de componentes Delphi para interação com equipa- }
 { mentos de Automação Comercial utilizados no Brasil                           }
 {                                                                              }
+{ Direitos Autorais Reservados (c) 2024 Daniel Simoes de Almeida               }
 {                                                                              }
-{ Colaboradores nesse arquivo: Italo Jurisato Junior                           }
+{ Colaboradores nesse arquivo: Italo Giurizzato Junior                         }
 {                                                                              }
 {  Você pode obter a última versão desse arquivo na pagina do  Projeto ACBr    }
 { Componentes localizado em      http://www.sourceforge.net/projects/acbr      }

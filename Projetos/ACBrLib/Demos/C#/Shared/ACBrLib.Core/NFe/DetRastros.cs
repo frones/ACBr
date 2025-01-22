25ed4f62724351b50d95b1feaa1c126e2e205683
diegoaf <diegoaf@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-01-22 20:46:49 +0000

    [+] #TK-6504: Adiciona novo enumerado DetRastros   Por: Diego Folieni
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@37042 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/NFe/DetRastros.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ACBrLib.Core.NFe
+{
+    [Flags]
+    public enum DetRastros
+    {
+        dr_nLote = 1 << 0,
+        dr_qLote = 1 << 1,
+        dr_dFab = 1 << 2,
+        dr_dVal = 1 << 3,
+        dr_cAgreg = 1 << 4
+    }
+}

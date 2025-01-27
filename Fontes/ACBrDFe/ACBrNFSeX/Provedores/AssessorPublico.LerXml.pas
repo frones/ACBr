584a9e2491f07271f877ac876669cf1b9506a66b
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-01-27 20:53:20 +0000

    27/01/2025 -- ACBrNFSeXServicos -- [*] #TK-6532: A cidade de Brasnorte/MT, trocou de provedor, agora é Fiorilli.   Por: Diego Reckziegel [*] #TK-6536: A cidade de Salto de Pirapora/SP, trocou a URL, o provedor continua o mesmo.   Por: Danny
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@37074 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/AssessorPublico.LerXml.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/AssessorPublico.LerXml.pas
@@ -76,8 +76,11 @@ begin
   DataCancel := ObterConteudo(ANode.Childrens.FindAnyNs('DATACANCEL'), tcStr);
   HoraCancel := ObterConteudo(ANode.Childrens.FindAnyNs('HORACANCEL'), tcStr);
 
-  if (OnlyNumber(DataCancel) <> '') and (OnlyNumber(HoraCancel) <> '') then
-    NFSe.NfseCancelamento.DataHora := StringToDateTime(DataCancel + ' ' + HoraCancel, 'DD/MM/YYYY hh:nn:ss');
+  if OnlyNumber(HoraCancel) <> '' then
+    DataCancel := DataCancel + ' ' + HoraCancel;
+
+  if OnlyNumber(DataCancel) <> '' then
+    NFSe.NfseCancelamento.DataHora := StringToDateTime(DataCancel, 'DD/MM/YYYY hh:nn:ss');
 
   NFSe.MotivoCancelamento := ObterConteudo(ANode.Childrens.FindAnyNs('MOTIVOCANCEL'), tcStr);
   NFSe.JustificativaCancelamento := ObterConteudo(ANode.Childrens.FindAnyNs('JUSTCANCEL'), tcStr);
@@ -132,7 +135,7 @@ end;
 function TNFSeR_AssessorPublico.LerXmlNfse(const ANode: TACBrXmlNode): Boolean;
 var
   AuxNode: TACBrXmlNode;
-  aValor: String;
+  aValor, aHora: string;
   ANodes: TACBrXmlNodeArray;
   i, mes, ano: integer;
 begin
@@ -164,10 +167,13 @@ begin
   if (ano > 0) and (mes > 0) then
     NFSe.Competencia := EncodeDataHora(IntToStr(Ano)+ '/' + Poem_Zeros(mes, 2));
 
-  aValor := ObterConteudo(AuxNode.Childrens.FindAnyNs('DATA'), tcStr) + ' ' +
-            ObterConteudo(AuxNode.Childrens.FindAnyNs('HORA'), tcStr);
+  aValor := ObterConteudo(AuxNode.Childrens.FindAnyNs('DATA'), tcStr);
+  aHora := ObterConteudo(AuxNode.Childrens.FindAnyNs('HORA'), tcStr);
 
-  if Trim(aValor) <> '' then
+  if OnlyNumber(aHora) <> '' then
+    aValor := aValor + ' ' + aHora;
+
+  if OnlyNumber(aValor) <> '' then
     NFSe.DataEmissao := StringToDateTime(aValor, 'DD/MM/YYYY hh:nn:ss');
 
   NFSe.OptanteSimplesNacional := snNao;

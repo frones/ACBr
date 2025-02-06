a4564807eb3c893e445a8df70de62acd60ca1d4c
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-02-06 13:27:29 +0000

    06/02/2025 -- Sintese.Provider -- [*] Alteração na função TratarXmlRetornado visando resolver o erro de UTF-8.   Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@37241 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFSeX/Provedores/Sintese.Provider.pas
+++ Fontes/ACBrDFe/ACBrNFSeX/Provedores/Sintese.Provider.pas
@@ -339,7 +339,10 @@ end;
 function TACBrNFSeXWebserviceSintese204.TratarXmlRetornado(
   const aXML: string): string;
 begin
-  Result := inherited TratarXmlRetornado(aXML);
+  Result := ConverteANSIparaUTF8(aXml);
+  Result := RemoverDeclaracaoXML(Result);
+
+  Result := inherited TratarXmlRetornado(Result);
 
   Result := ParseText(Result);
   Result := RemoverDeclaracaoXML(Result);

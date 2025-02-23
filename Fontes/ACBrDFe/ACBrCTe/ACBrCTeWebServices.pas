243a9844c410126b18b96a627e1e60ecd5a20024
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-02-23 15:14:38 +0000

    23/02/2025 -- ACBrCTeWebServices -- [*] #TK-6647: Ajustes para identificar o CT-e Simplificado no DistribuicaoDFe.    Por: galegoga
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@37449 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrCTe/ACBrCTeWebServices.pas
+++ Fontes/ACBrDFe/ACBrCTe/ACBrCTeWebServices.pas
@@ -3715,7 +3715,8 @@ begin
         *)
         schprocCTe,
         schprocCTeOS,
-        schprocGTVe:
+        schprocGTVe,
+		schprocCTeSimp:
           FNomeArq := FretDistDFeInt.docZip.Items[I].resDFe.chDFe + '-cte.xml';
 
         schprocEventoCTe:
@@ -3735,7 +3736,7 @@ begin
           FPDFeOwner.Gravar(FNomeArq, AXML, aPath);
 
         if (FretDistDFeInt.docZip.Items[I].schema in [schprocCTe, schprocCTeOS,
-             schprocGTVe]) then
+             schprocGTVe, schprocCTeSimp]) then
           FPDFeOwner.Gravar(FNomeArq, AXML, aPath);
       end;
     end;

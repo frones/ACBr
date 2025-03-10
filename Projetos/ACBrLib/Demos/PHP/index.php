c75d6f27a430e08db8685c654d45ac5d82c32d7a
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-03-10 17:43:31 +0000

    Atualização de Index do Demo PHP
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@37797 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/PHP/index.php
+++ Projetos/ACBrLib/Demos/PHP/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="icon" href="https://projetoacbr.com.br/wp-content/uploads/2020/02/favico-acbr.png" type="image/x-icon">
     <style>
         body {
             display: flex;
@@ -13,6 +14,9 @@
             height: 100vh;
             margin: 0;
             font-family: Arial, sans-serif;
+            background-image: url('https://projetoacbr.com.br/wp-content/uploads/2025/02/background-php-e1740514304960.jpg');
+            background-size: cover; 
+            background-position: center; 
         }
         nav ul {
             list-style-type: none;
@@ -31,25 +35,26 @@
             background-color: #007BFF;
             border-radius: 5px;
             transition: background-color 0.3s;
+            background: #333c4e;
         }
         nav ul li a:hover {
-            background-color: #0056b3;
+            background-color: #f4ad24;
         }
         img {
             margin-bottom: 20px;
+            width: 150px;
+            height: 150px;
         }
     </style>
 </head>
 <body>
-<a href="https://projetoacbr.com.br/"><img src="https://projetoacbr.com.br/wp-content/uploads/2024/06/ACBr_144_144.png" alt="ACBr Logo"></a>
+<a href="https://projetoacbr.com.br/"><img src="https://projetoacbr.com.br/wp-content/uploads/2020/02/favico-acbr.png" alt="ACBr Logo"></a>
     <nav>
         <ul>
             <li><a href="/Boleto/ACBrBoletoDemoMT.php">Boleto</a></li>
             <li><a href="/ConsultaCEP/ACBrCEPDemoMT.php">CEP</a></li>
             <li><a href="/ConsultaCNPJ/ACBrConsultaCNPJDemoMT.php">ConsultaCNPJ</a></li>
-            <li><a href="/CTe/ACBrCTeDemoMT.php">CTe</a></li>
             <li><a href="/GTIN/ACBrGTINDemoMT.php">GTIN</a></li>
-            <li><a href="/MDFe/ACBrMDFeDemoMT.php">MDFe</a></li>
             <li><a href="/NFe/ACBrNFeDemoMT.php">NFe</a></li>
             <li><a href="/NFSe/ACBrNFSeDemoMT.php">NFSe</a></li>
         </ul>

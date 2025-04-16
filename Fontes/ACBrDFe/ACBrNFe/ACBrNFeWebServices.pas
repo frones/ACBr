6ee222306a1e9efc6ad0fd820c339f8e8aba6a8e
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-04-16 12:22:56 +0000

    16/04/2025 [-] Corrigido a identificação do tipo de evento. Por: Italo Giurizzato Junior
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@38638 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrDFe/ACBrNFe/ACBrNFeWebServices.pas
+++ Fontes/ACBrDFe/ACBrNFe/ACBrNFeWebServices.pas
@@ -3116,25 +3116,23 @@ begin
     begin
       FPLayout := LayNFeEventoAN;
       UF       := 'AN';
-    end
-    else
-    begin
-      case FEvento.Evento.Items[0].InfEvento.tpEvento of
-        teInsucessoEntregaNFe, teCancInsucessoEntregaNFe:
+    end;
+
+    case FEvento.Evento.Items[0].InfEvento.tpEvento of
+      teInsucessoEntregaNFe, teCancInsucessoEntregaNFe:
+        begin
+          FPLayout := LayNFeEvento;
+          UF       := 'SVRS';
+        end;
+
+      teConcFinanceira, teCancConcFinanceira:
+        begin
+          if (FPConfiguracoesNFe.Geral.ModeloDF = moNFe) then
           begin
             FPLayout := LayNFeEvento;
             UF       := 'SVRS';
           end;
-
-        teConcFinanceira, teCancConcFinanceira:
-          begin
-            if (FPConfiguracoesNFe.Geral.ModeloDF = moNFe) then
-            begin
-              FPLayout := LayNFeEvento;
-              UF       := 'SVRS';
-            end;
-          end;
-      end;
+        end;
     end;
   end;
 

2e6d4f0dae3d2957531fa46b79345675b83acfed
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-05-05 17:44:58 +0000

    Inclusão Config PSP AppLess - C#
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@38867 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/PIXCD/Import/Dinamico/Shared/AppLessConfig.cs
@@ -0,0 +1,47 @@
+﻿using ACBrLib.Core;
+using ACBrLib.Core.Config;
+using ACBrLib.Core.DFe;
+using ACBrLib.Core.PIXCD;
+
+namespace ACBrLib.PIXCD
+{
+    public sealed class AppLessConfig : ACBrLibDFeConfig<ACBrPIXCD>
+    {
+        #region Constructors
+
+        public AppLessConfig(ACBrPIXCD acbrlib) : base(acbrlib, ACBrSessao.AppLess)
+        {
+
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string ClientID
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        public string ClientSecret
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+		public string SecretKeyHMAC
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        public string Scopes
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        #endregion Properties
+    }
+}

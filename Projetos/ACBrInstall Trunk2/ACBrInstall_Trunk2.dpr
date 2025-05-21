88824886f64d7a2dfa04473f419af05c4d635fd1
pandaaa <pandaaa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-05-21 13:15:35 +0000

    - Ajustes do ACBrInstall para controle de MUTEX - Ajustes do ACBr.inc para utilização padrão do XMLDocuments
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@39211 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrInstall Trunk2/ACBrInstall_Trunk2.dpr
+++ Projetos/ACBrInstall Trunk2/ACBrInstall_Trunk2.dpr
@@ -10,15 +10,39 @@ uses
   ACBrUtil.Strings in '..\..\Fontes\ACBrComum\ACBrUtil.Strings.pas',
   ACBrUtil.FilesIO in '..\..\Fontes\ACBrComum\ACBrUtil.FilesIO.pas',
   ACBr.PlataformaInstalacaoAlvo in 'ACBr.PlataformaInstalacaoAlvo.pas',
-  Vcl.Themes,
-  Vcl.Styles;
+  SysUtils,
+  Dialogs,
+  Windows;
 
 {$R *.res}
 
 {$SETPEOSVERSION 5.0}
 {$SETPESUBSYSVERSION 5.0}
 
+var
+  hMutex: THandle;
+  LastError: DWORD;
+
+const
+  MUTEX_NAME = 'ACBrInstallTrunkII';
+
 begin
+  hMutex := CreateMutex(nil, False, PChar(MUTEX_NAME));
+  LastError := GetLastError;
+
+  if (hMutex = 0) or (hMutex = INVALID_HANDLE_VALUE) then
+  begin
+    MessageDlg('Erro ao criar mutex. Código: ' + IntToStr(GetLastError), mtError, [mbOK], 0);
+    Halt(1);
+  end;
+
+  if LastError = ERROR_ALREADY_EXISTS then
+  begin
+    MessageDlg('O ACBrInstall já está aberto. Verifique se ele está minimizado ou em segundo plano.', mtError, [mbOK], 0);
+    CloseHandle(hMutex);
+    Halt(0);
+  end;
+
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.Title := 'Instalação do Projeto ACBr';

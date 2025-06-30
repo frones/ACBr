88cb980c1b9421f5c8c32872023d166b1a342e0b
danegd <danegd@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-06-30 13:13:48 +0000

    30/06/2025 -- ACBrBoletoRet_Sicredi_APIV2.pas -- [+] #TK-7243 Mapeado campo nossonumero sicredi no NossoNumeroCorrespondente tratar a concatenação e deixar somente nossonumero. (LerNossoNumeroCompleto=true devolve com DV) 	https://www.projetoacbr.com.br/forum/topic/83614-mapear-o-campo-nossonumero-sicredi/     Por: Daniel de Morais InfoCotidiano
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@40024 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/WS/ACBrBoletoRet_Sicredi_APIV2.pas
+++ Fontes/ACBrBoleto/WS/ACBrBoletoRet_Sicredi_APIV2.pas
@@ -53,6 +53,7 @@ type
    function TimeSicreditoDateTime(Const AValue : String) : String;
    function RetornaCodigoOcorrencia(pSituacaoGeralBoleto: string): String;
    function RetornaDescricaoStatusTitulo(AStatus: string): String;
+    function TrataNossoNumero(const ANossoNumero: string): string;
  public
    constructor Create(ABoletoWS: TACBrBoleto); override;
    destructor  Destroy; Override;
@@ -66,7 +67,9 @@ implementation
 
 uses
   ACBrBoletoConversao,
-  ACBrJSON;
+  ACBrJSON,
+  ACBrUtil.Base,
+  ACBrUtil.Strings;
 
 { TRetornoEnvio }
 
@@ -153,6 +156,7 @@ begin
               ARetornoWS.DadosRet.IDBoleto.CodBarras       := ARetornoWS.DadosRet.TituloRet.CodBarras;
               ARetornoWS.DadosRet.IDBoleto.LinhaDig        := ARetornoWS.DadosRet.TituloRet.LinhaDig;
               ARetornoWS.DadosRet.IDBoleto.NossoNum        := ARetornoWS.DadosRet.TituloRet.NossoNumero;
+              ARetornoWS.DadosRet.TituloRet.NossoNumeroCorrespondente  := TrataNossoNumero(ARetornoWS.DadosRet.TituloRet.NossoNumero);
 
               //Pagador
               //ARetornoWS.DadosRet.TituloRet.Sacado.codigo         := LJsonObject.AsJSONObject['pagador'].AsString['codigo'];
@@ -243,6 +247,8 @@ begin
             ARetornoWS.DadosRet.TituloRet.CodBarras     := ARetornoWS.DadosRet.IDBoleto.CodBarras;
             ARetornoWS.DadosRet.TituloRet.LinhaDig      := ARetornoWS.DadosRet.IDBoleto.LinhaDig;
             ARetornoWS.DadosRet.TituloRet.NossoNumero   := ARetornoWS.DadosRet.IDBoleto.NossoNum;
+            ARetornoWS.DadosRet.TituloRet.NossoNumeroCorrespondente  := TrataNossoNumero(ARetornoWS.DadosRet.TituloRet.NossoNumero);
+
           end else
           if (LTipoOperacao = tpBaixa) then
           begin
@@ -349,6 +355,8 @@ begin
 
 
             LListaRetorno.DadosRet.TituloRet.NossoNumero                := LListaRetorno.DadosRet.IDBoleto.NossoNum;
+            LListaRetorno.DadosRet.TituloRet.NossoNumeroCorrespondente  := TrataNossoNumero(LListaRetorno.DadosRet.TituloRet.NossoNumero);
+
             LListaRetorno.DadosRet.TituloRet.DataBaixa                  := DateSicreditoDateTime(LItemObject.AsString['dataPagamento']);
             LListaRetorno.DadosRet.TituloRet.HoraBaixa                  := TimeSicreditoDateTime(LItemObject.AsString['dataPagamento']);
             LListaRetorno.DadosRet.TituloRet.SeuNumero                  := LItemObject.AsString['seuNumero'];
@@ -445,6 +453,23 @@ begin
   LSegundos := Copy( AValue, 18,2 );
   Result   := Format( '%s:%s:%s' , [LHora,LMinuto,LSegundos]);
 end;
+
+function TRetornoEnvio_Sicredi_APIV2.TrataNossoNumero(
+  const ANossoNumero: string): string;
+var
+  LTamanhoNossoNumero : integer;
+begin
+  LTamanhoNossoNumero := 5;
+  if Assigned(ACBrBoleto) then
+  begin
+    if ACBrBoleto.LerNossoNumeroCompleto then
+       LTamanhoNossoNumero := 6;
+  end;
+
+  if NaoEstaVazio(ANossoNumero) and TamanhoIgual(ANossoNumero,9) then
+    Result := RemoveZerosEsquerda(Copy(ANossoNumero,4,LTamanhoNossoNumero));
+end;
+
 
 end.
 

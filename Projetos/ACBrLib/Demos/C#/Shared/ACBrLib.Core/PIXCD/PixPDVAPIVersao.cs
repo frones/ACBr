dfc7cd37f0ed857b6e32d9f8a64ea5dbedfcbe82
antoniocarlosjr <antoniocarlosjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-07-10 22:41:02 +0000

    C# - Nova classe para configurar versão de API para uso do PSP PixPDV.
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@40229 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Projetos/ACBrLib/Demos/C#/Shared/ACBrLib.Core/PIXCD/PixPDVAPIVersao.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace ACBrLib.Core.PIXCD
+{
+    public enum PixPDVAPIVersao
+    {
+        [Description("API Versão 1")]
+        apiVersao1 = 0,
+
+        [Description("API Versão Mediator")]
+        apiVersaoMediator = 1,
+    }
+}

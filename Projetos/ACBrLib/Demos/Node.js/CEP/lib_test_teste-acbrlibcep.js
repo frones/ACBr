bde61106b837ca597253fc3f270614f43625ce0d
doszapro <doszapro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2025-07-15 19:34:48 +0000

    Demo ACBrLib/Demos/Node.js/CEP/lib_test_teste-acbrlibcep.js --- [-] Remove referencias ffi-napi e ref-napi [*] Migra projeto de ffi-napi para koffi
    Por: doszapro
    
    git-svn-id: svn://svn.code.sf.net/p/acbr/code/trunk2@40350 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrLib/Demos/Node.js/CEP/lib_test_teste-acbrlibcep.js
+++ Projetos/ACBrLib/Demos/Node.js/CEP/lib_test_teste-acbrlibcep.js
@@ -1,33 +1,45 @@
 const path = require('path');
-const ffi = require('ffi-napi');
-const ref = require('ref-napi');
+const koffi = require('koffi');
 
 const pathDllACBrLibCEP = path.join(__dirname, 'ACBrCEP64.dll')
 var eArqConfig = path.join(__dirname, 'ACBrLib.ini');
 var eChaveCrypt = '';
 
-
-var tint = ref.refType('int');
-var tchar = ref.refType('string');
-
-var lib = ffi.Library(pathDllACBrLibCEP, {
-    CEP_Inicializar: ['int', ['string', 'string']],
-    CEP_Finalizar: ['int', []],
-    CEP_BuscarPorCEP: ['int', ['string', tchar, tint]],
-})
+const acbrcep = koffi.load(pathDllACBrLibCEP)
+const TAMANHO_BUFFER = 1024;
+const lib = {
+    CEP_Inicializar: acbrcep.func("CEP_Inicializar", 'int', ['string', 'string']),
+    CEP_ConfigGravarValor: acbrcep.func("CEP_ConfigGravarValor", 'int', ['string', 'string', 'string']),
+    CEP_ConfigGravar: acbrcep.func("CEP_ConfigGravar", 'int', ['string']),
+    CEP_UltimoRetorno: acbrcep.func("CEP_UltimoRetorno", 'int', ['char*', 'int*']),
+    CEP_Nome: acbrcep.func("CEP_Nome", 'int', ['char*', 'int*']),
+    CEP_Versao: acbrcep.func("CEP_Versao", 'int', ['char*', 'int*']),
+    CEP_ConfigLer: acbrcep.func("CEP_ConfigLer", 'int', ['string']),
+    CEP_ConfigLerValor: acbrcep.func("CEP_ConfigLerValor", 'int', ['string', 'string', 'char*', 'int*']),
+    CEP_Finalizar:  acbrcep.func("CEP_Finalizar", 'int', []),
+    CEP_BuscarPorCEP: acbrcep.func("CEP_BuscarPorCEP", 'int', ['string', 'char*', 'int*'])
+}
 
 try {
 
     var inicio = 2;
-    const buflength = 256;
 
-    let aloc_sResposta = Buffer.alloc(buflength);
-    let aloc_esTamanho = ref.alloc('int', buflength);
+    let aloc_sResposta = Buffer.alloc(TAMANHO_BUFFER);
+    let aloc_esTamanho = koffi.alloc('int', 1);
 
+    //configura tamanho do buffer
+    koffi.encode(aloc_esTamanho, 'int', TAMANHO_BUFFER)
 
 
     inicio = lib.CEP_Inicializar(eArqConfig, eChaveCrypt);
     console.log(`iniciou >>>>>>> ${inicio}`);
+
+    //configura biblioteca
+    lib.CEP_ConfigGravarValor('Principal','LogPath',__dirname)
+    lib.CEP_ConfigGravarValor('Principal','LogNivel','4')
+    lib.CEP_ConfigGravarValor('CEP','WebService','10')
+    lib.CEP_ConfigGravar(eArqConfig)
+    
 
     inicio = lib.CEP_BuscarPorCEP('18270-170', aloc_sResposta, aloc_esTamanho);
     console.log(`ultmio retorno >>>>>>>> ${inicio}`);
